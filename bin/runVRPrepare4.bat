28196099dc15a0c1fd85c4e49f1be8a7fbbf1d0a
Uwe Woessner <woessner@hlrs.de>
2015-02-02 13:52:56 +0100

    start script fixes

--- bin/runVRPrepare4.bat
+++ bin/runVRPrepare4.bat
@@ -22,14 +22,14 @@ DEL /Q "%TMP%\-3690*" > NUL
 
 REM get the 8.3 filename of current installation
 SET TMPFILE="%TMP%\~covtmpfile.txt"
-CALL "%WINDIR%\system32\cscript.exe" /nologo "%~dp0get8dot3path.vbs" "%~dp0">%TMPFILE%
+CALL "%WINDIR%\system32\cscript.exe" /nologo "%~dp0\..\scripts\get8dot3path.vbs" "%~dp0">%TMPFILE%
 SET /P INSTDIR=<%TMPFILE%
 DEL /Q %TMPFILE%
 
 SET EXTERNLIBS=%INSTDIR%\extern_libs
 
 REM note: ARCHSUFFIX has to be set in common.local.bat
-CALL %INSTDIR%\common.VISENSO.bat NONE %INSTDIR%
+CALL %INSTDIR%\scripts\common.VISENSO.bat NONE %INSTDIR%
 
 CALL "%~dp0\runCoviseScriptIF.bat" vr-prepare4 %*
 
