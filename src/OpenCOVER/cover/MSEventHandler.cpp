2166aceaa951f437e00eb5783b384e9e9b8a4ea2
Martin Aumüller <aumuell@reserv.at>
2015-02-27 14:50:46 +0100

    dedicated evdev keyboard device for OpenCOVER
    configure via COVER.Input.Keyboard:evdev

--- src/OpenCOVER/cover/MSEventHandler.cpp
+++ src/OpenCOVER/cover/MSEventHandler.cpp
@@ -46,6 +46,11 @@
 #include <unistd.h>
 #endif
 
+#ifdef __linux__
+#include <linux/input.h>
+#include <sys/inotify.h>
+#endif
+
 #ifndef OSG_NOTICE
 #define OSG_NOTICE std::cerr
 #endif
@@ -57,7 +62,6 @@ using namespace covise;
 // non-blocking input from stdin -- see http://ubuntuforums.org/showthread.php?t=1396108
 static int getch()
 {
-    int ch;
     struct termios old;
     struct termios tmp;
 
@@ -80,7 +84,7 @@ static int getch()
         return -1;
     fcntl(STDIN_FILENO, F_SETFL, oflags | O_NONBLOCK);
 
-    ch = getchar();
+    int ch = getchar();
 
     fcntl(STDIN_FILENO, F_SETFL, oflags);
 
@@ -88,57 +92,354 @@ static int getch()
 
     return ch;
 }
-
 #endif
+
+MSEventHandler::MSEventHandler()
+: numEventsToSync(0)
+, handleTerminal(true)
+, keyboardFd(-1)
+, notifyFd(-1)
+, watchFd(-1)
+, modifierState(0)
+{
+    if (coVRMSController::instance()->isMaster())
+    {
+        handleTerminal = covise::coCoviseConfig::isOn("terminal", "COVER.Input.Keyboard", true);
+
+        bool useEvent = false;
+        devicePathname = covise::coCoviseConfig::getEntry("evdev", "COVER.Input.Keyboard", &useEvent);
+        if (useEvent && openEvdev())
+        {
+            notifyFd = inotify_init();                                                                                             
+            if (notifyFd < 0)
+                perror("inotify_init");
+
+            size_t last = devicePathname.rfind("/");
+            devicePath = devicePathname.substr(0, last + 1);
+            deviceName = devicePathname.substr(last + 1);
+            int watchFd = inotify_add_watch(notifyFd, devicePath.c_str(), IN_DELETE | IN_CREATE);
+        }
+    }
+}
+
+MSEventHandler::~MSEventHandler()
+{
+    if (keyboardFd >= 0)
+        close(keyboardFd);
+    if (watchFd >= 0)
+        close(watchFd);
+    if (notifyFd >= 0)
+        close(notifyFd);
+}
+
+bool MSEventHandler::openEvdev()
+{
+    keyboardFd = open(devicePathname.c_str(), O_RDONLY | O_NONBLOCK);
+    if (keyboardFd < 0)
+    {
+        perror("Keyboard: failed to open device");
+        return false;
+    }
+
+    char name[256] = "Unknown";
+    if (ioctl(keyboardFd, EVIOCGNAME(sizeof(name)), name) < 0)
+    {
+        perror("Keyboard: no event device");
+        close(keyboardFd);
+        keyboardFd = -1;
+        return false;
+    }
+
+    std::cerr << "Keyboard: evdev keyboard \"" << name << "\"" << std::endl;
+}
 
 void MSEventHandler::update()
 {
-    if (!(coVRMSController::instance()->isSlave()))
+    if (coVRMSController::instance()->isMaster())
     {
 #ifndef _WIN32
-#ifndef DONT
-        bool escape = false;
-
-        int key = -1;
-        while ((key = getch()) != -1)
+        if (handleTerminal)
         {
-            fprintf(stderr, "key: %d\n", key);
+            bool escape = false;
 
-            if (key == 27)
+            int key = -1;
+            while ((key = getch()) != -1)
             {
-                // Alt was pressed
-                escape = true;
-                continue;
-            }
+                fprintf(stderr, "key: %d\n", key);
 
-            int mod = 0;
-            if (escape)
+                if (key == 27)
+                {
+                    // Alt was pressed
+                    escape = true;
+                    continue;
+                }
+
+                int mod = 0;
+                if (escape)
+                {
+                    mod |= osgGA::GUIEventAdapter::MODKEY_ALT;
+                    escape = false;
+                }
+
+                if (key >= 1 && key <= 26)
+                {
+                    // letter together with Ctrl
+                    mod |= osgGA::GUIEventAdapter::MODKEY_CTRL;
+                    key += 64;
+                }
+
+                if (key >= 65 && key <= 90)
+                {
+                    mod |= osgGA::GUIEventAdapter::MODKEY_SHIFT;
+                }
+
+                eventBuffer[numEventsToSync] = osgGA::GUIEventAdapter::KEYDOWN;
+                modBuffer[numEventsToSync] = mod;
+                keyBuffer[numEventsToSync] = key;
+                ++numEventsToSync;
+
+                eventBuffer[numEventsToSync] = osgGA::GUIEventAdapter::KEYUP;
+                modBuffer[numEventsToSync] = mod;
+                keyBuffer[numEventsToSync] = key;
+                ++numEventsToSync;
+            }
+        }
+#endif
+
+#ifdef __linux__
+#ifdef EVIOCGNAME
+        {
+            struct timeval time;
+            time.tv_sec = 0;
+            time.tv_usec = 0;
+            fd_set rfds;
+            FD_ZERO(&rfds);
+            FD_SET(notifyFd, &rfds);
+            const int ret = select(notifyFd + 1, &rfds, NULL, NULL, &time);
+            if (ret > 0 && FD_ISSET(notifyFd, &rfds))
             {
-                mod |= osgGA::GUIEventAdapter::MODKEY_ALT;
-                escape = false;
-            }
+                char notifyBuf[4096];
+                const int len = read(notifyFd, (struct inotify_event *)notifyBuf, sizeof(notifyBuf));
+                int i = 0;
+                while (i < len)
+                {
+                    struct inotify_event *e = (struct inotify_event *)&notifyBuf[i];
 
-            if (key >= 1 && key <= 26)
+                    if (e->mask & IN_DELETE)
+                    {
+                        if (deviceName == e->name)
+                        {
+                            std::cerr << "Keyboard: closing device " << devicePathname << std::endl;
+                            close(keyboardFd);
+                            keyboardFd = -1;
+                        }
+                    }
+                    if (e->mask & IN_CREATE)
+                    {
+                        if (deviceName == e->name && keyboardFd == -1)
+                        {
+                            openEvdev();
+                        }
+                    }
+
+                    i += (sizeof(struct inotify_event) + e->len);
+                }
+            }
+        }
+
+        {
+            struct input_event ev;
+            int ret = 0;
+            while ((ret = read(keyboardFd, &ev, sizeof(ev))) == sizeof(ev))
             {
-                // letter together with Ctrl
-                mod |= osgGA::GUIEventAdapter::MODKEY_CTRL;
-                key += 64;
+                keyBuffer[numEventsToSync] = 0;
+                modBuffer[numEventsToSync] = 0;
+                eventBuffer[numEventsToSync] = 0;
+
+                if (ev.type == EV_KEY)
+                {
+                    bool handled = false;
+#define LETTER(x) \
+                    case KEY_##x: \
+                                  keyBuffer[numEventsToSync] = #x[0] - 'A' + 'a'; \
+                    handled = true; \
+                    break;
+#define FUNC(x) \
+                    case KEY_F##x: \
+                                   keyBuffer[numEventsToSync] = 0xffbe /*osgGA::GUIEventAdapter::KEY_F1*/ + x - 1; \
+                    handled = true; \
+                    break;
+
+#define NUM(x) \
+                    case KEY_##x: \
+                                  keyBuffer[numEventsToSync] = #x[0]; \
+                    handled = true; \
+                    break;
+
+#define KP(x) \
+                    case KEY_KP##x: \
+                                    keyBuffer[numEventsToSync] = #x[0]; \
+                    handled = true; \
+                    break;
+
+                    int modifierBit = 0;
+                    switch (ev.code)
+                    {
+                        LETTER(A)
+                            LETTER(B)
+                            LETTER(C)
+                            LETTER(D)
+                            LETTER(E)
+                            LETTER(F)
+                            LETTER(G)
+                            LETTER(H)
+                            LETTER(I)
+                            LETTER(J)
+                            LETTER(K)
+                            LETTER(L)
+                            LETTER(M)
+                            LETTER(N)
+                            LETTER(O)
+                            LETTER(P)
+                            LETTER(Q)
+                            LETTER(R)
+                            LETTER(S)
+                            LETTER(T)
+                            LETTER(U)
+                            LETTER(V)
+                            LETTER(W)
+                            LETTER(X)
+                            LETTER(Y)
+                            LETTER(Z)
+
+                            NUM(0)
+                            NUM(1)
+                            NUM(2)
+                            NUM(3)
+                            NUM(4)
+                            NUM(5)
+                            NUM(6)
+                            NUM(7)
+                            NUM(8)
+                            NUM(9)
+
+                            KP(0)
+                            KP(1)
+                            KP(2)
+                            KP(3)
+                            KP(4)
+                            KP(5)
+                            KP(6)
+                            KP(7)
+                            KP(8)
+                            KP(9)
+
+                            FUNC(1)
+                            FUNC(2)
+                            FUNC(3)
+                            FUNC(4)
+                            FUNC(5)
+                            FUNC(6)
+                            FUNC(7)
+                            FUNC(8)
+                            FUNC(9)
+                            FUNC(10)
+                            FUNC(11)
+                            FUNC(12)
+                            FUNC(13)
+                            FUNC(14)
+                            FUNC(15)
+                            FUNC(16)
+                            FUNC(17)
+                            FUNC(18)
+                            FUNC(19)
+                            FUNC(20)
+                            FUNC(21)
+                            FUNC(22)
+                            FUNC(23)
+                            FUNC(24)
+
+                        case KEY_SPACE:
+                            keyBuffer[numEventsToSync] = ' ';
+                            handled = true;
+                            break;
+
+                        case KEY_COMMA:
+                            keyBuffer[numEventsToSync] = ',';
+                            handled = true;
+                            break;
+
+                        case KEY_DOT:
+                            keyBuffer[numEventsToSync] = '.';
+                            handled = true;
+                            break;
+
+                        case KEY_SLASH:
+                            keyBuffer[numEventsToSync] = '/';
+                            handled = true;
+                            break;
+
+                        case KEY_ESC:
+                            keyBuffer[numEventsToSync] = 27;
+                            handled = true;
+                            break;
+
+                        case KEY_SEMICOLON:
+                            keyBuffer[numEventsToSync] = ';';
+                            handled = true;
+                            break;
+
+                        case KEY_APOSTROPHE:
+                            keyBuffer[numEventsToSync] = '\'';
+                            handled = true;
+                            break;
+
+                        case KEY_LEFTSHIFT:
+                        case KEY_RIGHTSHIFT:
+                            modifierBit = osgGA::GUIEventAdapter::MODKEY_SHIFT;
+                            //handled = true;
+                            break;
+
+                        case KEY_LEFTALT:
+                        case KEY_RIGHTALT:
+                            modifierBit = osgGA::GUIEventAdapter::MODKEY_ALT;
+                            //handled = true;
+                            break;
+
+                        case KEY_LEFTCTRL:
+                        case KEY_RIGHTCTRL:
+                            modifierBit = osgGA::GUIEventAdapter::MODKEY_CTRL;
+                            //handled = true;
+                            break;
+                    }
+
+                    if (ev.value == 0)
+                    {
+                        eventBuffer[numEventsToSync] = osgGA::GUIEventAdapter::KEYUP;
+                        if (modifierBit)
+                            modifierState &= ~modifierBit;
+                    }
+                    else if (ev.value == 1)
+                    {
+                        eventBuffer[numEventsToSync] = osgGA::GUIEventAdapter::KEYDOWN;
+                        if (modifierBit)
+                            modifierState |= modifierBit;
+                    }
+
+                    if (modifierState & osgGA::GUIEventAdapter::MODKEY_SHIFT)
+                    {
+                        if (keyBuffer[numEventsToSync] >= 'a' && keyBuffer[numEventsToSync] <= 'z')
+                            keyBuffer[numEventsToSync] += 'A' - 'a';
+                    }
+
+                    modBuffer[numEventsToSync] = modifierState;
+
+                    if (handled)
+                    {
+                        ++numEventsToSync;
+                    }
+                }
             }
-
-            if (key >= 65 && key <= 90)
-            {
-                mod |= osgGA::GUIEventAdapter::MODKEY_SHIFT;
-            }
-
-            eventBuffer[numEventsToSync] = osgGA::GUIEventAdapter::KEYDOWN;
-            modBuffer[numEventsToSync] = mod;
-            keyBuffer[numEventsToSync] = key;
-            ++numEventsToSync;
-
-            eventBuffer[numEventsToSync] = osgGA::GUIEventAdapter::KEYUP;
-            modBuffer[numEventsToSync] = mod;
-            keyBuffer[numEventsToSync] = key;
-            ++numEventsToSync;
         }
 #endif
 #endif
--- src/OpenCOVER/cover/MSEventHandler.h
+++ src/OpenCOVER/cover/MSEventHandler.h
@@ -8,24 +8,27 @@ namespace opencover
 class MSEventHandler : public osgGA::GUIEventHandler
 {
 public:
-    MSEventHandler()
-    {
-        numEventsToSync = 0;
-    }
+    MSEventHandler();
+    ~MSEventHandler();
 
     void update();
     virtual bool handle(const osgGA::GUIEventAdapter &ea, osgGA::GUIActionAdapter &);
 
-    /* virtual void accept(osgGA::GUIEventHandlerVisitor& v)
-      {
-         v.visit(*this);
-      }*/
-
 protected:
-    int eventBuffer[1000];
-    int keyBuffer[1000];
-    int modBuffer[1000];
+    static const int NumEvents = 1000;
+    int eventBuffer[NumEvents];
+    int keyBuffer[NumEvents];
+    int modBuffer[NumEvents];
     int numEventsToSync;
+
+    bool handleTerminal; //< read keyboard input from controlling terminal
+    // evdev keyboard
+    int keyboardFd, notifyFd, watchFd;
+    std::string devicePathname;
+    std::string devicePath, deviceName;
+    int modifierState;
+
+    bool openEvdev();
 };
 }
 
