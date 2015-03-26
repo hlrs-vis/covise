171abfb4184a3ccf259e1bd82f734d2961f7972e
Andreas Grimm <andreas.grimm@hlrs.de>
2015-03-26 14:21:31 +0100

    removed Qt-Signals and Slots in mui and added eventListener

---
+++ src/OpenCOVER/cover/mui/support/coMUIListener.cpp
@@ -0,0 +1,5 @@
+#include "coMUIListener.h"
+
+coMUIListener::coMUIListener()
+{
+}
---
+++ src/OpenCOVER/cover/mui/support/coMUIListener.h
@@ -0,0 +1,10 @@
+#ifndef COMUILISTENER_H
+#define COMUILISTENER_H
+
+class coMUIListener
+{
+public:
+    coMUIListener();
+};
+
+#endif // COMUILISTENER_H
