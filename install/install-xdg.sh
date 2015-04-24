d1f4393fec08dc8982b8832f77d9fa33237a4aca
Martin Aumüller <aumuell@reserv.at>
2015-04-24 14:27:04 +0200

    make tablet UI available from menu

--- install/install-xdg.sh
+++ install/install-xdg.sh
@@ -2,6 +2,7 @@ xdg-mime install hlrs-net.xml
 
 xdg-desktop-menu install --noupdate hlrs-covise.desktop
 xdg-desktop-menu install --noupdate hlrs-cover.desktop
+xdg-desktop-menu install --noupdate hlrs-tablet.desktop
 xdg-desktop-menu forceupdate
 
 xdg-icon-resource install --noupdate --size 48 hlrs-cover.png
