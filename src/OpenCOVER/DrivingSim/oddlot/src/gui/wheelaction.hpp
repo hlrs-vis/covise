bd389291692ddf763d24c76400aeb38538da3302
hpcfloh <sauer@hlrs.de>
2015-10-12 08:54:51 +0200

    notify topviewgraph of wheel actions in the scene

---
+++ src/OpenCOVER/DrivingSim/oddlot/src/gui/wheelaction.hpp
@@ -0,0 +1,78 @@
+/* This file is part of COVISE.
+
+   You can use it under the terms of the GNU Lesser General Public License
+   version 2.1 or later, see lgpl-2.1.txt.
+
+ * License: LGPL 2+ */
+
+/**************************************************************************
+** ODD: OpenDRIVE Designer
+**   Frank Naegele (c) 2010
+**   <mail@f-naegele.de>
+**   10.05.2010
+**
+**************************************************************************/
+
+#ifndef WHEELACTION_HPP
+#define WHEELACTION_HPP
+
+class QGraphicsSceneWheelEvent;
+
+class WheelAction
+{
+
+    //################//
+    // STATIC         //
+    //################//
+
+public:
+
+    //################//
+    // FUNCTIONS      //
+    //################//
+
+public:
+    WheelAction(QGraphicsSceneWheelEvent *event);
+    virtual ~WheelAction()
+    { /* does nothing */
+    }
+
+    QGraphicsSceneWheelEvent *getEvent() const
+    {
+        return event_;
+    }
+
+    // Interception //
+    //
+    void intercept()
+    {
+        intercepted_ = true;
+    }
+    bool isIntercepted() const
+    {
+        return intercepted_;
+    }
+
+protected:
+private:
+    WheelAction()
+    { /* not allowed */
+    }
+    WheelAction(const WheelAction &)
+    { /* not allowed */
+    }
+
+    //################//
+    // PROPERTIES     //
+    //################//
+
+protected:
+private:
+    QGraphicsSceneWheelEvent *event_;
+
+    // Interception //
+    //
+    bool intercepted_;
+};
+
+#endif // WheelAction_HPP
