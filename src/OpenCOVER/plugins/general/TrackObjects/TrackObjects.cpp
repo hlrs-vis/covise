3082ad6bc69b203befaedba7c3086b3774d21345
Uwe Woessner <woessner@hlrs.de>
2015-11-04 07:57:01 +0100

    first working version

--- src/OpenCOVER/plugins/general/TrackObjects/TrackObjects.cpp
+++ src/OpenCOVER/plugins/general/TrackObjects/TrackObjects.cpp
@@ -1,22 +1,22 @@
 /* This file is part of COVISE.
 
-   You can use it under the terms of the GNU Lesser General Public License
-   version 2.1 or later, see lgpl-2.1.txt.
+You can use it under the terms of the GNU Lesser General Public License
+version 2.1 or later, see lgpl-2.1.txt.
 
- * License: LGPL 2+ */
+* License: LGPL 2+ */
 
 /****************************************************************************\ 
- **                                                            (C)2001 HLRS  **
- **                                                                          **
- ** Description: TrackObjects Plugin (does nothing)                          **
- **                                                                          **
- **                                                                          **
- ** Author: U.Woessner		                                                 **
- **                                                                          **
- ** History:  								                                 **
- ** Nov-01  v1	    				       		                             **
- **                                                                          **
- **                                                                          **
+**                                                            (C)2001 HLRS  **
+**                                                                          **
+** Description: TrackObjects Plugin (does nothing)                          **
+**                                                                          **
+**                                                                          **
+** Author: U.Woessner		                                                 **
+**                                                                          **
+** History:  								                                 **
+** Nov-01  v1	    				       		                             **
+**                                                                          **
+**                                                                          **
 \****************************************************************************/
 
 #include "TrackObjects.h"
@@ -27,6 +27,7 @@
 #include <PluginUtil/coBaseCoviseInteractor.h>
 #include <OpenVRUI/coToolboxMenu.h>
 #include <cover/coVRCollaboration.h>
+#include <config/CoviseConfig.h>
 
 #include <vrml97/vrml/VrmlNodeCOVER.h>
 
@@ -34,6 +35,33 @@ using namespace covise;
 using vrml::VrmlNodeCOVER;
 using vrml::theCOVER;
 
+
+TObject::TObject(const std::string &n)
+{
+    name = n;
+    const std::string conf = "COVER.Plugin.TrackObjects.Objects.Object:" + name;
+    offsetCoord.xyz[0] = coCoviseConfig::getFloat("x",conf,0.0);
+    offsetCoord.xyz[1] = coCoviseConfig::getFloat("y",conf,0.0);
+    offsetCoord.xyz[2] = coCoviseConfig::getFloat("z",conf,0.0);
+    offsetCoord.hpr[0] = coCoviseConfig::getFloat("h",conf,0.0);
+    offsetCoord.hpr[1] = coCoviseConfig::getFloat("p",conf,0.0);
+    offsetCoord.hpr[2] = coCoviseConfig::getFloat("r",conf,0.0);
+
+    offsetCoord.makeMat(offset);
+}
+TObject::~TObject()
+{
+}
+void TObject::setOffset(float x, float y, float z, float h, float p, float r)
+{
+    offsetCoord.xyz[0] = x;
+    offsetCoord.xyz[1] = y;
+    offsetCoord.xyz[2] = z;
+    offsetCoord.hpr[0] = h;
+    offsetCoord.hpr[1] = p;
+    offsetCoord.hpr[2] = r;
+    offsetCoord.makeMat(offset);
+}
 TrackObjects::TrackObjects()
 {
 }
@@ -42,6 +70,7 @@ bool TrackObjects::init()
 {
     pinboardEntry = new coSubMenuItem("Tangible");
     execButton = new coButtonMenuItem("RestartSimulation");
+
     execButton->setMenuListener(this);
     TangibleSimulationMenu = new coRowMenu("TangibleSimulationMenu");
     TangibleSimulationMenu->add(execButton);
@@ -57,14 +86,66 @@ bool TrackObjects::init()
         cover->getToolBar()->insert(ToolbarButton, 0);
     }
 
+    std::vector<std::string> names = coCoviseConfig::getScopeNames("COVER.Plugin.TrackObjects.Objects", "Object");
+
+    if (names.empty())
+    {
+        cout << "One Offset must be configured!" << endl;
+        names.push_back("default");
+    }
+    for (size_t i = 0; i < names.size(); ++i)
+    {
+        TObjects.push_back(new TObject(names[i]));
+    }
+    currentObject = TObjects[0];
+
     TrackObjectsTab = new coTUITab("TrackObjects", coVRTui::instance()->mainFolder->getID());
     trackObjects = new coTUIToggleButton("TrackObjects", TrackObjectsTab->getID());
+
+    objectChoiceLabel = new coTUILabel("Objec:",TrackObjectsTab->getID());
+    objectChoice = new coTUIComboBox("Objects", TrackObjectsTab->getID());
+    bodyChoiceLabel = new coTUILabel("Body:",TrackObjectsTab->getID());
+    bodyChoice = new coTUIComboBox("Body", TrackObjectsTab->getID());
+    getOffset = new coTUIButton("get offset");
+    snap = new coTUIButton("snap");
+
     TrackObjectsTab->setPos(0, 0);
     trackObjects->setPos(0, 0);
+    objectChoiceLabel->setPos(1,0);
+    objectChoiceLabel->setPos(2,0);
+    objectChoice->setPos(1,1);
+    objectChoice->setPos(2,1);
+    getOffset->setPos(3,0);
+    snap->setPos(4,0);
+    objectChoice->setEventListener(this);
+    bodyChoice->setEventListener(this);
     trackObjects->setEventListener(this);
-    
+    getOffset->setEventListener(this);
+    snap->setEventListener(this);
+    for (size_t i = 0; i < names.size(); ++i)
+    {
+        objectChoice->addEntry(names[i]);
+    }
+    std::string bodyName = coCoviseConfig::getEntry("value","COVER.Plugin.TrackObjects.BodyName", "");
+    if(bodyName.length()==0 && Input::instance()->getNumBodies()>0)
+    {
+        bodyName = Input::instance()->getBody(0)->getName();
+    }
+    for (size_t i = 0; i < Input::instance()->getNumBodies(); ++i)
+    {
+        std::string name = Input::instance()->getBody(i)->getName();
+        bodyChoice->addEntry(name);
+        if(name == bodyName)
+        {
+            bodyChoice->setSelectedEntry(i);
+        }
+    }
+
+
     x=y=z=h=p=r=0.0;
-    
+
+    trackingBody = Input::instance()->getBody(bodyName);
+
     posX = new coTUIEditFloatField("posX", TrackObjectsTab->getID());
     posY = new coTUIEditFloatField("posY", TrackObjectsTab->getID());
     posZ = new coTUIEditFloatField("posZ", TrackObjectsTab->getID());
@@ -108,12 +189,18 @@ TrackObjects::~TrackObjects()
 
 void TrackObjects::preFrame()
 {    
-    osg::Matrix tmpMat;
-    static float angle = 0.0;
-    angle+=0.01;
-    tmpMat.makeRotate(angle,1,0,0);
-     cover->getObjectsXform()->setMatrix(tmpMat);
-     coVRCollaboration::instance()->SyncXform();
+    if(trackingBody)
+    {
+        osg::Matrix currentMat = trackingBody->getMat();
+        if(currentMat != oldMat)
+        {
+            oldMat = currentMat;
+            osg::Matrix tmpMat;
+            tmpMat = currentMat * currentObject->offset;
+            cover->getObjectsXform()->setMatrix(tmpMat);
+            coVRCollaboration::instance()->SyncXform();
+        }
+    }
 }
 
 void TrackObjects::menuEvent(coMenuItem *m)
@@ -129,7 +216,16 @@ void TrackObjects::tabletPressEvent(coTUIElement *tUIItem)
 
 void TrackObjects::tabletEvent(coTUIElement *tUIItem)
 {
-    if (tUIItem == posX)
+    if(tUIItem == objectChoice)
+    {
+        currentObject = TObjects[objectChoice->getSelectedEntry()];
+        updateTUI();
+    }
+    else if(tUIItem == bodyChoice)
+    {
+        trackingBody = Input::instance()->getBody(bodyChoice->getSelectedText());
+    }
+    else if (tUIItem == posX)
     {
         x = posX->getValue();
     }
@@ -141,7 +237,7 @@ void TrackObjects::tabletEvent(coTUIElement *tUIItem)
     {
         z = posZ->getValue();
     }
-    if (tUIItem == rotH)
+    else if (tUIItem == rotH)
     {
         h = rotH->getValue();
     }
@@ -153,7 +249,35 @@ void TrackObjects::tabletEvent(coTUIElement *tUIItem)
     {
         r = rotR->getValue();
     }
+    else if (tUIItem == getOffset)
+    {
+        if(trackingBody)
+        {
+            osg::Matrix invTB = osg::Matrix::inverse(trackingBody->getMat());
+            osg::Matrix tmpMat = cover->getObjectsXform()->getMatrix();
+            currentObject->offset = invTB * tmpMat;
+            currentObject->offsetCoord = currentObject->offset;
+            updateTUI();
+        }
+    }
+    else if (tUIItem == snap)
+    {
+        snapTo45Degrees(&currentObject->offset);
+        currentObject->offsetCoord = currentObject->offset;
+        updateTUI();
+    }
+
 }
 
+void TrackObjects::updateTUI()
+{
+
+    posX->setValue(currentObject->offsetCoord.xyz[0]);
+    posY->setValue(currentObject->offsetCoord.xyz[1]);
+    posZ->setValue(currentObject->offsetCoord.xyz[2]);
+    rotH->setValue(currentObject->offsetCoord.hpr[0]);
+    rotP->setValue(currentObject->offsetCoord.hpr[1]);
+    rotR->setValue(currentObject->offsetCoord.hpr[2]);
+}
 
 COVERPLUGIN(TrackObjects)
