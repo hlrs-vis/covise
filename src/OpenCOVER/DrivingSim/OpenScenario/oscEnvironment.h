e50323bb98512000e5c8a52e1371f1707ba1d96d
Matthias Kühr <mattkue@users.noreply.github.com>
2015-11-12 17:44:07 +0100

    add members: name and include

--- src/OpenCOVER/DrivingSim/OpenScenario/oscEnvironment.h
+++ src/OpenCOVER/DrivingSim/OpenScenario/oscEnvironment.h
@@ -11,10 +11,12 @@ version 2.1 or later, see lgpl-2.1.txt.
 #include <oscObjectVariable.h>
 #include <oscVariables.h>
 #include <oscHeader.h>
+#include <oscNameId.h>
 #include <oscTimeOfDay.h>
 #include <oscNamedObject.h>
 #include <oscWeather.h>
 #include <oscRoadConditions.h>
+#include <oscFile.h>
 
 namespace OpenScenario {
 
@@ -27,14 +29,18 @@ public:
     oscEnvironment()
     {
         OSC_OBJECT_ADD_MEMBER(header,"oscHeader");
+        OSC_OBJECT_ADD_MEMBER(name,"oscNameId");
         OSC_OBJECT_ADD_MEMBER(timeOfDay,"oscTimeOfDay");
         OSC_OBJECT_ADD_MEMBER(weather,"oscWeather");
         OSC_OBJECT_ADD_MEMBER(roadConditions,"oscRoadConditions");
+        OSC_OBJECT_ADD_MEMBER(include,"oscFile");
     };
     oscHeaderMember header;
+    oscNameIdMember name;
     oscTimeOfDayMember timeOfDay;
     oscWeatherMember weather;
     oscRoadConditionsMember roadConditions;
+    oscFileMember include;
 };
 
 typedef oscObjectVariable<oscEnvironment *> oscEnvironmentMember;
