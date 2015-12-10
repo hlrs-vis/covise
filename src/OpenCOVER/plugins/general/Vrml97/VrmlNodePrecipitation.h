97baf6f292d3e96d75ce04c7b14b28fa500c5f0a
Uwe Woessner <woessner@hlrs.de>
2015-12-10 14:37:17 +0100

    rain

---
+++ src/OpenCOVER/plugins/general/Vrml97/VrmlNodePrecipitation.h
@@ -0,0 +1,73 @@
+/* This file is part of COVISE.
+
+   You can use it under the terms of the GNU Lesser General Public License
+   version 2.1 or later, see lgpl-2.1.txt.
+
+ * License: LGPL 2+ */
+
+//
+//  Vrml 97 library
+//  Copyright (C) 2001 Uwe Woessner
+//
+//  %W% %G%
+//  VrmlNodePrecipitation.h
+
+#ifndef _VRMLNODEPrecipitation_
+#define _VRMLNODEPrecipitation_
+
+#include <util/coTypes.h>
+
+#include <vrml97/vrml/VrmlNode.h>
+#include <vrml97/vrml/VrmlSFBool.h>
+#include <vrml97/vrml/VrmlSFInt.h>
+#include <vrml97/vrml/VrmlSFFloat.h>
+#include <vrml97/vrml/VrmlSFVec3f.h>
+#include <vrml97/vrml/VrmlSFString.h>
+#include <vrml97/vrml/VrmlSFRotation.h>
+#include <vrml97/vrml/VrmlNodeChild.h>
+#include <vrml97/vrml/VrmlScene.h>
+#include <osgParticle/PrecipitationEffect>
+
+using namespace opencover;
+using namespace vrml;
+
+class VRML97COVEREXPORT VrmlNodePrecipitation : public VrmlNodeChild
+{
+
+public:
+    // Define the fields of Precipitation nodes
+    static VrmlNodeType *defineType(VrmlNodeType *t = 0);
+    virtual VrmlNodeType *nodeType() const;
+
+    VrmlNodePrecipitation(VrmlScene *scene = 0);
+    VrmlNodePrecipitation(const VrmlNodePrecipitation &n);
+    virtual ~VrmlNodePrecipitation();
+    virtual void addToScene(VrmlScene *s, const char *);
+
+    virtual VrmlNode *cloneMe() const;
+
+    virtual VrmlNodePrecipitation *toPrecipitation() const;
+
+    virtual ostream &printFields(ostream &os, int indent);
+
+    virtual void setField(const char *fieldName, const VrmlField &fieldValue);
+    const VrmlField *getField(const char *fieldName) const;
+
+    virtual void render(Viewer *);
+
+    bool isEnabled()
+    {
+        return d_enabled.get();
+    }
+    static void update();
+
+private:
+    // Fields
+    VrmlSFInt d_numPrecipitation;
+    VrmlSFFloat d_fraction_changed;
+
+    VrmlSFBool d_enabled;
+    VrmlSFBool d_loop;
+    osg::ref_ptr<osgParticle::PrecipitationEffect> precipitationEffect;
+};
+#endif //_VRMLNODEPrecipitation_
