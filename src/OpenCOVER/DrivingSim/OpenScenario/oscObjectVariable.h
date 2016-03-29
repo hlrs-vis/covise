ae8c23fe1d5d92536b8a843239aa7d044ef62fed
Matthias Kühr <mattkue@users.noreply.github.com>
2016-03-29 17:35:27 +0200

    use a base template class for oscObjectVariable...
    the functions in the classes oscObjectVariable... are all the same and
    are summarised in one file now

--- src/OpenCOVER/DrivingSim/OpenScenario/oscObjectVariable.h
+++ src/OpenCOVER/DrivingSim/OpenScenario/oscObjectVariable.h
@@ -9,100 +9,17 @@ version 2.1 or later, see lgpl-2.1.txt.
 #define OSC_OBJECT_VARIABLE_H
 
 #include "oscExport.h"
-#include "oscMemberValue.h"
 #include "oscMember.h"
-#include "oscFactories.h"
-
-#include <xercesc/dom/DOMDocument.hpp>
-#include <xercesc/dom/DOMElement.hpp>
-#include <xercesc/util/XMLString.hpp>
+#include "oscObjectVariableBase.h"
 
 
 namespace OpenScenario
 {
 
 template<typename T>
-class oscObjectVariable: public oscMember
+class OPENSCENARIOEXPORT oscObjectVariable : public oscObjectVariableBase<T, oscMember>
 {
-protected:
-    T valueT;
 
-public:
-    oscObjectVariable() ///< constructor
-    {
-        type = oscMemberValue::OBJECT;
-        valueT=NULL;
-    };
-
-    bool initialize(xercesc::DOMAttr *)
-    {
-        return false;
-    };
-
-    T operator->()
-    {
-        return valueT;
-    };
-
-    oscObjectBase* getObject() const
-    {
-        return valueT;
-    };
-
-    oscObjectBase* getGenerateObject()
-    {
-        if (!valueT)
-        {
-            oscObjectBase *obj = oscFactories::instance()->objectFactory->create(typeName);
-            if(obj)
-            {
-                oscMember *member = static_cast<oscMember *>((oscObjectVariable<T>*)this);
-                obj->initialize(owner->getBase(), owner, member, owner->getSource());
-                setValue(obj);
-            }
-        }
-
-        return valueT;
-    };
-
-    void setValue(oscObjectBase *t)
-    {
-        if (t != NULL)
-        {
-            valueT = dynamic_cast<T>(t);
-        }
-        else
-        {
-            valueT = NULL;
-        }
-    };
-
-    void deleteValue()
-    {
-        delete valueT;
-        valueT = NULL;
-    };
-
-    bool exists() const
-    {
-        return valueT!=NULL;
-    };
-
-    bool writeToDOM(xercesc::DOMElement *currentElement, xercesc::DOMDocument *document)
-    {
-        if(valueT !=NULL)
-        {
-            xercesc::DOMElement *memberElement = document->createElement(xercesc::XMLString::transcode(name.c_str()));
-            currentElement->appendChild(memberElement);
-            valueT->writeToDOM(memberElement,document);
-        }
-            return true;
-    };
-
-    oscMemberValue::MemberTypes getValueType() const
-    {
-        return type;
-    };
 };
 
 }
--- src/OpenCOVER/DrivingSim/OpenScenario/oscObjectVariableCatalog.h
+++ src/OpenCOVER/DrivingSim/OpenScenario/oscObjectVariableCatalog.h
@@ -10,89 +10,16 @@ version 2.1 or later, see lgpl-2.1.txt.
 
 #include "oscExport.h"
 #include "oscMemberCatalog.h"
-#include "oscMemberValue.h"
+#include "oscObjectVariableBase.h"
 
 
 namespace OpenScenario
 {
 
 template<typename T>
-class OPENSCENARIOEXPORT oscObjectVariableCatalog: public oscMemberCatalog
+class OPENSCENARIOEXPORT oscObjectVariableCatalog : public oscObjectVariableBase<T, oscMemberCatalog>
 {
-protected:
-    T valueT;
 
-public:
-    oscObjectVariableCatalog() ///< constructor
-    {
-        type = oscMemberValue::OBJECT;
-        valueT = NULL;
-    };
-
-    T operator->()
-    {
-        return valueT;
-    };
-
-    oscObjectBase* getObject() const
-    {
-        return valueT;
-    };
-
-    oscObjectBase* getGenerateObject()
-    {
-        if (!valueT)
-        {
-            oscObjectBase *obj = oscFactories::instance()->objectFactory->create(typeName);
-            if(obj)
-            {
-                oscMember *member = static_cast<oscMember *>((oscObjectVariable<T>*)this);
-                obj->initialize(owner->getBase(), owner, member, owner->getSource());
-                setValue(obj);
-            }
-        }
-
-        return valueT;
-    };
-
-    void setValue(oscObjectBase *t)
-    {
-        if (t != NULL)
-        {
-            valueT = dynamic_cast<T>(t);
-        }
-        else
-        {
-            valueT = NULL;
-        }
-    };
-
-    void deleteValue()
-    {
-        delete valueT;
-        valueT = NULL;
-    };
-
-    bool exists() const
-    {
-        return valueT != NULL;
-    };
-
-    bool writeToDOM(xercesc::DOMElement *currentElement, xercesc::DOMDocument *document)
-    {
-        if(valueT !=NULL)
-        {
-            xercesc::DOMElement *memberElement = document->createElement(xercesc::XMLString::transcode(name.c_str()));
-            currentElement->appendChild(memberElement);
-            valueT->writeToDOM(memberElement,document);
-        }
-        return true;
-    };
-
-    oscMemberValue::MemberTypes getValueType() const
-    {
-        return type;
-    };
 };
 
 }
