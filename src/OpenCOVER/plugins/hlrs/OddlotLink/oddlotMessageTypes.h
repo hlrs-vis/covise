a8f3be74b50eafd2b88ab7014fb84742f72a8c6c
Uwe Woessner <woessner@hlrs.de>
2016-04-20 14:06:36 +0200

    missing file

---
+++ src/OpenCOVER/plugins/hlrs/OddlotLink/oddlotMessageTypes.h
@@ -0,0 +1,8 @@
+#ifndef _MESSAGE_TYPES_H
+#define _MESSAGE_TYPES_H
+enum MessageTypes
+    {
+        MSG_GetHeight = 500, // this must be the first Message
+        MSG_GetMap = 501,
+    };
+#endif
