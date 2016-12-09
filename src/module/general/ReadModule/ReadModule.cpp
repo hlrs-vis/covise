aafb8fd45596c4edd419d3b7c373bd9f39948cea
Myriam Guedey <guedey@hlrs.de>
2016-12-09 11:28:20 +0100

    Rename ReadAssimp to ReadModule

---
+++ src/module/general/ReadModule/ReadModule.cpp
@@ -0,0 +1,72 @@
+/* This file is part of COVISE.
+
+   You can use it under the terms of the GNU Lesser General Public License
+   version 2.1 or later, see lgpl-2.1.txt.
+
+ * License: LGPL 2+ */
+
+// +++++++++++++++++++++++++++++++++++++++++
+// MODULE ReadModule
+//
+// This module reads polygon meshes in all formats supported by assimp
+//
+
+#include "ReadModule.h"
+#include <do/coDoData.h>
+#include <alg/coFeatureLines.h>
+
+#ifndef _WIN32
+#include <inttypes.h>
+#endif
+
+
+// Module set-up in Constructor
+ReadModule::ReadModule(int argc, char *argv[])
+    : coModule(argc, argv, "Read STL")
+{
+    // file browser parameter
+    p_filename = addFileBrowserParam("file_path", "Data file path");
+    p_filename->setValue("data/nofile.stl", "*.stl;*.STL/*");
+
+
+    // activate automatic FixUsg ?
+    p_triangulate = addBooleanParam("triangulate", "triangulate polygons");
+    p_triangulate->setValue(1);
+
+    // show feature lines and smooth surfaces (when p_removeDoubleVert is true)
+    p_joinVertices = addBooleanParam("joinVertices", "join identical vertices");
+    p_joinVertices->setValue(1);
+
+    // Output ports
+    p_polyOut = addOutputPort("mesh", "Polygons", "Polygons");
+    p_normOut = addOutputPort("Normals", "Vec3", "velocity data");
+    p_linesOut = addOutputPort("Feature_lines", "Lines", "Feature lines");
+
+    // to be added later for coloured binary files
+    p_colorOut = addOutputPort("colors", "RGBA", "color data");
+
+}
+
+ReadModule::~ReadModule()
+{
+}
+
+// param callback read header again after all changes
+void
+ReadModule::param(const char *paraName, bool inMapLoading)
+{
+    if (inMapLoading)
+        return;
+
+   // if (strcmp(paraName, p_filename->getName()) == 0)
+    //    readHeader();
+}
+
+int ReadModule::compute(const char *)
+{
+    int result = SUCCESS;
+    return result;
+}
+
+
+MODULE_MAIN(IO, ReadModule)
