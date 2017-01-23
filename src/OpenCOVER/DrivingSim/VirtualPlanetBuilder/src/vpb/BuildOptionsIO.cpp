c7b1e2772759c792cd169c2f20e2d9b0c9b2fa97
Uwe Woessner <woessner@hlrs.de>
2017-01-23 01:37:00 +0100

    windows compile fix

--- src/OpenCOVER/DrivingSim/VirtualPlanetBuilder/src/vpb/BuildOptionsIO.cpp
+++ src/OpenCOVER/DrivingSim/VirtualPlanetBuilder/src/vpb/BuildOptionsIO.cpp
@@ -564,7 +564,7 @@ static bool readLayerImageOptions(osgDB::InputStream &is, vpb::BuildOptions &bo)
     is >> size >> IS_BEGIN_BRACKET;
     for (unsigned int i = 0; i < size; ++i)
     {
-        vpb::ImageOptions *imageOptions = dynamic_cast<vpb::ImageOptions *>(is.readObject());
+        vpb::ImageOptions *imageOptions = dynamic_cast<vpb::ImageOptions *>(is.readObject().get());
         if (imageOptions)
             bo.setLayerImageOptions(i, imageOptions);
     }
