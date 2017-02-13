6a5a081be40ce4b9496e9c5f3bb68ce3f10a76b3
Martin Aumüller <aumuell@reserv.at>
2017-02-13 11:30:36 +0100

    -fabi-version=6 necessary for Visionaray and older compilers
    use just when Visionaray is involved

--- cmake/Using/UseVisionaray.cmake
+++ cmake/Using/UseVisionaray.cmake
@@ -45,6 +45,11 @@ MACRO(USE_VISIONARAY)
                 set(EXTRA_LIBS ${EXTRA_LIBS} ${CUDA_LIBRARIES})
             endif()
 
+            IF(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+                set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fabi-version=6")
+                set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fabi-version=6")
+            ENDIF()
+
             SET(VISIONARAY_USED TRUE)
         ENDIF(NOT VISIONARAY_USED)
     else(COVISE_USE_VISIONARAY)
