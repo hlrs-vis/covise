2c675c45c8a494c7e048798592fe958b928daa53
Uwe Woessner <woessner@hlrs.de>
2017-02-14 09:28:55 +0100

    more mappings

--- src/OpenCOVER/DrivingSim/OpenScenario/oscNameMapping.cpp
+++ src/OpenCOVER/DrivingSim/OpenScenario/oscNameMapping.cpp
@@ -13,12 +13,16 @@ nameMapping::nameMapping()
 	bm.insert(bm_type::value_type(parent_name("ByEntity", "Collision"), "CollisionByEntity"));
 	bm.insert(bm_type::value_type(parent_name("ConditionGroup", "Start"), "StartConditionGroup"));
 	bm.insert(bm_type::value_type(parent_name("Target", "TimeOfCollision"), "CollisionTarget"));
+	bm.insert(bm_type::value_type(parent_name("Target", "Speed"), "SpeedTarget"));
+	bm.insert(bm_type::value_type(parent_name("Target", "LaneChange"), "LaneChangeTarget"));
+	bm.insert(bm_type::value_type(parent_name("Target", "LaneOffset"), "LaneOffsetTarget"));
 	bm.insert(bm_type::value_type(parent_name("Parameter", "entryName"), "EntryParameter"));
 	bm.insert(bm_type::value_type(parent_name("Parameter", "ByValue"), "ConditionParameter"));
 	bm.insert(bm_type::value_type(parent_name("Parameter", "OSCGlobalAction"), "ActionParameter"));
+	bm.insert(bm_type::value_type(parent_name("Parameter", "OSCParameterAssignment"), "SetParameter"));
 	bm.insert(bm_type::value_type(parent_name("Waypoint", "OSCTrajectory"), "Vertex"));
 	bm.insert(bm_type::value_type(parent_name("Parameter", "execution"), "ExecutionParameter"));
-	bm.insert(bm_type::value_type(parent_name("Route", "OSCPosition"), "PosRoute"));
+	bm.insert(bm_type::value_type(parent_name("Route", "OSCPosition"), "PositionRoute"));
 	bm.insert(bm_type::value_type(parent_name("Longitudinal", "FollowTrajectory"), "LongitudinalParams"));
 	bm.insert(bm_type::value_type(parent_name("Lateral", "FollowTrajectory"), "LateralParams"));
 	bm.insert(bm_type::value_type(parent_name("Signal", "ByState"), "SignalState"));
@@ -27,9 +31,17 @@ nameMapping::nameMapping()
 	bm.insert(bm_type::value_type(parent_name("Description", "OSCPedestrianController"), "Description"));
 	bm.insert(bm_type::value_type(parent_name("Dynamics", "Speed"), "SpeedDynamics"));
 	bm.insert(bm_type::value_type(parent_name("Dynamics", "LaneChange"), "LaneChangeDynamics"));
-	bm.insert(bm_type::value_type(parent_name("Dynamics", "LaneOffset"), "LaneOffsetDynamics")); 
-	bm.insert(bm_type::value_type(parent_name("Description", "OSCPedestrianController"), "Description"));
-	bm.insert(bm_type::value_type(parent_name("Description", "OSCPedestrianController"), "Description"));
+	bm.insert(bm_type::value_type(parent_name("Dynamics", "LaneOffset"), "LaneOffsetDynamics"));
+	bm.insert(bm_type::value_type(parent_name("Entity", "OSCGlobalAction"), "ActionEntity"));
+	bm.insert(bm_type::value_type(parent_name("Add", "Rule"), "AddRule"));
+	bm.insert(bm_type::value_type(parent_name("TimeOfDay", "OSCEnvironment"), "EnvironmentTimeOfDay"));
+	bm.insert(bm_type::value_type(parent_name("Speed", "EntityCondition"), "ConditionSpeed"));
+	bm.insert(bm_type::value_type(parent_name("Controller", "OSCPrivateAction"), "ActionController"));
+	bm.insert(bm_type::value_type(parent_name("Distance", "EntityCondition"), "ConditionDistance"));
+	bm.insert(bm_type::value_type(parent_name("Relative", "LaneChangeTarget"), "LaneChangeTargetRelative"));
+	bm.insert(bm_type::value_type(parent_name("Relative", "LaneOffsetTarget"), "LaneOffsetTargetRelative"));
+	bm.insert(bm_type::value_type(parent_name("Position", "PositionRoute"), "RoutePosition"));
+	
 	
 	
 };
