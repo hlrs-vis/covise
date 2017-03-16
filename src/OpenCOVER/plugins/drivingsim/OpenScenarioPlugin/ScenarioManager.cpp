541641792fdd6aa01bd79fac24a99cb3966df071
Uwe Woessner <woessner@hlrs.de>
2017-03-16 21:25:01 +0100

    beautify

--- src/OpenCOVER/plugins/drivingsim/OpenScenarioPlugin/ScenarioManager.cpp
+++ src/OpenCOVER/plugins/drivingsim/OpenScenarioPlugin/ScenarioManager.cpp
@@ -1,20 +1,35 @@
 #include "ScenarioManager.h"
 #include <vector>
 
-ScenarioManager::ScenarioManager(){}
-void ScenarioManager::setNumberOfActs(int numberOfActs_temp){
-numberOfActs = numberOfActs_temp;}
-void ScenarioManager::setNumberOfEntities(int numberOfEntities_temp){
-numberOfEntities = numberOfEntities_temp;}
+ScenarioManager::ScenarioManager()
+{
+}
+void ScenarioManager::setNumberOfActs(int numberOfActs_temp)
+{
+	numberOfActs = numberOfActs_temp;
+}
+void ScenarioManager::setNumberOfEntities(int numberOfEntities_temp)
+{
+	numberOfEntities = numberOfEntities_temp;
+}
 /*list<Entity*> ScenarioManager::getEntityList(){
 return entityList;}
 list<Act*> ScenarioManager::getActList(){
 return actList;}*/
-Entity* ScenarioManager::getEntityByName(string entityName){
-	for(list<Entity*>::iterator entity_iter = entityList.begin(); entity_iter != entityList.end(); entity_iter++){
-		if((*entity_iter)->getName()!=entityName){continue;}
-	    else{
-		return (*entity_iter);}}}
+Entity* ScenarioManager::getEntityByName(string entityName)
+{
+	for (list<Entity*>::iterator entity_iter = entityList.begin(); entity_iter != entityList.end(); entity_iter++)
+	{
+		if ((*entity_iter)->getName() != entityName)
+		{
+			continue;
+		}
+		else
+		{
+			return (*entity_iter);
+		}
+	}
+}
 
 
 
