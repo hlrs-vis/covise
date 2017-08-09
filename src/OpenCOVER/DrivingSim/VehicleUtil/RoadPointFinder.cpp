1bed4ff6cbf8a77575f19a6bdfff2fb7d1a8124d
Kevin Granz <hpckgran@visff2.hlrs.de>
2017-08-09 05:16:18 +0200

    updated road height detection

--- src/OpenCOVER/DrivingSim/VehicleUtil/RoadPointFinder.cpp
+++ src/OpenCOVER/DrivingSim/VehicleUtil/RoadPointFinder.cpp
@@ -1,4 +1,5 @@
 #include "RoadPointFinder.h"
+
 #include <unistd.h>
 
 RoadPointFinder::RoadPointFinder()
@@ -7,9 +8,19 @@ RoadPointFinder::RoadPointFinder()
 #else
     : XenomaiTask::XenomaiTask("RoadPointFinderTask", 0, 99, T_FPU | T_CPU(5))
 #endif
+	, roadMutex("rpf_road_mutex")
+	, positionMutex("rpf_position_mutex")
+	, longPosMutex("rpf_longpos_mutex")
+	, overruns(0)
 {
 	runTask = true;
 	taskFinished = false;
+	
+	for(int i = 0; i < 12; i++)
+	{
+		currentRoad[i] = NULL;
+		roadPoint[i] = osg::Vec3d();
+	}
 	
 	start();
 }
@@ -36,16 +47,61 @@ RoadPointFinder::~RoadPointFinder()
 
 void RoadPointFinder::run()
 {
-	std::cerr << "Starting RoadPointFinder..." << std::endl;
-	rt_task_set_periodic(NULL, TM_NOW, rt_timer_ns2ticks(1000000));
+	std::cout << "Starting RoadPointFinder..." << std::endl;
+	rt_task_set_periodic(NULL, TM_NOW, rt_timer_ns2ticks(3000000));
 	
+	while(loadingRoad)
+	{
+	}
+	
+	std::cerr << "RoadPointFinder is done waiting" << std::endl;
 	
 	while (runTask)
     {
 		if (overruns != 0)
 		{
-			std::cerr << "SteerCom::run(): overruns: " << overruns << std::endl;
+			std::cerr << "RoadPointFinder::run(): overruns: " << overruns << std::endl;
 			overruns=0;
 		}
+		
+		//std::cout << "RoadPointFinder running" << std::endl;
+		
+		for(int i = 0; i < 12; i++)
+		{
+			std::cout << "currentRoad " << i << ": " << currentRoad[i] << std::endl;
+			//std::cout << "currentLongPos " << i << ": " << currentLongPos[i] << std::endl;
+			
+			positionMutex.acquire(1000000);
+			Vector3D searchInVec(roadPoint[i].x(), roadPoint[i].y(), roadPoint[i].z());//1086.83,1507.96,10.4413);
+			positionMutex.release();
+			
+			//std::cout << "search in vector " << i << ": " << roadPoint[i].x() << roadPoint[i].y() << roadPoint[i].z() << std::endl;
+			
+			roadMutex.acquire(1000000);
+			longPosMutex.acquire(1000000);
+			//std::cout << "fl1 in" << std:: endl << "pointx" << tempVec.x() << std::endl << "pointy" << tempVec.y() << std::endl << "pointz" << tempVec.z() << std::endl;
+			Vector2D searchOutVec = RoadSystem::Instance()->searchPosition(searchInVec, currentRoad[i], currentLongPos[i]);
+			
+			
+			
+			std::cout << "searchOutVec " << i << ": " << searchOutVec.x() << " " << searchOutVec.y() << std::endl;
+			
+			//std::cout << "fl2 out" << std::endl << "pointx" << searchOutVec.x() << std::endl << "pointy" << searchOutVec.y() << std::endl;
+			currentLongPos[i] = searchOutVec.x();
+			longPosMutex.release();
+			
+			/*RoadPoint point = currentRoad[i]->getRoadPoint(searchOutVec.x(), searchOutVec.y());
+			
+			std::cout << "pointx" << point.x() << std::endl << "pointy" << point.y() << std::endl << "pointz" << point.z() << std::endl;*/
+			
+			roadMutex.release();
+			/*if (isnan(point.x()))
+			{
+				std::cout << "tire point " << i << " left road!" << std::endl;
+			}*/
+			
+		}
+		
+		rt_task_wait_period(&overruns);
 	}
 }
