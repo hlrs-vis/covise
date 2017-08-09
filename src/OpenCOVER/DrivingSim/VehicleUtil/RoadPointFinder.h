7367cd8d172896d50b499b77ef2d8496b2c7a4ad
Kevin Granz <hpckgran@visff2.hlrs.de>
2017-08-09 05:16:32 +0200

    updated road height detection

--- src/OpenCOVER/DrivingSim/VehicleUtil/RoadPointFinder.h
+++ src/OpenCOVER/DrivingSim/VehicleUtil/RoadPointFinder.h
@@ -2,16 +2,44 @@
 #define __RoadPointFinder_h
 
 #include "XenomaiTask.h"
-#include "CanOpenController.h"
-#include "XenomaiSteeringWheel.h"
+#include "XenomaiMutex.h"
 
-class RoadPointFinder : public XenomaiTask
+#include "RoadSystem/RoadSystem.h"
+
+class VEHICLEUTILEXPORT RoadPointFinder : public XenomaiTask
 {
 public:
 	RoadPointFinder();
 	~RoadPointFinder();
 	
+	void setRoad(Road *&road, int contactPointNumber);
+	Road* getRoad(int contactPointNumber);
 	
+	void setPoint(osg::Vec3d inVector, int contactPointNumber);
+	
+	double getLongPos(int contactPointNumber);
+	void setLongPos(double inLongPos, int contactPointNumber);
+	
+	void checkLoadingRoads(bool loadingState);
+	
+	XenomaiMutex roadMutex;
+	XenomaiMutex positionMutex;
+	XenomaiMutex longPosMutex;
+	
+	XenomaiMutex &getRoadMutex()
+    {
+        return roadMutex;
+    }
+    
+    XenomaiMutex &getPositionMutex()
+    {
+        return positionMutex;
+    }
+    
+    XenomaiMutex &getLongPosMutex()
+    {
+        return longPosMutex;
+    }
 	
 protected:
 	void run();
@@ -19,8 +47,54 @@ protected:
 	static const RTIME period = 1000000;
 	bool runTask;
 	bool taskFinished;
+	bool loadingRoad;
+	
+	Road* currentRoad[12];
+    double currentLongPos[12];
+	
+	osg::Vec3d roadPoint[12];
 	
 	
 };
+
+inline void RoadPointFinder::setRoad(Road *&road, int contactPointNumber)
+{
+	
+	currentRoad[contactPointNumber] = road;
+	
+}
+
+inline Road* RoadPointFinder::getRoad(int contactPointNumber)
+{
+	
+	return currentRoad[contactPointNumber];
+	
+}
+
+inline void RoadPointFinder::setPoint(osg::Vec3d inVector, int contactPointNumber)
+{
+	
+	roadPoint[contactPointNumber] = inVector;
+	
+}
+
+inline void RoadPointFinder::setLongPos(double inLongPos, int contactPointNumber)
+{
+	
+	currentLongPos[contactPointNumber] = inLongPos;
+	
+}
+
+inline double RoadPointFinder::getLongPos(int contactPointNumber)
+{
+	
+	return currentLongPos[contactPointNumber];
+	
+}
+
+inline void RoadPointFinder::checkLoadingRoads(bool loadingState)
+{
+	loadingRoad = loadingState;
+}
 
 #endif
