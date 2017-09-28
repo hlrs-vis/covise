b2649114fdd7885af93520afdf1e3e520a02e7ba
Martin Aumüller <aumuell@reserv.at>
2017-09-28 12:17:39 +0200

    fix linking of Multitouch plugin

---
+++ src/OpenCOVER/plugins/hlrs/Multitouch/MultitouchPluginPlugin.cpp
@@ -0,0 +1,26 @@
+/* This file is part of COVISE.
+
+   You can use it under the terms of the GNU Lesser General Public License
+   version 2.1 or later, see lgpl-2.1.txt.
+
+ * License: LGPL 2+ */
+
+/****************************************************************************\ 
+**                                                            (C)2012 HLRS  **
+**                                                                          **
+** Description: Multitouch Plugin											**
+**                                                                          **
+**                                                                          **
+** Author:																	**
+**         Jens Dehlke														**
+**                                                                          **
+** History:  								                                **
+** Feb-13  v1.1																** 
+** Sep-12  v1.0	    				       		                            **
+**                                                                          **
+**                                                                          **
+\****************************************************************************/
+
+#include "MultitouchPlugin.h"
+
+COVERPLUGIN(MultitouchPlugin)
