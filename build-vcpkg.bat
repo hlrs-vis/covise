158d1f17969022a834c5fac844c271e58a6015fb
Martin Aumüller <aumuell@reserv.at>
2017-11-05 22:27:59 +0100

    configurable triplet

--- build-vcpkg.bat
+++ build-vcpkg.bat
@@ -6,6 +6,8 @@ if "%VCPKG_ROOT%" EQU "" (
 )
 
 set "vcdir=%VCPKG_ROOT%"
+set VCPKG_DEFAULT_TRIPLET=x64-windows
+set "generator=Visual Studio 15 2017 Win64"
 
 set EXTERNLIBS=
 set ARCHSUFFIX=vcpkg
@@ -16,8 +18,8 @@ set "COVISEDESTDIR=%CD%"
 REM choco -y install cmake --installargs 'ADD_CMAKE_TO_PATH=""System""'
 REM choco -y install git swig winflexbison
 
-%vc% install assimp:x64-windows boost:x64-windows curl:x64-windows freeglut:x64-windows glew:x64-windows giflib:x64-windows libpng:x64-windows qt5:x64-windows tiff:x64-windows xerces-c:x64-windows zlib:x64-windows libjpeg-turbo:x64-windows vtk:x64-windows  
-%vc% install pthreads:x64-windows tbb:x64-windows
+%vc% install assimp boost curl freeglut glew giflib libpng qt5 tiff xerces-c zlib libjpeg-turbo vtk  
+%vc% install pthreads tbb libmicrohttpd python3
 
 %vc% list
 REM %vc% integrate project
@@ -26,15 +28,15 @@ mkdir %ARCHSUFFIX%
 cd %ARCHSUFFIX%
 mkdir build.covise
 cd build.covise
-cmake -G "Visual Studio 15 2017 Win64" "-DCMAKE_TOOLCHAIN_FILE=%vcdir%\scripts\buildsystems\vcpkg.cmake" -DCOVISE_BUILD_RENDERER:BOOL=OFF ../..
+cmake -G "%generator%" "-DCMAKE_TOOLCHAIN_FILE=%vcdir%\scripts\buildsystems\vcpkg.cmake" -DCOVISE_BUILD_RENDERER:BOOL=OFF ../..
 msbuild /m covise.sln
 
-"%vc%" install osg:x64-windows
+"%vc%" install osg:%vct%
 
 cd ..
 mkdir build.cover
 cd build.cover
-cmake -G "Visual Studio 15 2017 Win64" "-DCMAKE_TOOLCHAIN_FILE=%vcdir%\scripts\buildsystems\vcpkg.cmake" ../../src/OpenCOVER
+cmake -G "%generator%" "-DCMAKE_TOOLCHAIN_FILE=%vcdir%\scripts\buildsystems\vcpkg.cmake" ../../src/OpenCOVER
 msbuild /m OpenCOVER.sln
 
 :END
