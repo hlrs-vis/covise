ec38d75cc6a51a31c23f7e9a1780d4babbcf99ac
Uwe Woessner <woessner@hlrs.de>
2018-01-31 00:33:13 +0100

    added Timestep

--- src/tools/captureART/captureART.cpp
+++ src/tools/captureART/captureART.cpp
@@ -14,6 +14,7 @@
 #ifndef NOMINMAX
 #define NOMINMAX
 #endif
+#include <sys\timeb.h>
 
 #include "captureART.h"
 DTrackSDK *dt; ///ART DTrack SDK class
@@ -51,6 +52,7 @@ int main(int argc, char **argv)
 	if (!dt->receive())
 		cout << "Error receiving data!" << endl;
 
+	struct timeb frameTime;
 
 	dt->startMeasurement();
 
@@ -83,8 +85,9 @@ int main(int argc, char **argv)
 		}
 		for (int i = 0; i < dt->getNumBody(); i++)
 		{
+			ftime(&frameTime);
 			DTrack_Body_Type_d *b = dt->getBody(i);
-			fprintf(fp, "body %d: %f %f %f %f %f %f %f %f %f %f %f %f\n",i, b->loc[0], b->loc[1], b->loc[2], b->rot[0], b->rot[1], b->rot[2], b->rot[3], b->rot[4], b->rot[5], b->rot[6], b->rot[7], b->rot[8]);
+			fprintf(fp, "body %lld; %lld, %d: %f %f %f %f %f %f %f %f %f %f %f %f\n", frameTime.time, frameTime.millitm, i, b->loc[0], b->loc[1], b->loc[2], b->rot[0], b->rot[1], b->rot[2], b->rot[3], b->rot[4], b->rot[5], b->rot[6], b->rot[7], b->rot[8]);
 		}
 	}
 
