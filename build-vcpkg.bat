64eda5cb73c24b18e968ac411b0c84ffd9db52fb
Martin Aumüller <aumuell@reserv.at>
2018-02-07 09:24:10 +0100

    thin out dependencies, better support for ARCHSUFFIXes

--- build-vcpkg.bat
+++ build-vcpkg.bat
@@ -14,40 +14,61 @@ set VCPKG_DEFAULT_TRIPLET=x64-windows
 set "generator=Visual Studio 15 2017 Win64"
 
 set EXTERNLIBS=
-set ARCHSUFFIX=vcpkg
+set ARCHSUFFIX=vcpkgopt
+set cfg=RelWithDebInfo
 set "vc=%vcdir%\vcpkg"
 set "COVISEDIR=%CD%"
 set "COVISEDESTDIR=%CD%"
 
+if "%ARCHSUFFIX%" == "vcpkg" (
+    set cfg=Debug
+)
+
+set "verb=/consoleloggerparameters:Summary;Verbosity=minimal;ForceNoAlign;ShowTimestamp"
+set "par=/m"
+
 REM choco -y install cmake --installargs 'ADD_CMAKE_TO_PATH=""System""'
 REM choco -y install git swig winflexbison
 
-"%vc%" install assimp boost curl freeglut glew giflib libpng qt5 tiff xerces-c zlib libjpeg-turbo vtk  
+"%vc%" install assimp curl freeglut glew giflib libpng tiff xerces-c zlib libjpeg-turbo
+"%vc%" install vtk gdcm2 hdf5[cpp]
 "%vc%" install pthreads tbb libmicrohttpd python3
 "%vc%" install osg
-"%vc%" install ffmpeg opencv gdal
-"%vc%" install proj4
+"%vc%" install ffmpeg opencv
+"%vc%" install proj4 gdal libgeotiff
+"%vc%" install boost-asio boost-bimap boost-chrono boost-date-time boost-mpl boost-program-options boost-serialization boost-signals2 boost-smart-ptr boost-uuid boost-variant boost-interprocess
+"%vc%" install qt5-tools qt5-base qt5-svg
+"%vc%" install openvr
+"%vc%" install pcl
+REM "%vc%" install qt5
 
 %vc% list
 REM %vc% integrate project
 
-mkdir %ARCHSUFFIX%
-cd %ARCHSUFFIX%
+mkdir vcpkg
+cd vcpkg
+
+goto COVER
 
 :COVISE
 mkdir build.covise
 cd build.covise
 cmake -G "%generator%" "-DCMAKE_TOOLCHAIN_FILE=%vcdir%\scripts\buildsystems\vcpkg.cmake" -DCOVISE_BUILD_RENDERER:BOOL=OFF ../..
-msbuild /m covise.sln
+if errorlevel 1 goto UPDIR
+msbuild /m covise.sln /p:Configuration=%cfg% %verb% %par%
+if errorlevel 1 goto UPDIR
 cd ..
 
 :COVER
-
 mkdir build.cover
 cd build.cover
 cmake -G "%generator%" "-DCMAKE_TOOLCHAIN_FILE=%vcdir%\scripts\buildsystems\vcpkg.cmake" ../../src/OpenCOVER
-msbuild /m OpenCOVER.sln
+if errorlevel 1 goto UPDIR
+msbuild /m OpenCOVER.sln /p:Configuration=%cfg% %verb% %par%
+if errorlevel 1 goto UPDIR
+
+:UPDIR
+cd ..
 cd ..
 
-cd ..
 :END
