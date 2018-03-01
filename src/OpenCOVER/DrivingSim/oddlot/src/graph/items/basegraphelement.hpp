2d1ebc6c1c73e21f25030f4f86b25946cf698a63
Martin Aumüller <aumuell@reserv.at>
2018-03-01 15:19:33 +0100

    better

--- src/OpenCOVER/DrivingSim/oddlot/src/graph/items/basegraphelement.hpp
+++ src/OpenCOVER/DrivingSim/oddlot/src/graph/items/basegraphelement.hpp
@@ -137,8 +137,8 @@ private:
 	BaseGraphElement &operator=(const BaseGraphElement &); /* not allowed */
 
 	void init();
-    QGraphicsItem *This();
-    const QGraphicsItem *This() const;
+    T *This();
+    const T *This() const;
 
 
 	//################//
