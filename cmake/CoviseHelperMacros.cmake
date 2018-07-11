fcc313229adc5e17598c0c554539d6d98bf60db9
Martin Aumüller <aumuell@reserv.at>
2018-07-11 10:34:37 +0200

    remove unused macros

--- cmake/CoviseHelperMacros.cmake
+++ cmake/CoviseHelperMacros.cmake
@@ -51,10 +51,6 @@
 #
 #  --------------------------------------------------------------------------------------
 #
-#  COVISE_ADJUST_LIB_VARS (basename)
-#    - this is a support macro for cmake find modules (it simply sets/adjusts library-variables
-#      depending on what was previously found for example by FIND_LIBRARY()
-#
 #  COVISE_RESET (varname)
 #    - sets a variable to "not found"
 #
@@ -88,60 +84,6 @@
 # @author Blasius Czink
 #
 
-# Macro to adjust library-variables
-MACRO(COVISE_ADJUST_LIB_VARS basename)
-  # if only the release version was found, set the debug variable also to the release version
-  IF (${basename}_LIBRARY_RELEASE AND NOT ${basename}_LIBRARY_DEBUG)
-    SET(${basename}_LIBRARY_DEBUG ${${basename}_LIBRARY_RELEASE} CACHE FILEPATH "${basename}_LIBRARY_DEBUG" FORCE)
-    SET(${basename}_LIBRARY       ${${basename}_LIBRARY_RELEASE} CACHE FILEPATH "${basename}_LIBRARY" FORCE)
-    SET(${basename}_LIBRARIES     ${${basename}_LIBRARY_RELEASE} CACHE FILEPATH "${basename}_LIBRARIES" FORCE)
-  ENDIF (${basename}_LIBRARY_RELEASE AND NOT ${basename}_LIBRARY_DEBUG)
-
-  # if only the debug version was found, set the release variable also to the debug version
-  IF (${basename}_LIBRARY_DEBUG AND NOT ${basename}_LIBRARY_RELEASE)
-    SET(${basename}_LIBRARY_RELEASE ${${basename}_LIBRARY_DEBUG} CACHE FILEPATH "${basename}_LIBRARY_RELEASE" FORCE)
-    SET(${basename}_LIBRARY         ${${basename}_LIBRARY_DEBUG} CACHE FILEPATH "${basename}_LIBRARY" FORCE)
-    SET(${basename}_LIBRARIES       ${${basename}_LIBRARY_DEBUG} CACHE FILEPATH "${basename}_LIBRARIES" FORCE)
-  ENDIF (${basename}_LIBRARY_DEBUG AND NOT ${basename}_LIBRARY_RELEASE)
-  
-  # if both are set...
-  IF (${basename}_LIBRARY_DEBUG AND ${basename}_LIBRARY_RELEASE)
-    # if the generator supports configuration types then set
-    # optimized and debug libraries, or if the CMAKE_BUILD_TYPE has a value
-    IF (CMAKE_CONFIGURATION_TYPES OR CMAKE_BUILD_TYPE)
-      SET(${basename}_LIBRARY
-          optimized ${${basename}_LIBRARY_RELEASE}
-          debug     ${${basename}_LIBRARY_DEBUG}
-      )
-    ELSE(CMAKE_CONFIGURATION_TYPES OR CMAKE_BUILD_TYPE)
-      # if there are no configuration types and CMAKE_BUILD_TYPE has no value
-      # then just use the release libraries
-      SET(${basename}_LIBRARY ${${basename}_LIBRARY_RELEASE} )
-    ENDIF(CMAKE_CONFIGURATION_TYPES OR CMAKE_BUILD_TYPE)
-
-    SET(${basename}_LIBRARIES
-        optimized ${${basename}_LIBRARY_RELEASE}
-        debug ${${basename}_LIBRARY_DEBUG}
-        CACHE FILEPATH "${basename}_LIBRARIES" FORCE
-    )
-    
-  ENDIF (${basename}_LIBRARY_DEBUG AND ${basename}_LIBRARY_RELEASE)
-
-  SET(${basename}_LIBRARY "${${basename}_LIBRARY}" CACHE FILEPATH "The ${basename} library" FORCE)
-  # do not show those in normal view
-  MARK_AS_ADVANCED(FORCE
-    ${basename}_LIBRARY
-    ${basename}_LIBRARY_RELEASE
-    ${basename}_LIBRARY_DEBUG
-    ${basename}_LIBRARIES
-  )
-ENDMACRO(COVISE_ADJUST_LIB_VARS)
-
-
-# helper to reset variables to ...-NOTFOUND so that the FIND...-macros run again
-MACRO(COVISE_RESET varname)
-  SET (${varname} "${varname}-NOTFOUND")
-ENDMACRO(COVISE_RESET)
 
 # helper to dump the lib-values to a simple text-file
 MACRO(COVISE_DUMP_LIB_SETUP basename)
