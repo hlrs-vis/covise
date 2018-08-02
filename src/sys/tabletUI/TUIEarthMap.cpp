062c359112ce706e122efba4bc38c63d287ebecf
Nora Xu Tianyi <hpcnoxux@visff4.hlrs.de>
2018-08-02 19:10:04 +0200

    split one qml into three, added toolbar, center heli bug imposed

--- src/sys/tabletUI/TUIEarthMap.cpp
+++ src/sys/tabletUI/TUIEarthMap.cpp
@@ -59,10 +59,8 @@ TUIEarthMap::TUIEarthMap(int id, int type, QWidget *w, int parent, QString name)
     quickView->engine()->rootContext()->setContextProperty("size", geopath.path().size());
 
     quickView->engine()->addImportPath(QString(":/imports"));
-    //quickView->setSource(QUrl(QString("qrc:///mapviewer.qml")));
-    quickView->setSource(QUrl(QString("mapviewer.qml")));
+    quickView->setSource(QUrl(QString("qrc:///mapviewer.qml")));
     
-
     quickView->show();
     QObject *item = quickView->rootObject();
     Q_ASSERT(item);
@@ -155,7 +153,7 @@ void TUIEarthMap::setValue(TabletValue type, covise::TokenBuffer &tb)
         tb >> minHeight;
         tb >> maxHeight;
 	
-	//round up maxHeight, minHeight
+	//round up maxHeight, round down minHeight
 	int max, min;
 	int count = 0;
 	max = (int) maxHeight;
@@ -175,15 +173,18 @@ void TUIEarthMap::setValue(TabletValue type, covise::TokenBuffer &tb)
 	}
 	count = 0;
 	
-	while(min >= 10){
-		min = min / 10;
-		count ++;
-	}
+	if(min < 100){
+		min = 0;
+	}else{
+		while(min >= 10){
+			min = min / 10;
+			count ++;
+		}
 	
-	for(int i = 0; i < count; i++){
-		min = min * 10;
+		for(int i = 0; i < count; i++){
+			min = min * 10;
+		}
 	}
-	
 	
 	QVariant returnedValue;
         QMetaObject::invokeMethod(quickView->rootObject(), "updateHeightMinMax",
