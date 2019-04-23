def464961a802b29ac2242a3852db83b5e47cb36
Dennis Grieger <dennis.grieger@hlrs.de>
2019-04-23 18:11:31 +0200

    fix

--- src/OpenCOVER/cover/OpenCOVER.cpp
+++ src/OpenCOVER/cover/OpenCOVER.cpp
@@ -850,7 +850,7 @@ bool OpenCOVER::init()
     //connect to covise vrb
 	if (loadCovisePlugin && coCommandLine::argv(0) != "-c")
     {
-
+		cerr << "___AAAAAAA_____" << coCommandLine::argv(0) << endl;
 		char * coviseModuleID = coCommandLine::argv(4);
         char *ipAdress = coCommandLine::argv(5);
 		cerr << "I am local master " << coviseModuleID << ", my ip is " << ipAdress << endl;
