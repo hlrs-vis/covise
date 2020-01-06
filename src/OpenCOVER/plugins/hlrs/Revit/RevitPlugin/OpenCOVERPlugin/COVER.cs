6152cd4a7c9ccb9c08a50bd305c68ed8bea5c208
hpcwoess <woessner@hlrs.de>
2020-01-06 22:52:16 +0100

    only send non template views

--- src/OpenCOVER/plugins/hlrs/Revit/RevitPlugin/OpenCOVERPlugin/COVER.cs
+++ src/OpenCOVER/plugins/hlrs/Revit/RevitPlugin/OpenCOVERPlugin/COVER.cs
@@ -1230,7 +1230,7 @@ namespace OpenCOVERPlugin
         private void sendViewpoint(Autodesk.Revit.DB.Element elem)
         {
             Autodesk.Revit.DB.View view = (Autodesk.Revit.DB.View)elem;
-            if (view is Autodesk.Revit.DB.View3D)
+            if (view is Autodesk.Revit.DB.View3D && view.IsTemplate != true)
             {
                 Autodesk.Revit.DB.View3D v3d = (Autodesk.Revit.DB.View3D)view;
                 MessageBuffer mb = new MessageBuffer();
