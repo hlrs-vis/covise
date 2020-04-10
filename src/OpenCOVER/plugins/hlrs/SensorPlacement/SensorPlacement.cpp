90ee4b9693029f675fe6209e45dd5772f27ac556
MaEppl <matthias@msk-epple.de>
2020-04-10 09:14:21 +0200

    New Plugin for Sensor Placement

---
+++ src/OpenCOVER/plugins/hlrs/SensorPlacement/SensorPlacement.cpp
@@ -0,0 +1,1 @@
+#include "SensorPlacement.h"
---
+++ src/OpenCOVER/plugins/hlrs/SensorPlacement/SensorPlacement.h
@@ -0,0 +1,24 @@
+/* This file is part of COVISE.
+
+   You can use it under the terms of the GNU Lesser General Public License
+   version 2.1 or later, see lgpl-2.1.txt.
+
+ * License: LGPL 2+ */
+
+#pragma once
+/****************************************************************************\
+ **                                                            (C)2020 HLRS  **
+ **                                                                          **
+ ** Description: Camera position and orientation optimization                **
+ **                                                                          **
+ **                                                                          **
+ ** Author: Matthias Epple	                                                 **
+ **                                                                          **
+ ** History:  								                                 **
+ ** April 2020  v1	    				       		                         **
+ **                                                                          **
+ **                                                                          **
+\****************************************************************************/
+#include <config/CoviseConfig.h>
+#include <cover/coVRMSController.h>
+
