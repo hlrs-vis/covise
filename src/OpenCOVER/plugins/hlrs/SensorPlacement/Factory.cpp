ea705af21f2ccff80d6442f3a9b24ded273e8220
MaEppl <matthias@msk-epple.de>
2020-05-29 16:56:34 +0200

    move Factorx declaration to cpp-file

---
+++ src/OpenCOVER/plugins/hlrs/SensorPlacement/Factory.cpp
@@ -0,0 +1,31 @@
+#include "Factory.h"
+
+std::unique_ptr<SensorPosition> createSensor(SensorType sensor, osg::Matrix matrix)
+{
+    if(sensor == SensorType::Camera){
+        return myHelpers::make_unique<Camera>(matrix);
+    }
+    else{
+        assert(false);
+    }
+    return std::unique_ptr<SensorPosition>();
+}
+
+std::unique_ptr<Zone> createZone(ZoneType zone)
+{
+    osg::Matrix position;
+    position.setTrans(8,8,8);
+
+    if(zone == ZoneType::ROIzone){
+        
+        return myHelpers::make_unique<SafetyZone>(position);
+    }
+    else if(zone == ZoneType::SensorZone){
+
+        return myHelpers::make_unique<SensorZone>(position);
+    }
+    else{
+        assert(false);
+    }
+    return std::unique_ptr<Zone>();
+}
--- src/OpenCOVER/plugins/hlrs/SensorPlacement/Factory.h
+++ src/OpenCOVER/plugins/hlrs/SensorPlacement/Factory.h
@@ -9,40 +9,12 @@ enum class SensorType
     Camera = 0,
 };
 
-std::unique_ptr<SensorPosition> createSensor(SensorType sensor)
-{
-    if(sensor == SensorType::Camera){
-        osg::Matrix position;
-        position.setTrans(10,10,10);
-        return myHelpers::make_unique<Camera>(position);
-    }
-    else{
-        assert(false);
-    }
-    return std::unique_ptr<SensorPosition>();
-}
-
 enum class ZoneType
 {
     ROIzone = 0,
     SensorZone = 1
 };
 
-std::unique_ptr<Zone> createZone(ZoneType zone)
-{
-    osg::Matrix position;
-    position.setTrans(8,8,8);
+std::unique_ptr<SensorPosition> createSensor(SensorType sensor,osg::Matrix matrix = osg::Matrix::translate(osg::Vec3(8,8,8)));
 
-    if(zone == ZoneType::ROIzone){
-        
-        return myHelpers::make_unique<SafetyZone>(position);
-    }
-    else if(zone == ZoneType::SensorZone){
-
-        return myHelpers::make_unique<SensorZone>(position);
-    }
-    else{
-        assert(false);
-    }
-    return std::unique_ptr<Zone>();
-}
+std::unique_ptr<Zone> createZone(ZoneType zone);
