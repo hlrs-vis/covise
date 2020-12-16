3d6b4976fd3b96e8576d561af17d3f104176d126
hpcdgrie <dennis.grieger@hlrs.de>
2020-12-16 13:12:32 +0100

    Added tryPrint template to print only if there is a stream operator available

---
+++ src/kernel/util/tryPrint.h
@@ -0,0 +1,53 @@
+#ifndef COVISE_TRY_PRINT_H
+#define COVISE_TRY_PRINT_H
+#include <type_traits>
+#include <iostream>
+namespace covise
+{
+
+    template <class T>
+    static auto hasPrintMethod(int)
+        -> std::integral_constant<bool, std::is_reference<decltype(operator<<(std::cerr, T()))>::value>;
+
+    template <class>
+    static auto hasPrintMethod(...) -> std::false_type;
+
+    template <typename T>
+    struct HasPrintMethod : decltype(hasPrintMethod<T>(0))
+    {
+    };
+
+    template <class T>
+    typename std::enable_if<HasPrintMethod<T>::value>::type
+    tryPrintWithError(std::ostream &os, const T &t, const char *className, const char *errorMsg)
+    {
+        os << t;
+    }
+
+    template <class T>
+    typename std::enable_if<!HasPrintMethod<T>::value && std::is_arithmetic<T>::value>::type
+    tryPrintWithError(std::ostream &os, const T &t, const char *className, const char *errorMsg)
+    {
+        os << t;
+    }
+
+    // same as above
+    template <class T>
+    typename std::enable_if<!HasPrintMethod<T>::value && !std::is_arithmetic<T>::value>::type
+    tryPrintWithError(std::ostream &os, const T &t, const char *className, const char *errorMsg)
+    {
+        if (className && errorMsg)
+        {
+            os << className << errorMsg;
+        }
+    }
+
+    template <class T>
+    void tryPrint(const T &t)
+    {
+        tryPrintWithError(std::cerr, t, nullptr, nullptr);
+    }
+
+} // namespace covise
+
+#endif // !COVISE_TRY_PRINT_H
