9dfa6bbc16f2d85eb2dde5c103dc99210fd1ee39
hpcdgrie <dennis.grieger@hlrs.de>
2021-04-08 14:51:10 +0200

    Test setupServerConnection

--- tests/main.cpp
+++ tests/main.cpp
@@ -16,6 +16,7 @@ int main(int argc, char const *argv[])
     TEST(test_tryPrint)
     TEST(test_crbExec)
     TEST(testConnectionShutdown)
+    TEST(testSetupServerConnection)
     TEST(test_socket_write_receive)
     TEST(test_message_send_receive)
     std::cerr << "all tests succseeded!" << std::endl;
