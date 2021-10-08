1e2709331e391b3d5b197de78cb0c54197abe298
Martin Aumüller <aumuell@reserv.at>
2021-10-08 13:00:39 +0200

    launch coviseDaemon from menu

--- install/install-xdg.sh
+++ install/install-xdg.sh
@@ -3,10 +3,12 @@ xdg-mime install hlrs-net.xml
 xdg-desktop-menu install --noupdate hlrs-covise.desktop
 xdg-desktop-menu install --noupdate hlrs-cover.desktop
 xdg-desktop-menu install --noupdate hlrs-tablet.desktop
+xdg-desktop-menu install --noupdate hlrs-covise-daemon.desktop
 xdg-mime default hlrs-cover.desktop x-scheme-handler/cover
 xdg-desktop-menu forceupdate
 
 xdg-icon-resource install --noupdate --size 48 hlrs-cover.png
 xdg-icon-resource install --noupdate --size 48 hlrs-covise.png
+xdg-icon-resource install --noupdate --size 48 hlrs-covise-daemon.png
 xdg-icon-resource install --noupdate --context mimetypes --size 48 hlrs-covise.png text-x-net
 xdg-icon-resource forceupdate
