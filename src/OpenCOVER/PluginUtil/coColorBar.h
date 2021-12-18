b488585c97ef7f03ca15a8fd87296c6d1be1ae0f
Martin Aumüller <aumuell@reserv.at>
2021-12-19 00:50:11 +0100

    initialize

--- src/OpenCOVER/PluginUtil/coColorBar.h
+++ src/OpenCOVER/PluginUtil/coColorBar.h
@@ -42,12 +42,12 @@ namespace opencover
 class PLUGIN_UTILEXPORT coColorBar : public vrui::coMenuItem
 {
 private:
-    vrui::coColoredBackground *background_;
+    vrui::coColoredBackground *background_ = nullptr;
 
     // topspacer and texture in vertical row container
-    vrui::coColoredBackground *vspace_;
-    vrui::coTexturedBackground *texture_;
-    vrui::coRowContainer *textureAndVspace_;
+    vrui::coColoredBackground *vspace_ = nullptr;
+    vrui::coTexturedBackground *texture_ = nullptr;
+    vrui::coRowContainer *textureAndVspace_ = nullptr;
 
     // hspacers and labels in horiz containers, all labels in vert container
     vrui::coLabel *labels_[MAX_LABELS];
@@ -55,18 +55,18 @@ private:
     vrui::coTexturedBackground *hspaces_[MAX_LABELS];
     vrui::coRowContainer *labelAndHspaces_[MAX_LABELS];
     vrui::coColoredBackground *vspaces_[MAX_LABELS];
-    vrui::coRowContainer *allLabels_;
+    vrui::coRowContainer *allLabels_ = nullptr;
 
     // horiz container around textureAndVspacer and all labels
-    vrui::coRowContainer *textureAndLabels_;
+    vrui::coRowContainer *textureAndLabels_ = nullptr;
     // vert container around textureAndLabels_ and species label
     vrui::coRowContainer *everything_ = nullptr;
 
-    int numLabels_; // number of labels, max
+    int numLabels_ = 0; // number of labels, max
     float labelValues_[MAX_LABELS]; // numerical values of labels
     char format_str_[32]; // precision of float values
 
-    int numColors_;
+    int numColors_ = 0;
     float min_, max_;
     std::vector<unsigned char> image_, tickImage_;
     std::string name_; // the name of the colors module for example Colors_1
