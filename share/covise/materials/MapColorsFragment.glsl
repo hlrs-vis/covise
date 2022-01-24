298b6bf8dc26d1030e745feceba59b084a10b418
Martin Aumüller <aumuell@reserv.at>
2022-01-24 14:59:24 +0100

    shaders for rendering Vistle LayerGrid

---
+++ share/covise/materials/MapColorsFragment.glsl
@@ -0,0 +1,132 @@
+uniform sampler1D texUnit1;
+uniform float rangeMin, rangeMax;
+uniform bool blendWithMaterial;
+uniform bool Light0Enabled, Light1Enabled;
+uniform bool dataValid;
+
+varying float data;
+varying vec3 V;
+varying vec3 N;
+
+vec4 Ambient;
+vec4 Diffuse;
+vec4 Specular;
+
+void directionalLight(in int i, in vec3 normal)
+{
+   float nDotVP;         // normal . light direction
+   float nDotHV;         // normal . light half vector
+   float pf;             // power factor
+
+   nDotVP = abs(dot(normal, normalize(vec3(gl_LightSource[i].position))));
+   nDotHV = abs(dot(normal, vec3(gl_LightSource[i].halfVector)));
+
+   if (nDotVP == 0.0)
+   {
+       pf = 0.0;
+   }
+   else
+   {
+       pf = pow(nDotHV, gl_FrontMaterial.shininess);
+
+   }
+   Ambient  += gl_LightSource[i].ambient;
+   Diffuse  += gl_LightSource[i].diffuse * nDotVP;
+   Specular += gl_LightSource[i].specular * pf;
+}
+
+void pointLight(in int i, in vec3 N, in vec3 eye)
+{
+   float pf;           // power factor
+   float d;            // distance from surface to light source
+
+   vec3 L = normalize(gl_LightSource[i].position.xyz - V);
+   float NdotL = abs(dot(N, L));
+   if (NdotL == 0.0)
+   {
+       pf = 0.0;
+   }
+   else
+   {
+       vec3 E = normalize(-V);
+       vec3 R = normalize(-reflect(L, N));
+       pf = pow(abs(dot(R, E)), gl_FrontMaterial.shininess);
+   }
+
+
+   // Compute distance between surface and light position
+   d = length(L);
+
+   // Compute attenuation
+   float attenuation = 1.0 / (gl_LightSource[i].constantAttenuation +
+       gl_LightSource[i].linearAttenuation * d +
+       gl_LightSource[i].quadraticAttenuation * d * d);
+
+   Ambient  += gl_LightSource[i].ambient * attenuation;
+   Diffuse  += gl_LightSource[i].diffuse * NdotL * attenuation;
+   Specular += gl_LightSource[i].specular * pf * attenuation;
+}
+
+vec4 flight(in vec3 normal, vec4 color)
+{
+    vec3 eye = vec3 (0.0, 0.0, 1.0);
+
+    // Clear the light intensity accumulators
+    Ambient  = vec4 (0.0);
+    Diffuse  = vec4 (0.0);
+    Specular = vec4 (0.0);
+
+    if (Light0Enabled)
+        pointLight(0, normal, eye);
+    if (Light1Enabled)
+        pointLight(1, normal, eye);
+
+    //directionalLight(2, normal);
+
+    return color * (Diffuse + Ambient) + Specular * color.a;
+}
+
+vec4 flightm(in vec3 normal)
+{
+    vec3 eye = vec3(0.0, 0.0, 1.0);
+
+    // Clear the light intensity accumulators
+    Ambient = vec4(0.0);
+    Diffuse = vec4(0.0);
+    Specular = vec4(0.0);
+
+    if (Light0Enabled)
+        pointLight(0, normal, eye);
+    if (Light1Enabled)
+        pointLight(1, normal, eye);
+
+    //directionalLight(2, normal);
+
+    return gl_FrontMaterial.diffuse * Diffuse + gl_FrontMaterial.ambient * Ambient +
+           gl_FrontMaterial.specular * Specular;
+}
+
+
+void main (void) 
+{
+    if (dataValid)
+    {
+        float t = (data - rangeMin) / (rangeMax - rangeMin);
+        vec4 color = texture1D(texUnit1, t);
+        if (blendWithMaterial)
+        {
+            color = mix(gl_FrontMaterial.diffuse, color, color.a);
+            color.a = 1.0;
+        }
+        gl_FragColor = flight(normalize(N), color);
+
+        if (!blendWithMaterial)
+        {
+            gl_FragColor.a = color.a;
+        }
+    }
+    else
+    {
+        gl_FragColor = flightm(normalize(N));
+    }
+}
---
+++ share/covise/materials/MapColorsHeightmapGeometry.glsl
@@ -0,0 +1,77 @@
+#version 120
+#extension GL_EXT_geometry_shader4 : enable
+#extension GL_ARB_texture_rectangle : enable
+
+uniform sampler2DRect heightTex;
+uniform bool dataValid;
+uniform sampler2DRect dataTex;
+uniform vec2 size;
+uniform vec2 dist;
+uniform vec4 origin;
+
+uniform vec2 off[] = vec2[4](vec2(0, 0), vec2(0, 1), vec2(1, 0), vec2(1, 1));
+
+varying out vec3 N;
+varying out vec3 V;
+varying out float data = 0.;
+
+float getHeight(vec2 xy)
+{
+    return texture2DRect(heightTex, xy + vec2(.5, .5)).r;
+}
+
+float getData(vec2 xy)
+{
+    return texture2DRect(dataTex, xy + vec2(.5, .5)).r;
+}
+
+vec4 pos(vec2 xy, float h)
+{
+    vec4 p = origin + vec4(dist * xy, h, 0.);
+    return p;
+}
+
+void createVertex(vec2 xy)
+{
+    float h = getHeight(xy);
+    gl_Position = gl_ModelViewProjectionMatrix * pos(xy, h);
+    gl_ClipVertex = gl_Position;
+    V = gl_Position.xyz / gl_Position.w;
+
+    float dx = 0., dy = 0.;
+    float hn = h, hs = h, he = h, hw = h;
+    if (xy.x >= 1)
+    {
+        he = getHeight(xy - vec2(1, 0));
+        dx += dist.x;
+    }
+    if (xy.x < size.x)
+    {
+        hw = getHeight(xy + vec2(1, 0));
+        dx += dist.x;
+    }
+    if (xy.y >= 1)
+    {
+        hs = getHeight(xy + vec2(0, 1));
+        dy += dist.y;
+    }
+    if (xy.y < size.y)
+    {
+        hn = getHeight(xy + vec2(0, 1));
+        dy += dist.y;
+    }
+    N = normalize(gl_NormalMatrix * vec3((hw - he) / dx, (hn - hs) / dy, 1.));
+
+    if (dataValid)
+        data = getData(xy);
+    EmitVertex();
+}
+
+void main()
+{
+    vec2 xy = gl_PositionIn[0].xy;
+
+    for (int i = 0; i < 4; ++i)
+        createVertex(xy + off[i]);
+    EndPrimitive();
+}
