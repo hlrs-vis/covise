be70d5bcbbd0f3b4c802d12ca1975a4cea59a75d
Marko Djuric <marko.djuric@gmx.de>
2022-02-18 09:42:59 +0100

    OpenFOAMInterface: simplify code

--- src/tools/Revit/OpenFOAM/BIM/OpenFOAM/OpenFOAMCalculator.cs
+++ src/tools/Revit/OpenFOAM/BIM/OpenFOAM/OpenFOAMCalculator.cs
@@ -44,10 +44,7 @@ namespace OpenFOAMInterface.BIM.OpenFOAM
         /// <param name="meanFlowVelocity">Mean flow velocity.</param>
         /// <param name="turbulenceIntensity">Turbulenc intensity.</param>
         /// <returns>k for k-epsilon Turbulencemodel.</returns>
-        public double CalculateK(double meanFlowVelocity, double turbulenceIntensity)
-        {
-            return Math.Pow(meanFlowVelocity * turbulenceIntensity, 2) * 3 / 2;
-        }
+        public double CalculateK(double meanFlowVelocity, double turbulenceIntensity) => Math.Pow(meanFlowVelocity * turbulenceIntensity, 2) * 3 / 2;
 
         /// <summary>
         /// Calculate epsilon in OpenFOAM-convetion for k-epsilon turbulencemodel.
@@ -56,10 +53,7 @@ namespace OpenFOAMInterface.BIM.OpenFOAM
         /// <param name="turbulenceLengthScale">estimated size for turbulent eddies.</param>
         /// <param name="k">Turbulence energie.</param>
         /// <returns>Epsilon for k-epsilon turbulencemodel.</returns>
-        public double CalculateEpsilon(double turbulenceLengthScale, double k)
-        {
-            return Math.Pow(m_Cnue, 0.75) * Math.Pow(k, 1.5) / turbulenceLengthScale;
-        }
+        public double CalculateEpsilon(double turbulenceLengthScale, double k) => Math.Pow(m_Cnue, 0.75) * Math.Pow(k, 1.5) / turbulenceLengthScale;
 
         /// <summary>
         /// Calculates Reynoldsnumber.
@@ -68,10 +62,7 @@ namespace OpenFOAMInterface.BIM.OpenFOAM
         /// <param name="kinematicViscosity">Kinematic viscosity of the fluid.</param>
         /// <param name="characteristicLength">Characteristic length (hydraulic diameter in pipes)</param>
         /// <returns>Reynoldsnumber as double.</returns>
-        public double CalculateReynoldsnumber(double meanFlowVelocity, double kinematicViscosity, double characteristicLength)
-        {
-            return meanFlowVelocity * characteristicLength / kinematicViscosity;
-        }
+        public double CalculateReynoldsnumber(double meanFlowVelocity, double kinematicViscosity, double characteristicLength) => meanFlowVelocity * characteristicLength / kinematicViscosity;
 
         /// <summary>
         /// Calculate the hydraulicDiameter for pipes that aren't round.
@@ -79,22 +70,16 @@ namespace OpenFOAMInterface.BIM.OpenFOAM
         /// <param name="area">Flow area in pipe.</param>
         /// <param name="boundaryLength">Boundary length of flow area.</param>
         /// <returns>Hydraulic diameter for unround pipes.</returns>
-        public double CalculateHydraulicDiameter(double area, double boundaryLength)
-        {
-            return 4 * area / boundaryLength;
-        }
+        public double CalculateHydraulicDiameter(double area, double boundaryLength) => 4 * area / boundaryLength;
 
         /// <summary>
         /// Estimate the turbulence intensity based on Reynoldsnumber.
+        /// for fully developed piped flows.
+        /// Source: https://www.cfd-online.com/Wiki/Turbulence_intensity
         /// </summary>
         /// <param name="reynoldsNumber">Reynoldsnumber as double.</param>
         /// <returns>Turbulenceintensity as double.</returns>
-        public double EstimateTurbulenceIntensityPipe(double reynoldsNumber)
-        {
-            //for fully developed piped flows.
-            //Source: https://www.cfd-online.com/Wiki/Turbulence_intensity
-            return 0.16 * Math.Pow(reynoldsNumber, -1.0 / 8.0);
-        }
+        public double EstimateTurbulenceIntensityPipe(double reynoldsNumber) => 0.16 * Math.Pow(reynoldsNumber, -1.0 / 8.0);
 
         /// <summary>
         /// Returns the rho-normalized pressure.
@@ -102,31 +87,22 @@ namespace OpenFOAMInterface.BIM.OpenFOAM
         /// <param name="externalPressure">External pressure.</param>
         /// <param name="rho">Density of fluid.</param>
         /// <returns>rho-normalized pressure as double.</returns>
-        public double CalculateRhoNormalizedPressure(double externalPressure, double rho)
-        {
-            return externalPressure / rho;
-        }
+        public double CalculateRhoNormalizedPressure(double externalPressure, double rho) => externalPressure / rho;
 
         /// <summary>
         /// Estimate turbulence length scale for pipe flows.
+        /// For fully developed piped flows.
+        /// Source: https://www.cfd-online.com/Wiki/Turbulence_length_scale
         /// </summary>
         /// <param name="hydraulicDiameter">Hydraulic Diameter of the pipe.</param>
         /// <returns>Turbulence length scale as double.</returns>
-        public double EstimateTurbulencLengthScalePipe(double hydraulicDiameter)
-        {
-            //for fully developed piped flows.
-            //Source: https://www.cfd-online.com/Wiki/Turbulence_length_scale
-            return 0.038 * hydraulicDiameter;
-        }
+        public double EstimateTurbulencLengthScalePipe(double hydraulicDiameter) => 0.038 * hydraulicDiameter;
 
         /// <summary>
         /// Interpolates kinematic vicosity for the given temprature between 20 and 30 degree.
         /// </summary>
         /// <param name="temp">Reference tempreture for kinematic viscosity.</param>
         /// <returns>Kinematic viscosity at temprature.</returns>
-        public double InterpolateKinematicViscosity(double temp)
-        {
-            return m_ViAir20 + (m_ViAir30 - m_ViAir20) * (temp - 20) / 10;
-        }
+        public double InterpolateKinematicViscosity(double temp) => m_ViAir20 + (m_ViAir30 - m_ViAir20) * (temp - 20) / 10;
     }
 }
