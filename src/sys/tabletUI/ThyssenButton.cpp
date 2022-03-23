0bb39addeda5b604b8999fc786e08e5adde17ff9
Uwe Woessner <woessner@hlrs.de>
2022-03-23 13:50:39 +0100

    thyssenPannelButton

---
+++ src/sys/tabletUI/ThyssenButton.cpp
@@ -0,0 +1,100 @@
+/* This file is part of COVISE.
+
+   You can use it under the terms of the GNU Lesser General Public License
+   version 2.1 or later, see lgpl-2.1.txt.
+
+ * License: LGPL 2+ */
+
+#include <assert.h>
+#include <stdio.h>
+
+#include <QPushButton>
+#include <QLabel>
+#include <QPixmap>
+
+#include "ThyssenButton.h"
+#include "TUIApplication.h"
+#include <net/tokenbuffer.h>
+
+/// Constructor
+ThyssenButton::ThyssenButton(int id, int type, QWidget *w, int parent, QString name)
+    : TUIElement(id, type, w, parent, name)
+{
+    QPushButton *b = new QPushButton(w);
+    if (name.contains("."))
+    {
+        QPixmap pm(name);
+        if (pm.isNull())
+        {
+            QString covisedir = QString(getenv("COVISEDIR"));
+            QPixmap pm(covisedir + "/" + name);
+            QPixmap qm(covisedir + "/icons/" + name);
+            if (pm.isNull() && qm.isNull())
+            {
+                b->setText(name);
+            }
+            else if (pm.isNull())
+            {
+                b->setIcon(qm);
+            }
+            else
+            {
+                b->setIcon(pm);
+            }
+        }
+        else
+        {
+            b->setIcon(pm);
+        }
+    }
+    else
+        b->setText(name);
+
+    //b->setFixedSize(b->sizeHint());
+    widget = b;
+    connect(b, SIGNAL(pressed()), this, SLOT(pressed()));
+    connect(b, SIGNAL(released()), this, SLOT(released()));
+}
+
+/// Destructor
+ThyssenButton::~ThyssenButton()
+{
+    delete widget;
+}
+
+void ThyssenButton::pressed()
+{
+    covise::TokenBuffer tb;
+    tb << ID;
+    tb << TABLET_PRESSED;
+    TUIMainWindow::getInstance()->send(tb);
+}
+
+void ThyssenButton::released()
+{
+    covise::TokenBuffer tb;
+    tb << ID;
+    tb << TABLET_RELEASED;
+    TUIMainWindow::getInstance()->send(tb);
+}
+
+void ThyssenButton::setSize(int w, int h)
+{
+    QPushButton *b = (QPushButton *)widget;
+    b->setIconSize(QSize(w, h)); /* Max size of icons, smaller icons will not be scaled up */
+    b->setFixedSize(b->sizeHint());
+}
+
+const char *ThyssenButton::getClassName() const
+{
+    return "ThyssenButton";
+}
+
+void ThyssenButton::setLabel(QString textl)
+{
+    TUIElement::setLabel(textl);
+    if (QAbstractButton* b = qobject_cast<QAbstractButton*>(widget))
+    {
+        b->setText(textl);
+    }
+}
---
+++ src/sys/tabletUI/ThyssenButton.h
@@ -0,0 +1,41 @@
+/* This file is part of COVISE.
+
+   You can use it under the terms of the GNU Lesser General Public License
+   version 2.1 or later, see lgpl-2.1.txt.
+
+ * License: LGPL 2+ */
+
+#ifndef CO_TUI_BUTTON_H
+#define CO_TUI_BUTTON_H
+
+#include <QObject>
+
+#include "TUIElement.h"
+
+/** Basic Container
+ * This class provides basic functionality and a
+ * common interface to all Container elements.<BR>
+ * The functionality implemented in this class represents a container
+ * which arranges its children on top of each other.
+ */
+class ThyssenButton : public QObject, public TUIElement
+{
+    Q_OBJECT
+
+public:
+    ThyssenButton(int id, int type, QWidget *w, int parent, QString name);
+    virtual ~ThyssenButton();
+    virtual void setSize(int w, int h) override;
+    virtual void setLabel(QString textl) override;
+
+    /// get the Element's classname
+    virtual const char *getClassName() const override;
+
+public slots:
+
+    void pressed();
+    void released();
+
+protected:
+};
+#endif
