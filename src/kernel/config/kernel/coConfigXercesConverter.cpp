ad5c7d10ec128d0b4e24e02cb48f410fece5053c
hpcdgrie <dennis.grieger@hlrs.de>
2022-03-23 16:15:29 +0100

    Fallback on boost::locale::conv::from_utf to convert XMLCh if cxx_unicode_literals is not supported

--- src/kernel/config/kernel/coConfigXercesConverter.cpp
+++ src/kernel/config/kernel/coConfigXercesConverter.cpp
@@ -1,8 +1,10 @@
 #include "coConfigXercesConverter.h"
-#include <locale>
 #include <iostream>
+#include <string>
 #include <xercesc/util/XMLString.hpp>
 
+#ifdef HAVE_UNICODE_LITERALS
+#include <locale>
 // utility wrapper to adapt locale-bound facets for wstring/wbuffer convert
 template <class Facet>
 struct deletable_facet : Facet
@@ -16,10 +18,20 @@ std::string covise::xercescToStdString(const XMLCh *str)
 {
     if (!str)
         return "";
+    const wchar_t *s;
     static std::wstring_convert<deletable_facet<std::codecvt<XMLCh, char, std::mbstate_t>>, XMLCh> convert;
     return convert.to_bytes(str);
 }
+#else
+#include <boost/locale/encoding.hpp>
+std::string covise::xercescToStdString(const XMLCh *str)
+{
+    if (!str)
+        return "";
+    return boost::locale::conv::from_utf<XMLCh>(str, "Latin1");
+}
 
+#endif
 void Deleter::operator()(XMLCh *d)
 {
     xercesc::XMLString::release(&d);
