7cb67ec987bd9db5c3b3809545d3c5a60565602d
hpcdgrie <dennis.grieger@hlrs.de>
2022-03-25 09:27:40 +0100

    Use native xerces transcode to convert scercess chars to std::string

--- src/kernel/config/kernel/coConfigXercesConverter.cpp
+++ src/kernel/config/kernel/coConfigXercesConverter.cpp
@@ -2,36 +2,18 @@
 #include <iostream>
 #include <string>
 #include <xercesc/util/XMLString.hpp>
-
-#ifdef HAVE_UNICODE_LITERALS
 #include <locale>
-// utility wrapper to adapt locale-bound facets for wstring/wbuffer convert
-template <class Facet>
-struct deletable_facet : Facet
-{
-    template <class... Args>
-    deletable_facet(Args &&...args) : Facet(std::forward<Args>(args)...) {}
-    ~deletable_facet() {}
-};
-
-std::string covise::xercescToStdString(const XMLCh *str)
-{
-    if (!str)
-        return "";
-    const wchar_t *s;
-    static std::wstring_convert<deletable_facet<std::codecvt<XMLCh, char, std::mbstate_t>>, XMLCh> convert;
-    return convert.to_bytes(str);
-}
-#else
 #include <boost/locale/encoding.hpp>
-std::string covise::xercescToStdString(const XMLCh *str)
+
+std::string
+covise::xercescToStdString(const XMLCh *str)
 {
-    if (!str)
-        return "";
-    return boost::locale::conv::from_utf<XMLCh>(str, "Latin1");
+    char *c = xercesc::XMLString::transcode(str);
+    std::string retval = c;
+    xercesc::XMLString::release(&c);
+    return retval;
 }
 
-#endif
 void Deleter::operator()(XMLCh *d)
 {
     xercesc::XMLString::release(&d);
