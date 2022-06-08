ccb2ece0a10f56240123748e33b563590ae3dc67
Marko Djuric <marko.djuric@gmx.de>
2022-06-08 12:22:10 +0200

    OpenFOAMInterface: generate surface feature for all exported stl files.

--- src/tools/Revit/OpenFOAM/BIM/OpenFOAM/SurfaceFeatureExtract.cs
+++ src/tools/Revit/OpenFOAM/BIM/OpenFOAM/SurfaceFeatureExtract.cs
@@ -5,6 +5,8 @@
 
  * License: LGPL 2+ */
 using System.Collections.Generic;
+using Autodesk.Revit.DB;
+using utils;
 
 namespace OpenFOAMInterface.BIM.OpenFOAM
 {
@@ -17,6 +19,7 @@ namespace OpenFOAMInterface.BIM.OpenFOAM
         /// Dictionary for SurfaceFeatures
         /// </summary>
         //private Dictionary<string, object> m_SurfaceFeature;
+        private readonly Dictionary<KeyValuePair<string, Document>, KeyValuePair<List<Face>, Transform>> m_Faces;
 
         /// <summary>
         /// Name of the STL
@@ -32,12 +35,13 @@ namespace OpenFOAMInterface.BIM.OpenFOAM
         /// <param name="format">Ascii or Binary.</param>
         /// <param name="settings">Settings-object</param>
         /// <param name="stlName">Name of the stl</param>
-        public SurfaceFeatureExtract(Version version, string path, Dictionary<string, object> attributes, SaveFormat format, string stlName)
+        public SurfaceFeatureExtract(in Version version, in string path, in Dictionary<string, object> attributes, in Dictionary<KeyValuePair<string, Document>, KeyValuePair<List<Face>, Transform>> faces, in SaveFormat format, in string stlName)
             : base("surfaceFeatureExtractDict", "dictionary", version, path, attributes, format)
         {
             //m_SurfaceFeature = new Dictionary<string, object>();
             //m_SurfaceFeature = m_DictFolder;
             m_STLName = stlName;
+            m_Faces = faces;
             InitAttributes();
         }
 
@@ -47,6 +51,15 @@ namespace OpenFOAMInterface.BIM.OpenFOAM
         public override void InitAttributes()
         {
             FoamFile.Attributes.Add(m_STLName + ".stl", m_DictFile);
+            foreach(var face in m_Faces)
+                FoamFile.Attributes.Add(face.Key.Key + ".stl", m_DictFile);
+
+            var s = FOAMInterface.Singleton.Settings;
+            foreach (var entry in s.MeshResolution)
+            {
+                var name = AutodeskHelperFunctions.GenerateNameFromElement(entry.Key);
+                FoamFile.Attributes.Add(name + ".stl", m_DictFile);
+            }
 
             //TO-DO: Dont set in this class
             //BIM.OpenFOAMExport.Exporter.Instance.settings.Features.Add("{file \"" + m_STLName + ".eMesh\"; level 3;}");
