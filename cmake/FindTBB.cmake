cd656f94fd98adbfe504f6aefd3256c36b5e76bc
Martin Aumüller <aumuell@reserv.at>
2022-06-24 17:34:58 +0200

    also find oneTBB

--- cmake/FindTBB.cmake
+++ cmake/FindTBB.cmake
@@ -14,144 +14,219 @@
 ## limitations under the License.                                           ##
 ## ======================================================================== ##
 
-FIND_PATH(TBB_ROOT include/tbb/task_scheduler_init.h
+find_path(
+    TBB_ROOT include/tbb/task_group.h
     DOC "Root of TBB installation"
-    PATHS ${PROJECT_SOURCE_DIR}/tbb 
-          "C:/Program Files (x86)/Intel/Composer XE/tbb" 
-          /opt/intel/composerxe/tbb 
+    PATHS ${PROJECT_SOURCE_DIR}/tbb
+          "C:/Program Files (x86)/Intel/Composer XE/tbb"
+          /opt/intel/composerxe/tbb
           /opt/homebrew/opt/tbb
           /usr/local/opt/tbb
-		  $ENV{EXTERNLIBS}/tbb
-	  /usr
-)
+          $ENV{EXTERNLIBS}/tbb
+          /usr)
 #UNSET(TBB_INCLUDE_DIR CACHE)
 #UNSET(TBB_LIBRARY CACHE)
 #UNSET(TBB_LIBRARY_MALLOC CACHE)
 
-INCLUDE(FindPackageHandleStandardArgs)
+include(FindPackageHandleStandardArgs)
 
-IF (WIN32)
-  
-  #IF (CMAKE_SIZEOF_VOID_P EQUAL 8)
-    SET(TBB_ARCH intel64)
-  #ELSE()
-  #  SET(TBB_ARCH ia32)
-  #ENDIF()
+if(WIN32)
 
-  IF (MSVC10)
-    SET(TBB_VCVER vc10)
-  ELSEIF (MSVC11)
-    SET(TBB_VCVER vc11)
-  ELSEIF(MSVC12)
-    SET(TBB_VCVER vc12)
-  ELSEIF(MSVC13)
-    SET(TBB_VCVER vc13)
-  ELSE()
-    SET(TBB_VCVER vc14)
-  ENDIF()
+    #IF (CMAKE_SIZEOF_VOID_P EQUAL 8)
+    set(TBB_ARCH intel64)
+    #ELSE()
+    #  SET(TBB_ARCH ia32)
+    #ENDIF()
 
-  SET(TBB_LIBDIR ${TBB_ROOT}/lib/${TBB_ARCH}/${TBB_VCVER})
-  SET(TBB_BINDIR ${TBB_ROOT}/bin/${TBB_ARCH}/${TBB_VCVER})
+    if(MSVC10)
+        set(TBB_VCVER vc10)
+    elseif(MSVC11)
+        set(TBB_VCVER vc11)
+    elseif(MSVC12)
+        set(TBB_VCVER vc12)
+    elseif(MSVC13)
+        set(TBB_VCVER vc13)
+    else()
+        set(TBB_VCVER vc14)
+    endif()
 
-  FIND_PATH(TBB_INCLUDE_DIR tbb/task_scheduler_init.h PATHS ${TBB_ROOT}/include NO_DEFAULT_PATH)
-  FIND_LIBRARY(TBB_LIBRARY tbb PATHS ${TBB_LIBDIR} NO_DEFAULT_PATH)
-  FIND_LIBRARY(TBB_LIBRARY_MALLOC tbbmalloc PATHS ${TBB_LIBDIR} NO_DEFAULT_PATH)
-  FIND_LIBRARY(TBB_LIBRARY_DEBUG tbb_debug PATHS ${TBB_LIBDIR} NO_DEFAULT_PATH)
-  FIND_LIBRARY(TBB_LIBRARY_MALLOC_DEBUG tbbmalloc_debug PATHS ${TBB_LIBDIR} NO_DEFAULT_PATH)
+    set(TBB_LIBDIR ${TBB_ROOT}/lib/${TBB_ARCH}/${TBB_VCVER})
+    set(TBB_BINDIR ${TBB_ROOT}/bin/${TBB_ARCH}/${TBB_VCVER})
 
-  if (NOT TBB_LIBRARY)
-    SET(TBB_LIBDIR ${TBB_ROOT}/lib)
-    SET(TBB_DEBUGLIBDIR ${TBB_ROOT}/debug/lib)
-    SET(TBB_BINDIR ${TBB_ROOT}/bin)
+    find_path(
+        TBB_INCLUDE_DIR tbb/task_group.h
+        PATHS ${TBB_ROOT}/include
+        NO_DEFAULT_PATH)
+    find_library(
+        TBB_LIBRARY tbb
+        PATHS ${TBB_LIBDIR}
+        NO_DEFAULT_PATH)
+    find_library(
+        TBB_LIBRARY_MALLOC tbbmalloc
+        PATHS ${TBB_LIBDIR}
+        NO_DEFAULT_PATH)
+    find_library(
+        TBB_LIBRARY_DEBUG tbb_debug
+        PATHS ${TBB_LIBDIR}
+        NO_DEFAULT_PATH)
+    find_library(
+        TBB_LIBRARY_MALLOC_DEBUG tbbmalloc_debug
+        PATHS ${TBB_LIBDIR}
+        NO_DEFAULT_PATH)
 
-    FIND_LIBRARY(TBB_LIBRARY tbb PATHS ${TBB_LIBDIR} NO_DEFAULT_PATH)
-    FIND_LIBRARY(TBB_LIBRARY_MALLOC tbbmalloc PATHS ${TBB_LIBDIR} NO_DEFAULT_PATH)
-    FIND_LIBRARY(TBB_LIBRARY_DEBUG tbb_debug PATHS ${TBB_LIBDIR} ${TBB_DEBUGLIBDIR} NO_DEFAULT_PATH)
-    FIND_LIBRARY(TBB_LIBRARY_MALLOC_DEBUG tbbmalloc_debug PATHS ${TBB_LIBDIR} ${TBB_DEBUGLIBDIR} NO_DEFAULT_PATH)
-  endif()
+    if(NOT TBB_LIBRARY)
+        set(TBB_LIBDIR ${TBB_ROOT}/lib)
+        set(TBB_DEBUGLIBDIR ${TBB_ROOT}/debug/lib)
+        set(TBB_BINDIR ${TBB_ROOT}/bin)
 
-  FIND_PACKAGE_HANDLE_STANDARD_ARGS(TBB DEFAULT_MSG TBB_INCLUDE_DIR TBB_LIBRARY TBB_LIBRARY_MALLOC TBB_LIBRARY_DEBUG TBB_LIBRARY_MALLOC_DEBUG)
+        find_library(
+            TBB_LIBRARY tbb
+            PATHS ${TBB_LIBDIR}
+            NO_DEFAULT_PATH)
+        find_library(
+            TBB_LIBRARY_MALLOC tbbmalloc
+            PATHS ${TBB_LIBDIR}
+            NO_DEFAULT_PATH)
+        find_library(
+            TBB_LIBRARY_DEBUG tbb_debug
+            PATHS ${TBB_LIBDIR} ${TBB_DEBUGLIBDIR}
+            NO_DEFAULT_PATH)
+        find_library(
+            TBB_LIBRARY_MALLOC_DEBUG tbbmalloc_debug
+            PATHS ${TBB_LIBDIR} ${TBB_DEBUGLIBDIR}
+            NO_DEFAULT_PATH)
+    endif()
 
-  IF (TBB_FOUND)
-    SET(TBB_INCLUDE_DIRS ${TBB_INCLUDE_DIR})
-    SET(TBB_LIBRARIES
-      optimized ${TBB_LIBRARY} ${TBB_LIBRARY_MALLOC}
-      debug ${TBB_LIBRARY_DEBUG} ${TBB_LIBRARY_MALLOC_DEBUG})
-  ENDIF()
+    find_package_handle_standard_args(
+        TBB
+        DEFAULT_MSG
+        TBB_INCLUDE_DIR
+        TBB_LIBRARY
+        TBB_LIBRARY_MALLOC
+        TBB_LIBRARY_DEBUG
+        TBB_LIBRARY_MALLOC_DEBUG)
 
-  IF (TBB_INCLUDE_DIR AND TBB_LIBRARY_MIC AND TBB_LIBRARY_MALLOC_MIC)
-    SET(TBB_FOUND_MIC TRUE)
-    SET(TBB_INCLUDE_DIRS_MIC ${TBB_INCLUDE_DIR_MIC})
-    SET(TBB_LIBRARIES_MIC ${TBB_LIBRARY_MIC} ${TBB_LIBRARY_MALLOC_MIC})
-  ENDIF()
+    if(TBB_FOUND)
+        set(TBB_INCLUDE_DIRS ${TBB_INCLUDE_DIR})
+        set(TBB_LIBRARIES optimized ${TBB_LIBRARY} ${TBB_LIBRARY_MALLOC} debug ${TBB_LIBRARY_DEBUG} ${TBB_LIBRARY_MALLOC_DEBUG})
+    endif()
 
-  MARK_AS_ADVANCED(TBB_INCLUDE_DIR)
-  MARK_AS_ADVANCED(TBB_LIBRARY)
-  MARK_AS_ADVANCED(TBB_LIBRARY_MALLOC)
-  MARK_AS_ADVANCED(TBB_LIBRARY_DEBUG)
-  MARK_AS_ADVANCED(TBB_LIBRARY_MALLOC_DEBUG)
+    if(TBB_INCLUDE_DIR
+       AND TBB_LIBRARY_MIC
+       AND TBB_LIBRARY_MALLOC_MIC)
+        set(TBB_FOUND_MIC TRUE)
+        set(TBB_INCLUDE_DIRS_MIC ${TBB_INCLUDE_DIR_MIC})
+        set(TBB_LIBRARIES_MIC ${TBB_LIBRARY_MIC} ${TBB_LIBRARY_MALLOC_MIC})
+    endif()
 
-ELSE ()
+    mark_as_advanced(TBB_INCLUDE_DIR)
+    mark_as_advanced(TBB_LIBRARY)
+    mark_as_advanced(TBB_LIBRARY_MALLOC)
+    mark_as_advanced(TBB_LIBRARY_DEBUG)
+    mark_as_advanced(TBB_LIBRARY_MALLOC_DEBUG)
 
-  IF (APPLE)
-    IF (ENABLE_INSTALLER)
-      FIND_PATH(TBB_INCLUDE_DIR tbb/task_scheduler_init.h)
-      FIND_LIBRARY(TBB_LIBRARY tbb)
-      FIND_LIBRARY(TBB_LIBRARY_MALLOC tbbmalloc)
-    ELSE()
-      FIND_PATH(TBB_INCLUDE_DIR tbb/task_scheduler_init.h PATHS ${TBB_ROOT}/include NO_DEFAULT_PATH)
-      FIND_LIBRARY(TBB_LIBRARY tbb PATHS ${TBB_ROOT}/lib NO_DEFAULT_PATH)
-      FIND_LIBRARY(TBB_LIBRARY_MALLOC tbbmalloc PATHS ${TBB_ROOT}/lib NO_DEFAULT_PATH)
-    ENDIF()
-  ELSE()
+else()
 
-    FIND_PATH(TBB_INCLUDE_DIR tbb/task_scheduler_init.h PATHS ${TBB_ROOT}/include NO_DEFAULT_PATH)
-    FIND_LIBRARY(TBB_LIBRARY tbb PATHS ${TBB_ROOT}/lib ${TBB_ROOT}/lib64 ${TBB_ROOT}/lib/x86_64-linux-gnu ${TBB_ROOT}/lib/intel64/gcc4.4 NO_DEFAULT_PATH)
-    FIND_LIBRARY(TBB_LIBRARY_MALLOC tbbmalloc PATHS ${TBB_ROOT}/lib ${TBB_ROOT}/lib64 ${TBB_ROOT}/lib/x86_64-linux-gnu ${TBB_ROOT}/lib/intel64/gcc4.4 NO_DEFAULT_PATH)
-  ENDIF()
+    if(APPLE)
+        if(ENABLE_INSTALLER)
+            find_path(TBB_INCLUDE_DIR tbb/task_group.h)
+            find_library(TBB_LIBRARY tbb)
+            find_library(TBB_LIBRARY_MALLOC tbbmalloc)
+        else()
+            find_path(
+                TBB_INCLUDE_DIR tbb/task_group.h
+                PATHS ${TBB_ROOT}/include
+                NO_DEFAULT_PATH)
+            find_library(
+                TBB_LIBRARY tbb
+                PATHS ${TBB_ROOT}/lib
+                NO_DEFAULT_PATH)
+            find_library(
+                TBB_LIBRARY_MALLOC tbbmalloc
+                PATHS ${TBB_ROOT}/lib
+                NO_DEFAULT_PATH)
+        endif()
+    else()
 
-  FIND_PATH(TBB_INCLUDE_DIR_MIC tbb/task_scheduler_init.h PATHS ${TBB_ROOT}/include NO_DEFAULT_PATH)
-  FIND_LIBRARY(TBB_LIBRARY_MIC tbb PATHS ${TBB_ROOT}/lib/mic NO_DEFAULT_PATH)
-  FIND_LIBRARY(TBB_LIBRARY_MALLOC_MIC tbbmalloc PATHS ${TBB_ROOT}/lib/mic NO_DEFAULT_PATH)
+        find_path(
+            TBB_INCLUDE_DIR tbb/task_group.h
+            PATHS ${TBB_ROOT}/include
+            NO_DEFAULT_PATH)
+        find_library(
+            TBB_LIBRARY tbb
+            PATHS ${TBB_ROOT}/lib ${TBB_ROOT}/lib64 ${TBB_ROOT}/lib/x86_64-linux-gnu ${TBB_ROOT}/lib/intel64/gcc4.4
+            NO_DEFAULT_PATH)
+        find_library(
+            TBB_LIBRARY_MALLOC tbbmalloc
+            PATHS ${TBB_ROOT}/lib ${TBB_ROOT}/lib64 ${TBB_ROOT}/lib/x86_64-linux-gnu ${TBB_ROOT}/lib/intel64/gcc4.4
+            NO_DEFAULT_PATH)
+    endif()
 
-  MARK_AS_ADVANCED(TBB_INCLUDE_DIR_MIC)
-  MARK_AS_ADVANCED(TBB_LIBRARY_MIC)
-  MARK_AS_ADVANCED(TBB_LIBRARY_MALLOC_MIC)
+    find_path(
+        TBB_INCLUDE_DIR_MIC tbb/task_group.h
+        PATHS ${TBB_ROOT}/include
+        NO_DEFAULT_PATH)
+    find_library(
+        TBB_LIBRARY_MIC tbb
+        PATHS ${TBB_ROOT}/lib/mic
+        NO_DEFAULT_PATH)
+    find_library(
+        TBB_LIBRARY_MALLOC_MIC tbbmalloc
+        PATHS ${TBB_ROOT}/lib/mic
+        NO_DEFAULT_PATH)
 
-  FIND_PACKAGE_HANDLE_STANDARD_ARGS(TBB DEFAULT_MSG TBB_INCLUDE_DIR TBB_LIBRARY TBB_LIBRARY_MALLOC)
+    mark_as_advanced(TBB_INCLUDE_DIR_MIC)
+    mark_as_advanced(TBB_LIBRARY_MIC)
+    mark_as_advanced(TBB_LIBRARY_MALLOC_MIC)
 
-  IF (TBB_FOUND)
-    SET(TBB_INCLUDE_DIRS ${TBB_INCLUDE_DIR})
-    SET(TBB_LIBRARIES ${TBB_LIBRARY} ${TBB_LIBRARY_MALLOC})
-  ENDIF()
+    find_package_handle_standard_args(TBB DEFAULT_MSG TBB_INCLUDE_DIR TBB_LIBRARY TBB_LIBRARY_MALLOC)
 
-  IF (TBB_INCLUDE_DIR AND TBB_LIBRARY_MIC AND TBB_LIBRARY_MALLOC_MIC)
-    SET(TBB_FOUND_MIC TRUE)
-    SET(TBB_INCLUDE_DIRS_MIC ${TBB_INCLUDE_DIR_MIC})
-    SET(TBB_LIBRARIES_MIC ${TBB_LIBRARY_MIC} ${TBB_LIBRARY_MALLOC_MIC})
-  ENDIF()
+    if(TBB_FOUND)
+        set(TBB_INCLUDE_DIRS ${TBB_INCLUDE_DIR})
+        set(TBB_LIBRARIES ${TBB_LIBRARY} ${TBB_LIBRARY_MALLOC})
+    endif()
 
-  MARK_AS_ADVANCED(TBB_INCLUDE_DIR)
-  MARK_AS_ADVANCED(TBB_LIBRARY)
-  MARK_AS_ADVANCED(TBB_LIBRARY_MALLOC)
+    if(TBB_INCLUDE_DIR
+       AND TBB_LIBRARY_MIC
+       AND TBB_LIBRARY_MALLOC_MIC)
+        set(TBB_FOUND_MIC TRUE)
+        set(TBB_INCLUDE_DIRS_MIC ${TBB_INCLUDE_DIR_MIC})
+        set(TBB_LIBRARIES_MIC ${TBB_LIBRARY_MIC} ${TBB_LIBRARY_MALLOC_MIC})
+    endif()
 
-ENDIF()
+    mark_as_advanced(TBB_INCLUDE_DIR)
+    mark_as_advanced(TBB_LIBRARY)
+    mark_as_advanced(TBB_LIBRARY_MALLOC)
+
+endif()
 
 ##############################################################
 # Install TBB
 ##############################################################
 
-IF (FALSE) # not building -> not installing
-IF (WIN32)
-  INSTALL(PROGRAMS ${TBB_BINDIR}/tbb.dll ${TBB_BINDIR}/tbbmalloc.dll DESTINATION bin COMPONENT tutorials)
-  INSTALL(PROGRAMS ${TBB_BINDIR}/tbb.dll ${TBB_BINDIR}/tbbmalloc.dll DESTINATION lib COMPONENT libraries)
-ELSEIF (APPLE)
-  IF (NOT ENABLE_INSTALLER)
-    INSTALL(PROGRAMS ${TBB_ROOT}/lib/libc++/libtbb.dylib ${TBB_ROOT}/lib/libc++/libtbbmalloc.dylib DESTINATION lib COMPONENT libraries)
-  ENDIF()
-ELSE()
-  IF (NOT ENABLE_INSTALLER)
-    INSTALL(PROGRAMS ${TBB_ROOT}/lib/intel64/gcc4.4/libtbb.so.2 ${TBB_ROOT}/lib/intel64/gcc4.4/libtbbmalloc.so.2 DESTINATION lib COMPONENT libraries)
-  ENDIF()
-ENDIF()
-ENDIF()
+if(FALSE) # not building -> not installing
+    if(WIN32)
+        install(
+            PROGRAMS ${TBB_BINDIR}/tbb.dll ${TBB_BINDIR}/tbbmalloc.dll
+            DESTINATION bin
+            COMPONENT tutorials)
+        install(
+            PROGRAMS ${TBB_BINDIR}/tbb.dll ${TBB_BINDIR}/tbbmalloc.dll
+            DESTINATION lib
+            COMPONENT libraries)
+    elseif(APPLE)
+        if(NOT ENABLE_INSTALLER)
+            install(
+                PROGRAMS ${TBB_ROOT}/lib/libc++/libtbb.dylib ${TBB_ROOT}/lib/libc++/libtbbmalloc.dylib
+                DESTINATION lib
+                COMPONENT libraries)
+        endif()
+    else()
+        if(NOT ENABLE_INSTALLER)
+            install(
+                PROGRAMS ${TBB_ROOT}/lib/intel64/gcc4.4/libtbb.so.2 ${TBB_ROOT}/lib/intel64/gcc4.4/libtbbmalloc.so.2
+                DESTINATION lib
+                COMPONENT libraries)
+        endif()
+    endif()
+endif()
