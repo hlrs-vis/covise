f987088e5d49892c14f9d4d026d3212b14a553a5
hpcdgrie <grieger@hlrs.de>
2022-11-24 16:21:20 +0100

    Automated build environment for Windows using Ninja and VS Code

---
+++ .vscode/CMakeWrapper.bat
@@ -0,0 +1,29 @@
+@echo off
+for /f "tokens=*" %%a in (%0\..\covise.env) do (
+  set %%a
+)
+echo %*
+@echo off
+set last=0
+setlocal ENABLEDELAYEDEXPANSION
+for %%x in (%*) do (
+   if "%%x" EQU "-DCMAKE_BUILD_TYPE:STRING" (
+    set last=1
+   )
+   IF !last! == 1 (
+      if "%%x" EQU "Debug" (
+        set ARCHSUFFIX="zebu"
+      ) else if "%%x" EQU "Release" (
+        set ARCHSUFFIX="zebuopt"
+      ) else if "%%x" EQU "RelWithDebInfo" (
+        set ARCHSUFFIX="zebuopt"
+      ) else if "%%x" EQU "MinSizeRel" (
+        set ARCHSUFFIX="zebuopt"
+      )
+   )
+)
+
+cmake %*
+
+
+ 
