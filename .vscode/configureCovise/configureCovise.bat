42d1285e6ff27d1146867882eaa528df107fce99
hpcdgrie <dennis.grieger@hlrs.de>
2023-02-10 12:56:22 +0100

    Fixed VS Code taskt not editing settings

--- .vscode/configureCovise/configureCovise.bat
+++ .vscode/configureCovise/configureCovise.bat
@@ -8,7 +8,7 @@ call %COVISEDIR%/winenv.bat %ARCHSUFFIX% %QT_VERSION%
 Rem write down covise environment to reuse in vs code debugger settings and CMake wrapper
 set > %COVISEDIR%/.vscode/configureCovise/covise.env
 cd %COVISEDIR%/.vscode/configureCovise
-
+set currend_dir=%cd%
 set "compiled="
 if exist %~dp0/build/configureVsCodeSettings.exe set compiled=1
 if exist %~dp0/build/Debug\configureVsCodeSettings.exe set compiled=1
@@ -19,11 +19,12 @@ if not defined compiled (
     echo generator %generator%
     cmake -DCMAKE_PREFIX_PATH=%EXTERNLIBS% -G %GENERATOR% ..
     cmake --build .
-    if exist configureVsCodeSettings.exe (
-        configureVsCodeSettings.exe %*
-    ) else if exist Debug\ (
-        Debug\configureVsCodeSettings.exe %*
-    )
+)
+cd %currend_dir%
+if exist build\configureVsCodeSettings.exe (
+    build\configureVsCodeSettings.exe %*
+) else if exist build\Debug\ (
+    build\Debug\configureVsCodeSettings.exe %*
 )
 
 if exist %EXTERNLIBS%\stow.bat if NOT exist %EXTERNLIBS%\all call %EXTERNLIBS%\stow.bat
