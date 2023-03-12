3dc4f308084ab8201803c1dba9efe42a8d01092f
Uwe Woessner <woessner@hlrs.de>
2023-03-12 22:48:48 +0100

    Drehgeber plugin

--- src/OpenCOVER/plugins/hlrs/Drehgeber/Drehgeber.cpp
+++ src/OpenCOVER/plugins/hlrs/Drehgeber/Drehgeber.cpp
@@ -15,26 +15,27 @@
 using namespace vrml;
 
 
-class Drehgeber;
-std::vector<Drehgeber *> drehgebers;
+class VrmlNodeDrehgeber;
+std::vector<VrmlNodeDrehgeber*> drehgebers;
 
-class PLUGINEXPORT Drehgeber : public vrml::VrmlNodeChild
+class PLUGINEXPORT VrmlNodeDrehgeber : public vrml::VrmlNodeChild
 {
 public:
     static VrmlNode *creator(VrmlScene *scene)
     {
-        return new Drehgeber(scene);
+        return new VrmlNodeDrehgeber(scene);
     }
-    Drehgeber(VrmlScene *scene) : VrmlNodeChild(scene), m_index(drehgebers.size())
+    VrmlNodeDrehgeber(VrmlScene *scene) : VrmlNodeChild(scene), m_index(drehgebers.size())
     {
 
         std::cerr << "vrml Machine node created" << std::endl;
         drehgebers.push_back(this);
     }
-    ~Drehgeber()
+    ~VrmlNodeDrehgeber()
     {
         drehgebers.erase(drehgebers.begin() + m_index);
     }
+
     // Define the fields of XCar nodes
     static VrmlNodeType *defineType(VrmlNodeType *t = 0)
     {
@@ -56,7 +57,7 @@ public:
     virtual VrmlNodeType *nodeType() const { return defineType(); };
     VrmlNode *cloneMe() const
     {
-        return new Drehgeber(*this);
+        return new VrmlNodeDrehgeber(*this);
     }
     void setAngle(VrmlSFFloat position)
     {
@@ -70,18 +71,70 @@ private:
 
 
 
-COVERPLUGIN(TestTfm)
+COVERPLUGIN(Drehgeber)
 
-TestTfm::TestTfm()
+Drehgeber::Drehgeber()
 : ui::Owner("TestTfm_owner", cover->ui)
 , m_menu(new ui::Menu("TestTfm", this))
 , m_rotator(new ui::Slider(m_menu, "rotation_angle_in_degree")) {
     m_rotator->setBounds(0, 360);
 
-    VrmlNamespace::addBuiltIn(Drehgeber::defineType());
+
+    VrmlNamespace::addBuiltIn(VrmlNodeDrehgeber::defineType());
 
     m_rotator->setCallback([this](float angle, bool){
         for(auto drehgeber : drehgebers)
             drehgeber->setAngle(angle /360 * 2 * M_PI);
     });
+}
+bool Drehgeber::init()
+{
+    SerialDevice = configString("Serial", "device", "COM2");
+    int64_t br = 115200;
+    baudrate = configInt("Serial", "baudrate", br);
+    SerialDevice->setUpdater([this](std::string val) {
+        AVRClose();
+        AVRInit(val.c_str(), (int)baudrate.get()->value());
+        });
+    baudrate->setUpdater([this](int64_t val) {
+        AVRClose();
+        AVRInit(SerialDevice.get()->value().c_str(), (int)baudrate.get()->value());
+        });
+
+    AVRInit(SerialDevice.get()->value().c_str(), (int)baudrate.get()->value());
+    return true;
+}
+Drehgeber::~Drehgeber()
+{
+    AVRClose();
+}
+bool Drehgeber::update()
+{
+    float na = (float)((counter / 1000.0) * 2*M_PI);
+    if (na != angle)
+    {
+        angle = na;
+
+        for (auto drehgeber : drehgebers)
+            drehgeber->setAngle(angle);
+        return true;
+    }
+    return false; // don't request that scene be re-rendered
+}
+
+void Drehgeber::run()
+{
+    while (running)
+    {
+        int nc=0;
+        unsigned char buf[101];
+        unsigned char c;
+        do {
+        AVRReadBytes(1,&c);
+        buf[nc] = c;
+        nc++;
+        buf[nc] = '\0';
+        } while (c != '\n' && nc < 100);
+        sscanf((char *)buf, "%d", &counter);
+    }
 }
