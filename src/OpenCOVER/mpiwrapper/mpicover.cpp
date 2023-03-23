fa0201c9895aa3225922c6033e20c0c97caf6176
Martin Aumüller <aumuell@reserv.at>
2023-03-23 12:10:41 +0100

    search for configuration in $COVISEDIR/config

--- src/OpenCOVER/mpiwrapper/mpicover.cpp
+++ src/OpenCOVER/mpiwrapper/mpicover.cpp
@@ -86,6 +86,10 @@ extern "C" COEXPORT int mpi_main(MPI_Comm comm, int shmGroupRoot, pthread_barrie
     covise::coConfigConstants::setRank(myID, shmGroupRoot);
     covise::coConfigConstants::setMaster(mastername);
     opencover::config::Access config(my_hostname, mastername, myID);
+    if (auto covisedir = getenv("COVISEDIR"))
+    {
+        config.setPrefix(covisedir);
+    }
 
 #ifdef _WIN32
     // note: console has to be allocated after possible handling of argument '-d',
