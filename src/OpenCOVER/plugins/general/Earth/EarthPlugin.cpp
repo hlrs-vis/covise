51a871e12367948e199669409695720a5a2038dd
Martin Aumüller <aumuell@reserv.at>
2023-07-25 22:21:07 +0200

    compile fix for osgEarth 3.4

--- src/OpenCOVER/plugins/general/Earth/EarthPlugin.cpp
+++ src/OpenCOVER/plugins/general/Earth/EarthPlugin.cpp
@@ -175,7 +175,6 @@ struct KMLUIBuilder : public osg::NodeVisitor
 EarthPlugin::EarthPlugin()
 : coVRPlugin(COVER_PLUGIN_NAME)
 {
-    int _maxLights = Registry::instance()->getCapabilities().getMaxLights();
     plugin = this;
     mapNode = NULL;
     oldDump = NULL;
