ec4feaebf7b9ac1e26b26b20f40530b62dd7bb7c
Martin Aumüller <aumuell@reserv.at>
2023-08-08 17:34:38 +0200

    update from vistle

--- src/module/general/ReadFoam/archivemodel.cpp
+++ src/module/general/ReadFoam/archivemodel.cpp
@@ -18,144 +18,142 @@
 namespace {
 
 struct Cleaner {
+    Cleaner(std::function<void()> task): task(std::move(task)) {}
 
-    Cleaner(std::function<void()> task)
-        : task(std::move(task)) {
-    }
+    virtual ~Cleaner() { clean(); }
 
-    virtual ~Cleaner() {
-        clean();
-    }
-
-    void clean() {
+    void clean()
+    {
         if (task)
             task();
         reset();
     }
 
-    void reset(std::function<void()> ntask = std::function<void()>()) {
-        task = std::move(ntask);
-    }
+    void reset(std::function<void()> ntask = std::function<void()>()) { task = std::move(ntask); }
     std::function<void()> task;
 };
 
-}
+} // namespace
 
 namespace fs {
 
-Directory::Directory(const Directory *dir, const std::string &name)
-: Entry(dir, name)
+Directory::Directory(const Directory *dir, const std::string &name): Entry(dir, name)
 {}
 
-Directory::Directory(const Model *m)
-: Entry(m)
+Directory::Directory(const Model *m): Entry(m)
 {}
 
 
-Directory *Directory::findDirectory(const std::string &name) {
-   auto it = dirs.find(name);
-   if (it == dirs.end())
-       return nullptr;
-   return &it->second;
+Directory *Directory::findDirectory(const std::string &name)
+{
+    auto it = dirs.find(name);
+    if (it == dirs.end())
+        return nullptr;
+    return &it->second;
 }
-const Directory *Directory::findDirectory(const std::string &name) const {
-   auto it = dirs.find(name);
-   if (it == dirs.end())
-       return nullptr;
-   return &it->second;
+const Directory *Directory::findDirectory(const std::string &name) const
+{
+    auto it = dirs.find(name);
+    if (it == dirs.end())
+        return nullptr;
+    return &it->second;
 }
 
-File *Directory::findFile(const std::string &name) {
+File *Directory::findFile(const std::string &name)
+{
     auto it = files.find(name);
     if (it == files.end())
         return nullptr;
-   return &it->second;
+    return &it->second;
 }
-const File *Directory::findFile(const std::string &name) const {
+const File *Directory::findFile(const std::string &name) const
+{
     auto it = files.find(name);
     if (it == files.end())
         return nullptr;
-   return &it->second;
+    return &it->second;
 }
 
-Entry *Directory::find(const std::string &name) {
+Entry *Directory::find(const std::string &name)
+{
     if (auto ent = findDirectory(name))
         return ent;
     return findFile(name);
 }
-const Entry *Directory::find(const std::string &name) const {
+const Entry *Directory::find(const std::string &name) const
+{
     if (auto ent = findDirectory(name))
         return ent;
     return findFile(name);
 }
 
-Directory *Directory::addDirectory(const std::string &name) {
-   if (auto d = findDirectory(name))
-      return d;
-   if (findFile(name))
-      return nullptr;
-   auto result = dirs.emplace(name, Directory(this, name));
-   return &result.first->second;
+Directory *Directory::addDirectory(const std::string &name)
+{
+    if (auto d = findDirectory(name))
+        return d;
+    if (findFile(name))
+        return nullptr;
+    auto result = dirs.emplace(name, Directory(this, name));
+    return &result.first->second;
 }
 
-File *Directory::addFile(const std::string &name) {
-   if (auto f = findFile(name))
-      return f;
-   if (findDirectory(name))
-      return nullptr;
-   auto result = files.emplace(name, File(this, name));
-   return &result.first->second;
+File *Directory::addFile(const std::string &name)
+{
+    if (auto f = findFile(name))
+        return f;
+    if (findDirectory(name))
+        return nullptr;
+    auto result = files.emplace(name, File(this, name));
+    return &result.first->second;
 }
 
-File::File(const Directory *dir, const std::string &name)
-: Entry(dir, name)
+File::File(const Directory *dir, const std::string &name): Entry(dir, name)
 {}
 
-Entry::Entry(const Directory *parent, std::string name)
-: model(parent->model)
-, parent(parent)
-, name(std::move(name))
+Entry::Entry(const Directory *parent, std::string name): model(parent->model), parent(parent), name(std::move(name))
 {}
 
-Entry::Entry(const Model *model)
-: model(model)
+Entry::Entry(const Model *model): model(model)
 {}
 
 Entry::~Entry() = default;
 
-bool Entry::operator<(const std::string &other) const {
+bool Entry::operator<(const std::string &other) const
+{
     return name < other;
 }
 
-const std::string &Entry::string() const {
+const std::string &Entry::string() const
+{
     return name;
 }
 
-Path Entry::path() const {
+Path Entry::path() const
+{
     if (!parent)
         return Path(model, name);
 
     return parent->path() / name;
 }
 
-fs::Entry::operator Path() const {
+fs::Entry::operator Path() const
+{
     return path();
 }
 
-bool Entry::operator<(const Entry &other) const {
+bool Entry::operator<(const Entry &other) const
+{
     return name < other.name;
 }
 
-Path::Path(const Model &model)
-: model(&model) {
-}
-
-Path::Path(const Model *model)
-: model(model)
+Path::Path(const Model &model): model(&model)
 {}
 
-Path::Path(const Model *model, const std::string &path)
-: model(model) {
+Path::Path(const Model *model): model(model)
+{}
+
+Path::Path(const Model *model, const std::string &path): model(model)
+{
     if (path.empty()) {
         absolute = false;
         return;
@@ -163,15 +161,15 @@ Path::Path(const Model *model, const std::string &path)
     if (path.front() != '/')
         absolute = false;
     std::vector<std::string> c;
-    boost::split(c, path, [](char c){return c=='/';}, boost::token_compress_on);
+    boost::split(
+        c, path, [](char c) { return c == '/'; }, boost::token_compress_on);
     std::copy(c.begin(), c.end(), std::back_inserter(components));
 }
 
-Path::Path(const Entry &entry)
-: model(entry.model)
+Path::Path(const Entry &entry): model(entry.model)
 {
     std::vector<std::string> c;
-    for (const Entry *e=&entry; e; e=e->parent) {
+    for (const Entry *e = &entry; e; e = e->parent) {
         c.emplace_back(e->string());
         if (e->parent == &model->root) {
             absolute = true;
@@ -182,13 +180,15 @@ Path::Path(const Entry &entry)
     std::copy(c.rbegin(), c.rend(), std::back_inserter(components));
 }
 
-Path Path::filename() const {
+Path Path::filename() const
+{
     if (components.empty())
         return Path(model);
     return Path(model, components.back());
 }
 
-Path Path::stem() const {
+Path Path::stem() const
+{
     auto fn = filename().string();
     auto pos = fn.find('.');
     if (pos == std::string::npos)
@@ -197,7 +197,8 @@ Path Path::stem() const {
     return Path(model, fn.substr(0, pos));
 }
 
-Path Path::extension() const {
+Path Path::extension() const
+{
     auto fn = filename().string();
     auto pos = fn.find('.');
     if (pos == std::string::npos)
@@ -206,7 +207,8 @@ Path Path::extension() const {
     return Path(model, fn.substr(pos));
 }
 
-std::string Path::string() const {
+std::string Path::string() const
+{
     std::string path;
     for (auto it = components.begin(); it != components.end(); ++it) {
         if (it != components.begin())
@@ -216,7 +218,8 @@ std::string Path::string() const {
     return path;
 }
 
-Path Path::operator+(const std::string &name) const {
+Path Path::operator+(const std::string &name) const
+{
     Path path(*this);
     if (path.components.empty()) {
         path /= name;
@@ -226,27 +229,31 @@ Path Path::operator+(const std::string &name) const {
     return path;
 }
 
-Path Path::operator/(const std::string &name) const {
+Path Path::operator/(const std::string &name) const
+{
     Path path(*this);
     return path /= name;
 }
 
-Path &Path::operator/=(const std::string &name) {
+Path &Path::operator/=(const std::string &name)
+{
     if (!name.empty())
         components.emplace_back(name);
     return *this;
 }
 
-bool Path::is_directory() const {
+bool Path::is_directory() const
+{
     return model->isDirectory(*this);
-
 }
 
-const Model *Path::getModel() const {
+const Model *Path::getModel() const
+{
     return model;
 }
 
-bool Path::exists() const {
+bool Path::exists() const
+{
     return model->exists(*this);
 }
 
@@ -255,7 +262,8 @@ class ModelPrivate {
     friend class ::archive_streambuf;
 
 public:
-    ~ModelPrivate() {
+    ~ModelPrivate()
+    {
 #ifdef HAVE_LIBZIP
         if (zipfile)
             zip_close(zipfile);
@@ -268,9 +276,7 @@ public:
 };
 
 Model::Model(const std::string &archiveOrDirectory, Format format)
-    : format(format)
-    , container(archiveOrDirectory)
-    , root(this)
+: format(format), container(archiveOrDirectory), root(this)
 {
     d.reset(new ModelPrivate);
 #ifdef HAVE_LIBZIP
@@ -279,7 +285,7 @@ Model::Model(const std::string &archiveOrDirectory, Format format)
         archive = true;
         format = FormatZip;
         int64_t nument = zip_get_num_entries(d->zipfile, 0);
-        for (int64_t idx=0; idx<nument; ++idx) {
+        for (int64_t idx = 0; idx < nument; ++idx) {
             std::string pathname = zip_get_name(d->zipfile, idx, 0);
             if (auto file = dynamic_cast<File *>(addPath(pathname))) {
                 file->index = idx;
@@ -290,7 +296,8 @@ Model::Model(const std::string &archiveOrDirectory, Format format)
     }
 #else
     if (format == FormatZip) {
-        throw std::runtime_error("failed to open archive " + archiveOrDirectory + " in zip format: not compiled with libzip");
+        throw std::runtime_error("failed to open archive " + archiveOrDirectory +
+                                 " in zip format: not compiled with libzip");
     }
 #endif
 
@@ -300,9 +307,7 @@ Model::Model(const std::string &archiveOrDirectory, Format format)
 #else
         archive = true;
         struct archive *a = archive_read_new();
-        Cleaner archiveCleaner([a](){
-            archive_read_free(a);
-        });
+        Cleaner archiveCleaner([a]() { archive_read_free(a); });
 
         archive_read_support_format_zip(a);
         archive_read_support_format_tar(a);
@@ -311,22 +316,16 @@ Model::Model(const std::string &archiveOrDirectory, Format format)
             throw std::runtime_error("failed to open archive " + archiveOrDirectory);
         }
 
-#ifndef HAVE_LIBARCHIVE_READ_CURRENT_POSITION
         File *prev = nullptr;
-#endif
         struct archive_entry *entry = nullptr;
         while (archive_read_next_header(a, &entry) == ARCHIVE_OK) {
-#ifndef HAVE_LIBARCHIVE_READ_CURRENT_POSITION
             if (prev) {
                 prev->offset = (archive_read_header_position(a) - prev->size) & ~(int64_t)0x1ff;
                 //std::cerr << prev->path().string() << " OFFSET: " << prev->offset << std::endl;
             }
-#endif
             std::string pathname = archive_entry_pathname(entry);
             auto type = archive_entry_filetype(entry);
-#ifndef HAVE_LIBARCHIVE_READ_CURRENT_POSITION
             prev = nullptr;
-#endif
             if (type == AE_IFDIR) {
                 /* auto dir = dynamic_cast<Directory *>(addPath(pathname)); */
             } else if (type == AE_IFREG) {
@@ -334,39 +333,34 @@ Model::Model(const std::string &archiveOrDirectory, Format format)
                 if (file) {
                     size_t sz = archive_entry_size(entry);
                     file->size = sz;
-#ifdef HAVE_LIBARCHIVE_READ_CURRENT_POSITION
-                    int64_t off = archive_read_current_position(a);
-#else
                     int64_t off = archive_read_header_position(a);
-#endif
                     file->offset = off;
                     //std::cerr << file->path().string() << " OFFSET: " << file->offset  << "+" << file->size << std::endl;
                 }
-#ifndef HAVE_LIBARCHIVE_READ_CURRENT_POSITION
                 prev = file;
-#endif
             }
             //archive_read_data_skip(a); // not necessary, and might be detrimental to performance
         }
-#ifndef HAVE_LIBARCHIVE_READ_CURRENT_POSITION
         if (prev) {
             prev->offset = (archive_read_header_position(a) - prev->size) & ~(int64_t)0x1ff;
             //std::cerr << prev->path().string() << " OFFSET: " << prev->offset << std::endl;
         }
 #endif
-#endif
     }
 }
 
-Model::operator Directory() const {
-   return root;
+Model::operator Directory() const
+{
+    return root;
 }
 
-Entry *Model::addPath(const std::string &path) {
+Entry *Model::addPath(const std::string &path)
+{
     std::vector<std::string> components;
-    boost::split(components, path, [](char c){return c=='/';}, boost::token_compress_on);
+    boost::split(
+        components, path, [](char c) { return c == '/'; }, boost::token_compress_on);
     Directory *dir = &root;
-    for (size_t i=0; i+1<components.size(); ++i) {
+    for (size_t i = 0; i + 1 < components.size(); ++i) {
         File *f = dir->findFile(components[i]);
         if (f) {
             std::cerr << "already have file of same name" << std::endl;
@@ -389,19 +383,21 @@ Entry *Model::addPath(const std::string &path) {
     return f;
 }
 
-bool Model::isDirectory(const std::string &path) const {
+bool Model::isDirectory(const std::string &path) const
+{
     if (path.empty())
         return true;
 
     return path.back() == '/';
 }
 
-bool Model::exists(const Path &path, bool requireDirectory) const {
+bool Model::exists(const Path &path, bool requireDirectory) const
+{
     if (path.components.empty())
         return true;
 
     const Directory *dir = &root;
-    for (size_t i=0; i+1<path.components.size(); ++i) {
+    for (size_t i = 0; i + 1 < path.components.size(); ++i) {
         const auto &c = path.components[i];
         auto d = dir->findDirectory(c);
         if (!d)
@@ -412,7 +408,8 @@ bool Model::exists(const Path &path, bool requireDirectory) const {
     return requireDirectory ? dir->findDirectory(path.components.back()) : dir->find(path.components.back());
 }
 
-const Entry *Model::findEntry(const Path &path) const {
+const Entry *Model::findEntry(const Path &path) const
+{
     return findEntry(path.components);
 }
 
@@ -422,7 +419,7 @@ const Entry *Model::findEntry(const std::vector<std::string> &pathcomponents) co
         return &root;
 
     const Directory *dir = &root;
-    for (size_t i=0; i+1<pathcomponents.size(); ++i) {
+    for (size_t i = 0; i + 1 < pathcomponents.size(); ++i) {
         const auto &c = pathcomponents[i];
         auto d = dir->findDirectory(c);
         if (!d)
@@ -433,23 +430,27 @@ const Entry *Model::findEntry(const std::vector<std::string> &pathcomponents) co
     return dir->find(pathcomponents.back());
 }
 
-const Directory *Model::findDirectory(const Path &path) const {
-
+const Directory *Model::findDirectory(const Path &path) const
+{
     return dynamic_cast<const Directory *>(findEntry(path));
 }
 
-const File *Model::findFile(const std::string &pathname) const {
+const File *Model::findFile(const std::string &pathname) const
+{
     std::vector<std::string> components;
-    boost::split(components, pathname, [](char c){return c=='/';}, boost::token_compress_on);
+    boost::split(
+        components, pathname, [](char c) { return c == '/'; }, boost::token_compress_on);
     auto e = findEntry(components);
     return dynamic_cast<const File *>(e);
 }
 
-const std::string &Model::getContainer() const {
+const std::string &Model::getContainer() const
+{
     return container;
 }
 
-bool Model::isDirectory(const Path &path) const {
+bool Model::isDirectory(const Path &path) const
+{
     return exists(path, true);
 }
 
@@ -470,7 +471,8 @@ DirectoryIterator::DirectoryIterator(const Path &path)
 
 DirectoryIterator &DirectoryIterator::operator=(const DirectoryIterator &other) = default;
 
-bool DirectoryIterator::operator==(const DirectoryIterator &other) const {
+bool DirectoryIterator::operator==(const DirectoryIterator &other) const
+{
     if (dir != other.dir)
         return false;
     if (dir == nullptr)
@@ -482,33 +484,38 @@ bool DirectoryIterator::operator==(const DirectoryIterator &other) const {
     return true;
 }
 
-bool DirectoryIterator::operator!=(const DirectoryIterator &other) const {
-   return !(*this == other);
+bool DirectoryIterator::operator!=(const DirectoryIterator &other) const
+{
+    return !(*this == other);
 }
 
-DirectoryIterator DirectoryIterator::operator++(int) {
+DirectoryIterator DirectoryIterator::operator++(int)
+{
     auto ret = *this;
     ++*this;
     return ret;
 }
 
-const Entry &DirectoryIterator::operator*() const {
+const Entry &DirectoryIterator::operator*() const
+{
     assert(dir);
     if (dit != dir->dirs.end())
-       return dit->second;
+        return dit->second;
     assert(fit != dir->files.end());
     return fit->second;
 }
 
-const Entry *DirectoryIterator::operator->() const {
+const Entry *DirectoryIterator::operator->() const
+{
     assert(dir);
     if (dit != dir->dirs.end())
-       return &dit->second;
+        return &dit->second;
     assert(fit != dir->files.end());
     return &fit->second;
 }
 
-DirectoryIterator &DirectoryIterator::operator++() {
+DirectoryIterator &DirectoryIterator::operator++()
+{
     assert(dir);
     if (dit != dir->dirs.end()) {
         ++dit;
@@ -522,22 +529,26 @@ DirectoryIterator &DirectoryIterator::operator++() {
     return *this;
 }
 
-bool is_directory(const Entry &e) {
+bool is_directory(const Entry &e)
+{
     return dynamic_cast<const Directory *>(&e) != nullptr;
 }
 
-bool is_directory(const Path &path) {
+bool is_directory(const Path &path)
+{
     return path.is_directory();
 }
 
-bool exists(const Path &path) {
+bool exists(const Path &path)
+{
     return path.exists();
 }
 
 } // namespace fs
 
 
-archive_streambuf::archive_streambuf(const fs::File *file) {
+archive_streambuf::archive_streambuf(const fs::File *file)
+{
     auto end = buf + sizeof(buf);
     setg(end, end, end);
 
@@ -547,8 +558,7 @@ archive_streambuf::archive_streambuf(const fs::File *file) {
     if (file->index >= 0) {
         auto &zipfile = file->model->d->zipfile;
         zip = zip_fopen_index(zipfile, file->index, 0);
-    }
-    else
+    } else
 #endif
     {
 #ifdef HAVE_LIBARCHIVE
@@ -579,18 +589,19 @@ archive_streambuf::archive_streambuf(const fs::File *file) {
         archive = nullptr;
         throw std::runtime_error("did not find " + file->pathname + " in archive " + container);
 #else
-        throw std::runtime_error("cannot read from " + file->pathname + " from " + container + ": not compiled with libarchive");
+        throw std::runtime_error("cannot read from " + file->pathname + " from " + container +
+                                 ": not compiled with libarchive");
 #endif
     }
 }
 
-archive_streambuf::~archive_streambuf() {
+archive_streambuf::~archive_streambuf()
+{
 #ifdef HAVE_LIBZIP
     if (zip) {
         auto z = static_cast<zip_file *>(zip);
         zip_fclose(z);
-    }
-    else
+    } else
 #endif
     {
 #ifdef HAVE_LIBARCHIVE
@@ -602,7 +613,8 @@ archive_streambuf::~archive_streambuf() {
     }
 }
 
-std::streambuf::int_type archive_streambuf::underflow() {
+std::streambuf::int_type archive_streambuf::underflow()
+{
 #ifdef HAVE_LIBZIP
     if (zip) {
         auto z = static_cast<zip_file *>(zip);
@@ -613,9 +625,8 @@ std::streambuf::int_type archive_streambuf::underflow() {
         if (n == 0)
             return EOF;
         nread += n;
-        setg(buf, buf, buf+n);
-    }
-    else
+        setg(buf, buf, buf + n);
+    } else
 #endif
     {
 #ifdef HAVE_LIBARCHIVE
@@ -624,7 +635,7 @@ std::streambuf::int_type archive_streambuf::underflow() {
         if (n == 0)
             return EOF;
         nread += n;
-        setg(buf, buf, buf+n);
+        setg(buf, buf, buf + n);
 #endif
     }
     return traits_type::to_int_type(*gptr());
