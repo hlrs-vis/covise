eb47680238b7236db9f4702392cf69b009716dd5
hpcdgrie <grieger@hlrs.de>
2023-08-18 13:51:35 +0200

    Anly do opc-ua communication on master and broadcast results to slaves

--- src/OpenCOVER/plugins/general/ToolMachine/opcua.cpp
+++ src/OpenCOVER/plugins/general/ToolMachine/opcua.cpp
@@ -12,18 +12,23 @@
 #include <iostream>
 
 #include <cover/ui/VectorEditField.h>
+#include <cover/coVRMSController.h>
 
+struct DoubleCallback
+{
+ std::function<void(double)> cb;
+ double val = 0;
+};
 
-std::map<UA_UInt32, std::function<void(double)>> doubleCallbacks;
+std::map<UA_UInt32, DoubleCallback> doubleCallbacks;
+auto msController = opencover::coVRMSController::instance();
 
 static void handler_TheAnswerChanged(UA_Client *client, UA_UInt32 subId, void *subContext,
                          UA_UInt32 monId, void *monContext, UA_DataValue *value) {
     
 
     if(UA_Variant_hasScalarType(&value->value, &UA_TYPES[UA_TYPES_DOUBLE])) {
-        std::cerr << "the value is: "  << *(UA_Double*)value->value.data << std::endl;
-        doubleCallbacks[*(UA_UInt32 *)monContext](*(UA_Double*)value->value.data);
-
+        doubleCallbacks[*(UA_UInt32 *)monContext].val = *(UA_Double*)value->value.data;
     }
     else{
         std::cerr << "wrong type " << value->value.type->typeKind << std::endl;
@@ -154,143 +159,91 @@ void OpcUaClient::listVariables(UA_Client* client)
 
 bool OpcUaClient::connect()
 {
-/* Create the server and set its config */
-    client = UA_Client_new();
-    UA_ClientConfig *cc = UA_Client_getConfig(client);
-
-    /* Set securityMode and securityPolicyUri */
-    cc->securityMode = UA_MESSAGESECURITYMODE_SIGNANDENCRYPT;
-    cc->securityPolicyUri = UA_STRING_NULL;
-
-    UA_ByteString certificate = loadFile(m_certificate->getValue().c_str());
-    UA_ByteString privateKey  = loadFile(m_key->getValue().c_str());
-
-    /* If no trust list is passed, all certificates are accepted. */
-    UA_ClientConfig_setDefaultEncryption(cc, certificate, privateKey,
-                                            NULL, 0, NULL, 0);
-    UA_CertificateVerification_AcceptAll(&cc->certificateVerification);
-    UA_ByteString_clear(&certificate);
-    UA_ByteString_clear(&privateKey);
-    
-
-
-    /* The application URI must be the same as the one in the certificate.
-     * The script for creating a self-created certificate generates a certificate
-     * with the Uri specified below.*/
-    UA_ApplicationDescription_clear(&cc->clientDescription);
-    cc->clientDescription.applicationUri = UA_STRING_ALLOC("urn:open62541.server.application");
-    cc->clientDescription.applicationType = UA_APPLICATIONTYPE_CLIENT;
-
-    /* Connect to the server */
-    UA_StatusCode retval = UA_STATUSCODE_GOOD;
-    UA_ClientConfig_setAuthenticationUsername(cc, m_username->getValue().c_str(), m_password->getValue().c_str());
-    retval = UA_Client_connect(client, m_serverIp->getValue().c_str());
-    /* Alternative */
-    //retval = UA_Client_connectUsername(client, serverurl, username, password);
-
-    if(retval != UA_STATUSCODE_GOOD) {
-        UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "Could not connect");
-        UA_Client_delete(client);
-        return false;
-    }
-
-    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "Connected!");
-
+    if(msController->isMaster())
     {
-        /* Read the server-time */
-        UA_Variant value;
-        UA_Variant_init(&value);
-        UA_Client_readValueAttribute(client,
-                UA_NODEID_NUMERIC(0, UA_NS0ID_SERVER_SERVERSTATUS_CURRENTTIME),
-                &value);
+
+        /* Create the server and set its config */
+        client = UA_Client_new();
+        UA_ClientConfig *cc = UA_Client_getConfig(client);
+
+        /* Set securityMode and securityPolicyUri */
+        cc->securityMode = UA_MESSAGESECURITYMODE_SIGNANDENCRYPT;
+        cc->securityPolicyUri = UA_STRING_NULL;
+
+        UA_ByteString certificate = loadFile(m_certificate->getValue().c_str());
+        UA_ByteString privateKey  = loadFile(m_key->getValue().c_str());
+
+        /* If no trust list is passed, all certificates are accepted. */
+        UA_ClientConfig_setDefaultEncryption(cc, certificate, privateKey,
+                                                NULL, 0, NULL, 0);
+        UA_CertificateVerification_AcceptAll(&cc->certificateVerification);
+        UA_ByteString_clear(&certificate);
+        UA_ByteString_clear(&privateKey);
         
-        if(UA_Variant_hasScalarType(&value, &UA_TYPES[UA_TYPES_DATETIME])) {
-            UA_DateTimeStruct dts = UA_DateTime_toStruct(*(UA_DateTime *)value.data);
-            UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND,
-                        "The server date is: %02u-%02u-%04u %02u:%02u:%02u.%03u",
-                        dts.day, dts.month, dts.year, dts.hour, dts.min, dts.sec, dts.milliSec);
+
+
+        /* The application URI must be the same as the one in the certificate.
+        * The script for creating a self-created certificate generates a certificate
+        * with the Uri specified below.*/
+        UA_ApplicationDescription_clear(&cc->clientDescription);
+        cc->clientDescription.applicationUri = UA_STRING_ALLOC("urn:open62541.server.application");
+        cc->clientDescription.applicationType = UA_APPLICATIONTYPE_CLIENT;
+
+        /* Connect to the server */
+        UA_StatusCode retval = UA_STATUSCODE_GOOD;
+        UA_ClientConfig_setAuthenticationUsername(cc, m_username->getValue().c_str(), m_password->getValue().c_str());
+        retval = UA_Client_connect(client, m_serverIp->getValue().c_str());
+        /* Alternative */
+        //retval = UA_Client_connectUsername(client, serverurl, username, password);
+
+        if(retval != UA_STATUSCODE_GOOD) {
+            UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "Could not connect");
+            UA_Client_delete(client);
+            auto b = msController->syncBool(false);
+            return false;
         }
-        UA_Variant_clear(&value);
+
+        UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "Connected!");
+
+        {
+            /* Read the server-time */
+            UA_Variant value;
+            UA_Variant_init(&value);
+            UA_Client_readValueAttribute(client,
+                    UA_NODEID_NUMERIC(0, UA_NS0ID_SERVER_SERVERSTATUS_CURRENTTIME),
+                    &value);
+            
+            if(UA_Variant_hasScalarType(&value, &UA_TYPES[UA_TYPES_DATETIME])) {
+                UA_DateTimeStruct dts = UA_DateTime_toStruct(*(UA_DateTime *)value.data);
+                UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND,
+                            "The server date is: %02u-%02u-%04u %02u:%02u:%02u.%03u",
+                            dts.day, dts.month, dts.year, dts.hour, dts.min, dts.sec, dts.milliSec);
+            }
+            UA_Variant_clear(&value);
+        }
+        listVariables(client);
+        if(m_onConnect)
+            m_onConnect();
+        auto b = msController->syncBool(true);
+        return true;
+    } else{
+        return msController->syncBool(false);
     }
-    listVariables(client);
-    if(m_onConnect)
-        m_onConnect();
-    return true;
-    // registerDouble("CURRENT|A", [](double){
-    //     std::cerr << "hello world" << std::endl;
-    // });
-
-
-//     /* Read attribute */
-//     int32_t value = 0;
-//     printf("\nReading the value of node (1, \"the.answer\"):\n");
-//     UA_Variant *val = UA_Variant_new();
-//     retval = UA_Client_readValueAttribute(client, UA_NODEID_STRING(1, (char*)"the.answer"), val);
-//     if(retval == UA_STATUSCODE_GOOD && UA_Variant_isScalar(val) &&
-//        val->type == &UA_TYPES[UA_TYPES_INT32]) {
-//             value = *(int32_t*)val->data;
-//             printf("the value is: %i\n", value);
-//     }
-//     UA_Variant_delete(val);
-
-//     /* Write node attribute */
-//     value++;
-//     printf("\nWriting a value of node (1, \"the.answer\"):\n");
-//     UA_WriteRequest wReq;
-//     UA_WriteRequest_init(&wReq);
-//     wReq.nodesToWrite = UA_WriteValue_new();
-//     wReq.nodesToWriteSize = 1;
-//     wReq.nodesToWrite[0].nodeId = UA_NODEID_STRING_ALLOC(1, "the.answer");
-//     wReq.nodesToWrite[0].attributeId = UA_ATTRIBUTEID_VALUE;
-//     wReq.nodesToWrite[0].value.hasValue = true;
-//     wReq.nodesToWrite[0].value.value.type = &UA_TYPES[UA_TYPES_INT32];
-//     wReq.nodesToWrite[0].value.value.storageType = UA_VARIANT_DATA_NODELETE; /* do not free the integer on deletion */
-//     wReq.nodesToWrite[0].value.value.data = &value;
-//     UA_WriteResponse wResp = UA_Client_Service_write(client, wReq);
-//     if(wResp.responseHeader.serviceResult == UA_STATUSCODE_GOOD)
-//             printf("the new value is: %i\n", value);
-//     UA_WriteRequest_clear(&wReq);
-//     UA_WriteResponse_clear(&wResp);
-
-//     /* Write node attribute (using the highlevel API) */
-//     value++;
-//     UA_Variant *myVariant = UA_Variant_new();
-//     UA_Variant_setScalarCopy(myVariant, &value, &UA_TYPES[UA_TYPES_INT32]);
-//     UA_Client_writeValueAttribute(client, UA_NODEID_STRING(1, (char*)"the.answer"), myVariant);
-//     UA_Variant_delete(myVariant);
-
-
-
-// #ifdef UA_ENABLE_METHODCALLS
-//     /* Call a remote method */
-//     UA_Variant input;
-//     UA_String argString = UA_STRING((char*)"Hello Server");
-//     UA_Variant_init(&input);
-//     UA_Variant_setScalarCopy(&input, &argString, &UA_TYPES[UA_TYPES_STRING]);
-//     size_t outputSize;
-//     UA_Variant *output;
-//     retval = UA_Client_call(client, UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER),
-//                             UA_NODEID_NUMERIC(1, 62541), 1, &input, &outputSize, &output);
-//     if(retval == UA_STATUSCODE_GOOD) {
-//         printf("Method call was successful, and %lu returned values available.\n",
-//                (unsigned long)outputSize);
-//         UA_Array_delete(output, outputSize, &UA_TYPES[UA_TYPES_VARIANT]);
-//     } else {
-//         printf("Method call was unsuccessful, and %x returned values available.\n", retval);
-//     }
-//     UA_Variant_clear(&input);
-// #endif
+  
 
 }
 
 bool OpcUaClient::disconnect()
 {
-    /* Clean up */
-    if(m_onDisconnect)
-        m_onDisconnect();
-    UA_Client_disconnect(client);
-    UA_Client_delete(client);
-    client = nullptr;
+    if(opencover::coVRMSController::instance()->isMaster())
+    {
+        /* Clean up */
+        if(m_onDisconnect)
+            m_onDisconnect();
+        UA_Client_disconnect(client);
+        UA_Client_delete(client);
+        client = nullptr;
+    }
     return true;
 }
 
@@ -299,35 +252,59 @@ bool OpcUaClient::registerDouble(const std::string &name, const std::function<vo
     auto fieldIt = m_numericalNodes.find(name);
     if(fieldIt == m_numericalNodes.end())
         return false;
-    /* Create a subscription */
-    UA_CreateSubscriptionRequest request = UA_CreateSubscriptionRequest_default();
-    UA_CreateSubscriptionResponse response = UA_Client_Subscriptions_create(client, request,
-                                                                            NULL, NULL, NULL);
+    bool retval = false;
+    if(msController->isMaster())
+    {
+        /* Create a subscription */
+        UA_CreateSubscriptionRequest request = UA_CreateSubscriptionRequest_default();
+        UA_CreateSubscriptionResponse response = UA_Client_Subscriptions_create(client, request,
+                                                                                NULL, NULL, NULL);
 
-    UA_UInt32 subId = response.subscriptionId;
-    auto cbIt = doubleCallbacks.emplace(std::make_pair(subId, cb)).first;                        
-    if(response.responseHeader.serviceResult != UA_STATUSCODE_GOOD)
-        return false;
-    UA_MonitoredItemCreateRequest monRequest =
-        UA_MonitoredItemCreateRequest_default(UA_NODEID_NUMERIC(fieldIt->second.nameSpace, fieldIt->second.nodeId));
+        msController->syncData(&response.subscriptionId, sizeof(UA_UInt32));
+        UA_UInt32 subId = response.subscriptionId;
+        auto cbIt = doubleCallbacks.emplace(std::make_pair(subId, DoubleCallback{cb})).first;                        
+        if(response.responseHeader.serviceResult != UA_STATUSCODE_GOOD)
+        {
+            auto b = msController->syncBool(false);
+            return false;
+        }
+        UA_MonitoredItemCreateRequest monRequest =
+            UA_MonitoredItemCreateRequest_default(UA_NODEID_NUMERIC(fieldIt->second.nameSpace, fieldIt->second.nodeId));
 
-    UA_MonitoredItemCreateResult monResponse =
-    UA_Client_MonitoredItems_createDataChange(client, response.subscriptionId,
-                                              UA_TIMESTAMPSTORETURN_BOTH,
-                                              monRequest, const_cast<UA_UInt32*>(&cbIt->first), handler_TheAnswerChanged, NULL);
-    if(monResponse.statusCode != UA_STATUSCODE_GOOD)
-        return false;
-    return true;
-
-    /* The first publish request should return the initial value of the variable */
-   
+        UA_MonitoredItemCreateResult monResponse =
+        UA_Client_MonitoredItems_createDataChange(client, response.subscriptionId,
+                                                UA_TIMESTAMPSTORETURN_BOTH,
+                                                monRequest, const_cast<UA_UInt32*>(&cbIt->first), handler_TheAnswerChanged, NULL);
+        if(monResponse.statusCode != UA_STATUSCODE_GOOD)
+        {   
+            auto b = msController->syncBool(false);
+            return false;
+        }
+        auto b = msController->syncBool(true);
+        return true;
+    } else {
+        UA_UInt32 subId;
+        msController->syncData(&subId, sizeof(UA_UInt32));
+        
+        doubleCallbacks.emplace(std::make_pair(subId, DoubleCallback{cb}));
+        bool retval = msController->syncBool(false);
+        return retval;
+    }
 }
 
 void OpcUaClient::update()
 {
-    if(client)
+    if(msController->isMaster())
     {
-        UA_Client_run_iterate(client, 0);
+        if(client)
+        {
+            UA_Client_run_iterate(client, 0);
+        }
+    } 
+    for(auto &val : doubleCallbacks)
+    {
+        msController->syncData(&val.second.val, sizeof(double));
+        val.second.cb(val.second.val);
     }
 }
 
