aa4468ac18c396e721cd2f466d2c5be68a0fb5cc
Uwe Woessner <woessner@hlrs.de>
2023-09-05 18:10:46 +0200

    support for multiple machines

--- src/OpenCOVER/plugins/general/ToolMachine/ToolMachine.cpp
+++ src/OpenCOVER/plugins/general/ToolMachine/ToolMachine.cpp
@@ -5,6 +5,7 @@
 #include <vrml97/vrml/VrmlNodeType.h>
 #include <vrml97/vrml/VrmlNamespace.h>
 #include <vrml97/vrml/VrmlSFVec3f.h>
+#include <vrml97/vrml/VrmlMFString.h>
 #include <vrml97/vrml/VrmlNodeChild.h>
 #include <util/coExport.h>
 #include <vrml97/vrml/VrmlScene.h>
@@ -67,7 +68,48 @@ public:
         }
          t->addEventOut("aAxisYOffsetPos", VrmlField::SFVEC3F);
          t->addEventOut("aAxisYOffsetNeg", VrmlField::SFVEC3F);
+         t->addExposedField("MachineName", VrmlField::SFSTRING);
+         t->addExposedField("XAxis", VrmlField::SFVEC3F);
+         t->addExposedField("YAxis", VrmlField::SFVEC3F);
+         t->addExposedField("ZAxis", VrmlField::SFVEC3F);
+         t->addExposedField("AAxis", VrmlField::SFVEC3F);
+         t->addExposedField("BAxis", VrmlField::SFVEC3F);
+         t->addExposedField("CAxis", VrmlField::SFVEC3F);
+         t->addExposedField("AxisNames", VrmlField::MFSTRING);
         return t;
+    }
+
+    // Set the value of one of the node fields.
+
+    void setField(const char* fieldName,
+        const VrmlField& fieldValue)
+    {
+        if
+            TRY_FIELD(MachineName, SFString)
+        else if
+            TRY_FIELD(XAxis, SFVec3f)
+        else if
+            TRY_FIELD(YAxis, SFVec3f)
+        else if
+            TRY_FIELD(ZAxis, SFVec3f)
+        else if
+            TRY_FIELD(AAxis, SFVec3f)
+        else if
+            TRY_FIELD(BAxis, SFVec3f)
+        else if
+            TRY_FIELD(CAxis, SFVec3f)
+        else if
+            TRY_FIELD(AxisNames, MFString)
+        else
+            VrmlNodeChild::setField(fieldName, fieldValue);
+        if (strcmp(fieldName, "MachineName") == 0)
+        {
+            //connect to the specified machine through OPC-UA
+        }
+        else
+        {
+
+        }
     }
 
     virtual VrmlNodeType *nodeType() const { return defineType(); };
@@ -80,18 +122,48 @@ public:
     void move(int axis, float value)
     {
         auto t = System::the->time();
-        if(axis >= 2)
+        VrmlSFVec3f v;
+        if (strcmp(d_AxisNames[axis], "X") == 0)
         {
-            osg::Vec3f v;
-            v[axis -2] = value /1000;
-            eventOut(t, axisNames[axis], VrmlSFVec3f{v.x(), v.z(), -v.y()});
+            v = d_XAxis;
+        }
+        else if (strcmp(d_AxisNames[axis], "Y") == 0)
+        {
+            v = d_YAxis;
+        }
+        else if (strcmp(d_AxisNames[axis], "Z") == 0)
+        {
+            v = d_ZAxis;
+        }
+        else if (strcmp(d_AxisNames[axis], "A") == 0)
+        {
+            v = d_AAxis;
+        }
+        else if (strcmp(d_AxisNames[axis], "B") == 0)
+        {
+            v = d_BAxis;
+        }
+        else if (strcmp(d_AxisNames[axis], "C") == 0)
+        {
+            v = d_CAxis;
+        }
+        if(axis >= 2) // ugly hack to find out if an axis is translational
+        {
+            v.multiply(value /1000.0);
+            eventOut(t, d_AxisNames[axis], v);
         }
         else{
-            osg::Vec3f v;
-            v[axis] = 1;
-            eventOut(t, axisNames[axis], VrmlSFRotation{v.x(), v.z(), v.y(), -value / 180 *(float)osg::PI});
+            eventOut(t, d_AxisNames[axis], VrmlSFRotation{v.x(), v.y(), v.z(), value / 180 *(float)osg::PI});
         }
     }
+    VrmlSFString d_MachineName;
+    VrmlSFVec3f d_XAxis;
+    VrmlSFVec3f d_YAxis;
+    VrmlSFVec3f d_ZAxis;
+    VrmlSFVec3f d_AAxis;
+    VrmlSFVec3f d_BAxis;
+    VrmlSFVec3f d_CAxis;
+    VrmlMFString d_AxisNames;
 private:
     size_t m_index = 0;
 };
@@ -212,20 +284,21 @@ void ToolMaschinePlugin::key(int type, int keySym, int mod)
 
 bool ToolMaschinePlugin::update()
 {
-    auto client = opcua::getClient();
-    if(!client || !client->isConnected())
-        return true;
-    std::array<double, 5> axisValues;
-
-    for (size_t i = 0; i < 5; i++)
-        axisValues[i] = client->readNumericValue(m_axisNames[i]->ui()->selectedItem()) + m_offsets[i]->ui()->number();
-    for(const auto &m : machineNodes)
+    for (const auto& m : machineNodes)
     {
+        
+        auto client = opcua::getClient(/*m->d_MachineName.get()*/); // get the client with this name
+        if (!client || !client->isConnected())
+            return true;
+        std::array<double, 5> axisValues;
+
+        for (size_t i = 0; i < 5; i++)
+            axisValues[i] = client->readNumericValue(m_axisNames[i]->ui()->selectedItem()) + m_offsets[i]->ui()->number();
         for (size_t i = 0; i < 5; i++)
             m->move(i, axisValues[i]);
-        
+
+        m_currents.update(axisValues, axisValues);
     }
-    m_currents.update(axisValues, axisValues);
     return true;
 }
 
