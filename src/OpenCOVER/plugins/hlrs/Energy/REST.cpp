f607413b343ebcfa4b19a9309eeb53f53a6a1320
MDjur <marko.djuric@gmx.de>
2024-01-29 12:30:43 +0100

    plugin-EnergyCampus: add REST lib

---
+++ src/OpenCOVER/plugins/hlrs/Energy/REST.cpp
@@ -0,0 +1,47 @@
+#include "REST.h"
+#include <iostream>
+#include <curl/curl.h>
+#include <string>
+
+using namespace std;
+
+namespace {
+
+/**
+ * @brief Callback function to handle curl's response
+ * 
+ * @param contents Pointer to the content of data
+ * @param size Size of each data element
+ * @param nmemb Number of data elements
+ * @param userp Pointer to the user-defined data
+ * @return size_t The total size of the response data
+ */
+size_t writeCallback(void *contents, size_t size, size_t nmemb, void *userp) {
+    ((string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+}
+
+bool performCurlRequest(const string& url, string& response) {
+    auto curl = curl_easy_init();
+    if (!curl) {
+        cerr << "Failed to initialize CURL" << endl;
+        return false;
+    }
+
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+    auto res = curl_easy_perform(curl);
+    curl_easy_cleanup(curl);
+
+    if (res != CURLE_OK) {
+        cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << endl;
+        return false;
+    }
+
+    return true;
+}
+
+void cleanupcurl() { curl_global_cleanup(); }
---
+++ src/OpenCOVER/plugins/hlrs/Energy/REST.h
@@ -0,0 +1,28 @@
+/* This file is part of COVISE.
+
+  You can use it under the terms of the GNU Lesser General Public License
+  version 2.1 or later, see lgpl-2.1.txt.
+
+* License: LGPL 2+ */
+
+#ifndef _REST_H
+#define _REST_H
+
+#include <curl/curl.h>
+#include <string>
+
+/**
+ * @brief Function to perform a CURL request
+ * 
+ * @param url The URL to send the request to
+ * @param response The response data received from the request (storage)
+ * @return bool True if the request was successful, false otherwise
+ */
+bool performCurlRequest(const std::string& url, std::string& response);
+
+/**
+ * @brief Function to cleanup the CURL library
+ */
+void cleanupcurl();
+
+#endif
