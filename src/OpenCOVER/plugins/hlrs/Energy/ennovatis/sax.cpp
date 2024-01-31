784b78757bcb5c04bd94f94af213300c349f8847
MDjur <marko.djuric@gmx.de>
2024-01-31 17:25:28 +0100

    plugin-EnergyCampus: prototype for SAX channelid parser using nlohmann SAX-Interface

---
+++ src/OpenCOVER/plugins/hlrs/Energy/ennovatis/sax.cpp
@@ -0,0 +1,106 @@
+#include <nlohmann/json.hpp>
+#include <sax.h>
+
+using json = nlohmann::json;
+namespace {
+constexpr bool debug = true;
+}
+
+bool sax_channelid_parser::string(string_t &val)
+{
+    if constexpr (debug)
+        m_debugLogs.push_back("string(val=" + val + ")");
+
+    if (m_currBuilding) {
+        m_currBuilding = false;
+    }
+    return true;
+}
+
+bool sax_channelid_parser::key(string_t &val)
+{
+    if constexpr (debug)
+        m_debugLogs.push_back("key(val=" + val + ")");
+
+    // m_currBuilding = val == "building" && m_strDevList->empty();
+    m_currBuilding = val == "building";
+    return true;
+}
+
+bool sax_channelid_parser::null()
+{
+    if constexpr (debug)
+        m_debugLogs.push_back("null()");
+    return true;
+}
+
+bool sax_channelid_parser::boolean(bool val)
+{
+    if constexpr (debug)
+        m_debugLogs.push_back("boolean(val=" + std::string(val ? "true" : "false") + ")");
+    return true;
+}
+
+bool sax_channelid_parser::number_integer(number_integer_t val)
+{
+    if constexpr (debug)
+        m_debugLogs.push_back("number_integer(val=" + std::to_string(val) + ")");
+    return true;
+}
+
+bool sax_channelid_parser::number_unsigned(number_unsigned_t val)
+{
+    if constexpr (debug)
+        m_debugLogs.push_back("number_unsigned(val=" + std::to_string(val) + ")");
+    return true;
+}
+
+bool sax_channelid_parser::number_float(number_float_t val, const string_t &s)
+{
+    if constexpr (debug)
+        m_debugLogs.push_back("number_float(val=" + std::to_string(val) + ", s=" + s + ")");
+    return true;
+}
+
+bool sax_channelid_parser::start_object(std::size_t elements)
+{
+    if constexpr (debug)
+        m_debugLogs.push_back("start_object(elements=" + std::to_string(elements) + ")");
+    return true;
+}
+
+bool sax_channelid_parser::end_object()
+{
+    if constexpr (debug)
+        m_debugLogs.push_back("end_object()");
+    return true;
+}
+
+bool sax_channelid_parser::start_array(std::size_t elements)
+{
+    if constexpr (debug)
+        m_debugLogs.push_back("start_array(elements=" + std::to_string(elements) + ")");
+    return true;
+}
+
+bool sax_channelid_parser::end_array()
+{
+    if constexpr (debug)
+        m_debugLogs.push_back("end_array()");
+    return true;
+}
+
+bool sax_channelid_parser::binary(json::binary_t &val)
+{
+    if constexpr (debug)
+        m_debugLogs.push_back("binary(val=[...])");
+    return true;
+}
+
+bool sax_channelid_parser::parse_error(std::size_t position, const std::string &last_token, const json::exception &ex)
+{
+    if constexpr (debug)
+        m_debugLogs.push_back("parse_error(position=" + std::to_string(position) + ", last_token=" + last_token +
+                              ",\n            ex=" + std::string(ex.what()) + ")");
+    return false;
+}
---
+++ src/OpenCOVER/plugins/hlrs/Energy/ennovatis/sax.h
@@ -0,0 +1,40 @@
+#ifndef _SAX_H
+#define _SAX_H
+
+#include <nlohmann/json.hpp>
+
+/**
+ * @brief JSON SAX handler for parsing JSON data.
+ *
+ * This struct implements the nlohmann::json_sax interface to handle SAX events
+ * during JSON parsing. Each event corresponds to a specific type of JSON value,
+ * such as null, boolean, number, string, object, or array.
+ */
+class sax_channelid_parser: public nlohmann::json::json_sax_t {
+public:
+    //   sax_location_parser(EnergyPlugin::DeviceList &_map)
+    //       : m_strDevList(std::make_unique<EnergyPlugin::DeviceList>(_map)) {}
+    sax_channelid_parser() = default;
+
+    bool string(string_t &val) override;
+    bool key(string_t &val) override;
+    bool null() override;
+    bool boolean(bool val) override;
+    bool number_integer(number_integer_t val) override;
+    bool number_unsigned(number_unsigned_t val) override;
+    bool number_float(number_float_t val, const string_t &s) override;
+    bool start_object(std::size_t elements) override;
+    bool end_object() override;
+    bool start_array(std::size_t elements) override;
+    bool end_array() override;
+    bool binary(nlohmann::json::binary_t &val) override;
+    bool parse_error(std::size_t position, const std::string &last_token, const nlohmann::json::exception &ex) override;
+    const std::vector<std::string> &getDebugLogs() const { return m_debugLogs; }
+
+private:
+    bool m_currBuilding = false;
+    //   std::unique_ptr<EnergyPlugin::DeviceList> m_strDevList;
+    std::vector<std::string> m_debugLogs;
+};
+
+#endif
