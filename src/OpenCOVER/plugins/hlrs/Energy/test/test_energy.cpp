deb775ad3c116687ea83d3af721ec1c83f191d1c
MDjur <marko.djuric@gmx.de>
2024-02-01 10:05:44 +0100

    plugin-EnergyCampus: add new tests for SAX interface

--- src/OpenCOVER/plugins/hlrs/Energy/test/test_energy.cpp
+++ src/OpenCOVER/plugins/hlrs/Energy/test/test_energy.cpp
@@ -1,9 +1,16 @@
 #include "../ennovatis/REST.h"
+#include "../ennovatis/sax.h"
+
+// #include "REST.h"
 #include <gtest/gtest.h>
+#include <nlohmann/json.hpp>
+#include <fstream>
 
 using namespace ennovatis;
 
 namespace {
+constexpr auto pathToJSON("/data/DiTEnS/ennovatis/channelids.json");
+
 TEST(performCurlRequestTest, ValidUrl)
 {
     std::string url = "example.com";
@@ -20,5 +27,21 @@ TEST(performCurlRequestTest, InvalidUrl)
     bool result = performCurlRequest(url, response);
     ASSERT_FALSE(result);
     // Add additional assertions to validate the response
+}
+
+TEST(saxTest, ValidJSONSAXParsing)
+{
+    std::ifstream inputFilestream(pathToJSON);
+    sax_channelid_parser slp;
+    // no errors in parsing
+    ASSERT_TRUE(nlohmann::json::sax_parse(inputFilestream, &slp));
+}
+
+TEST(saxTest, EnabledLogging)
+{
+    std::ifstream inputFilestream(pathToJSON);
+    sax_channelid_parser slp;
+    nlohmann::json::sax_parse(inputFilestream, &slp);
+    EXPECT_TRUE(!slp.getDebugLogs().empty());
 }
 } // namespace
