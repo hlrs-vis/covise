678a553a03ef03f5fb7f0dbeb5151b531592b97b
MDjur <marko.djuric@gmx.de>
2024-02-02 18:06:09 +0100

    plugin-EnergyCampus: further testing for REST + adding notation for test script

--- src/OpenCOVER/plugins/hlrs/Energy/test/test_energy.cpp
+++ src/OpenCOVER/plugins/hlrs/Energy/test/test_energy.cpp
@@ -13,6 +13,7 @@ constexpr auto testDataDir(ENERGYCAMPUS_TEST_DATA_DIR);
 std::string pathToJSON = testDataDir;
 std::string pokemonJSON = pathToJSON + "/pokemon.json";
 
+/**************** REST tests ****************/
 TEST(performCurlRequestTest, ValidUrl)
 {
     std::string url = "example.com";
@@ -31,6 +32,24 @@ TEST(performCurlRequestTest, InvalidUrl)
     // Add additional assertions to validate the response
 }
 
+TEST(performCurlRequestTest, ValidResponse)
+{
+    std::string url("https://hacker-news.firebaseio.com/v0/item/8863.json");
+    std::string response;
+    std::string ref_string(R"({"by":"dhouston","descendants":71,"id":8863,"kids":[9224,8917,8884,8887,8952,8869,8873,8958,8940,8908,9005,9671,9067,9055,8865,8881,8872,8955,10403,8903,8928,9125,8998,8901,8902,8907,8894,8870,8878,8980,8934,8943,8876],"score":104,"time":1175714200,"title":"My YC app: Dropbox - Throw away your USB drive","type":"story","url":"http://www.getdropbox.com/u/2/screencast.html"})");
+    bool result = performCurlRequest(url, response);
+    ASSERT_TRUE(result);
+    // Add additional assertions to validate the response
+    EXPECT_EQ(ref_string, response);
+}
+
+TEST(cleanupCurl, ValidCleanup)
+{
+    cleanupcurl();
+    // Add additional assertions to validate the cleanup
+}
+
+/**************** SAX tests ****************/
 TEST(saxTest, ValidJSONSAXParsing)
 {
     std::ifstream pokemonFilestream(pokemonJSON);
@@ -48,11 +67,15 @@ TEST(saxTest, ValidLogging)
     nlohmann::json::sax_parse(pokemonFilestream, &slp);
     pokemonFilestream.close();
     EXPECT_TRUE(!slp.getDebugLogs().empty());
-    for (auto& log : slp.getDebugLogs())
-    {
+    for (auto &log: slp.getDebugLogs()) {
         std::string line;
         std::getline(resultFilestream, line);
         EXPECT_EQ(line, log);
     }
 }
+
+/**************** Building tests ****************/
+// TEST(buidlingText, ValidBuidling)
+// {
+
 } // namespace
