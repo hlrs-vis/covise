dd5c2c73978153f3ac9fa7544c026b492bf47a57
MDjur <marko.djuric@gmx.de>
2024-09-24 11:45:50 +0200

    coCurlHTTPClient: add option to add custom parameters used to init curl_easy_setopt

--- src/OpenCOVER/HTTPClient/CURL/request.cpp
+++ src/OpenCOVER/HTTPClient/CURL/request.cpp
@@ -8,7 +8,7 @@ namespace opencover {
 namespace httpclient {
 namespace curl {
 
-CURL *Request::initCurl(const HTTPMethod &method, std::string &response) const
+CURL *Request::initCurl(const HTTPMethod &method, std::string &response, const Options &options) const
 {
     auto curl = curl_easy_init();
     if (!curl)
@@ -19,12 +19,24 @@ CURL *Request::initCurl(const HTTPMethod &method, std::string &response) const
     //don't wait forever
     curl_easy_setopt(curl,CURLOPT_TIMEOUT, 10);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+    
+    //custom options
+    for (const auto &option : options) {
+        std::visit([&curl, &option](auto &&arg) {
+            using T = std::decay_t<decltype(arg)>;
+            if constexpr (std::is_same_v<T, std::string>) {
+                curl_easy_setopt(curl, option.first, arg.c_str());
+            } else {
+                curl_easy_setopt(curl, option.first, arg);
+            }
+        }, option.second);
+    }
     return curl;
 }
 
-bool Request::httpRequest(const HTTPMethod &method, string &response) const
+bool Request::httpRequest(const HTTPMethod &method, string &response, const Options &options) const
 {
-    auto curl = initCurl(method, response);
+    auto curl = initCurl(method, response, options);
     if (!curl)
         return false;
 
--- src/OpenCOVER/HTTPClient/CURL/request.h
+++ src/OpenCOVER/HTTPClient/CURL/request.h
@@ -4,13 +4,17 @@
 #include "export.h"
 #include "methods.h"
 #include <curl/curl.h>
+#include <vector>
+#include <variant>
 #include <string>
 
 namespace opencover {
 namespace httpclient {
 namespace curl {
 
+using CURLOptionValue = std::variant<long, std::string, void*, curl_off_t, curl_write_callback>;
 struct CURLHTTPCLIENTEXPORT Request {
+    typedef std::vector<std::pair<CURLoption, CURLOptionValue>> Options;
     Request() { curl_global_init(CURL_GLOBAL_DEFAULT); };
     ~Request() { curl_global_cleanup(); };
     Request(const Request &) = delete;
@@ -26,9 +30,22 @@ struct CURLHTTPCLIENTEXPORT Request {
      * @return bool True if the request was successful, false otherwise.
      */
     [[nodiscard]] bool httpRequest(const HTTPMethod &method, std::string &response) const;
+    /**
+     * @brief Performs a CURL request to the specified URL.
+     * 
+     * This function sends a CURL request to the specified URL and stores the response data in the provided string.
+     * 
+     * @param method HTTPMethod object (GET, POST, PUT, DELETE).
+     * @param response The response data received from the request (storage).
+     * @param options Custom options parameter passed to curl_easy_setopt.
+     * @return bool True if the request was successful, false otherwise.
+     */
+    [[nodiscard]] bool httpRequest(const HTTPMethod &method, std::string &response, const Options &options) const;
+
+    // [[nodiscard]] bool httpRequest(const HTTPMethod &method, std::string &response) const;
 
 private:
-    CURL *initCurl(const HTTPMethod &httpMethod, std::string &response) const;
+    CURL *initCurl(const HTTPMethod &httpMethod, std::string &response, const Options &options) const;
 };
 
 } // namespace curl
