127a466f60a745abfad0dabde9e36b9b011c7e3b
hpcdgrie <grieger@hlrs.de>
2024-10-16 10:58:37 +0200

    Moved computeLevensteinDistance in util/string library

--- src/OpenCOVER/plugins/hlrs/Energy/Energy.cpp
+++ src/OpenCOVER/plugins/hlrs/Energy/Energy.cpp
@@ -61,8 +61,21 @@
 #include <string>
 #include <vector>
 
+#include <boost/filesystem.hpp>
+#include <boost/tokenizer.hpp>
+
+#include <config/CoviseConfig.h>
+#include <cover/coVRAnimationManager.h>
+#include <cover/coVRFileManager.h>
+#include <cover/coVRTui.h>
+#include <utils/string/LevenshteinDistane.h>
+
+#include <osg/LineWidth>
+#include <osg/Version>
+
 using namespace opencover;
 using namespace opencover::utils::read;
+using namespace opencover::utils::string;
 using namespace energy;
 
 namespace {
@@ -109,55 +122,7 @@ bool cmpStrtNo(const std::string &strtName, const std::string &strtName2) {
   return helper_cmpStrNo_as_int(strtNo, strtNo2);
 };
 
-// Case-sensitive char comparer
-bool helper_cmpChar(char a, char b) { return (a == b); }
-
-// Case-insensitive char comparer
-bool helper_cmpCharIgnoreCase(char a, char b) {
-  return (std::tolower(a) == std::tolower(b));
-}
-
-/**
- * Computes the Levenshtein distance between two strings. 0 means the strings
- * are equal. The higher the number, the more different chars are in the
- * strings. e.g. "kitten" and "sitting" have a Levenshtein distance of 3.
- * Source:
- * http://www.blackbeltcoder.com/Articles/algorithms/approximate-string-comparisons-using-levenshtein-distance
- *
- * @param s1 The first string.
- * @param s2 The second string.
- * @param ignoreCase Flag indicating whether to ignore case sensitivity
- * (default: false).
- * @return The Levenshtein distance between the two strings.
- */
-size_t computeLevensteinDistance(const std::string &s1, const std::string &s2,
-                                 bool ignoreCase = false) {
-  const auto &len1 = s1.size(), len2 = s2.size();
-
-  // allocate distance matrix
-  std::vector<std::vector<size_t>> d(len1 + 1, std::vector<size_t>(len2 + 1));
-
-  auto isEqual = [&](char a, char b) {
-    return (ignoreCase) ? helper_cmpCharIgnoreCase(a, b) : helper_cmpChar(a, b);
-  };
-
-  d[0][0] = 0;
-  // compute distance
-  for (int i = 1; i <= len1; ++i) d[i][0] = i;
-  for (int j = 1; j <= len2; ++j) d[0][j] = j;
-
-  for (int i = 1; i <= len1; ++i) {
-    for (int j = 1; j <= len2; ++j) {
-      if (isEqual(s1[i - 1], s2[j - 1]))
-        d[i][j] = d[i - 1][j - 1];
-      else
-        d[i][j] = std::min({d[i - 1][j] + 1, d[i][j - 1] + 1, d[i - 1][j - 1] + 1});
-    }
-  }
-
-  return d[len1][len2];
-}
-}  // namespace
+} // namespace
 
 /* #region GENERAL */
 EnergyPlugin *EnergyPlugin::m_plugin = nullptr;
