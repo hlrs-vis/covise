be39aa5964bef1a7a19be7e336c5d02737d079d4
MDjur <marko.djuric@gmx.de>
2024-10-21 15:05:40 +0200

    EnergyCampus: move #endif to the end

--- src/OpenCOVER/plugins/hlrs/Energy/core/utils/color.h
+++ src/OpenCOVER/plugins/hlrs/Energy/core/utils/color.h
@@ -1,6 +1,5 @@
 #ifndef _CORE_UTILS_COLOR_H
 #define _CORE_UTILS_COLOR_H
-#endif
 
 #include <osg/Geode>
 #include <osg/Material>
@@ -19,3 +18,4 @@ auto createMaterial(const osg::Vec4 &color, osg::Material::Face faceMask);
 void overrideGeodeColor(osg::Geode *geode, const osg::Vec4 &color,
                         osg::Material::Face faceMask = osg::Material::FRONT);
 } // namespace core::utils::color
+#endif
