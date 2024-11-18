e2079978953a5c6ac2752a85cbbb6d8709ee4051
MDjur <marko.djuric@gmx.de>
2024-11-18 17:47:47 +0100

    EnergyCampus: add drawables as interface for handling multiple drawbles

---
+++ src/OpenCOVER/plugins/hlrs/Energy/core/interfaces/IDrawables.h
@@ -0,0 +1,19 @@
+#ifndef _CORE_INTERFACES_IDRAWABLES_H
+#define _CORE_INTERFACES_IDRAWABLES_H
+
+#include <osg/Node>
+
+namespace core::interface {
+class IDrawables {
+public:
+  virtual void initDrawables() = 0;
+  virtual void updateDrawables() = 0;
+  auto getDrawables() { return m_drawables; }
+  auto getDrawable(size_t index) { return m_drawables.at(index); }
+
+protected:
+  std::vector<osg::ref_ptr<osg::Node>> m_drawables;
+};
+} // namespace core
+
+#endif
