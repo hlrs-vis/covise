hlrs-vis/covise

commit e01df9effc5d545207ee9c1a0e8fc5b12249b8fb

try general plugins last
First try if loading an input driver plugin finds the missing driver,
only then try to load a general opencover plugin with a less specific name.
Otherwise, SpaceNavigator plugin would always be tried before
input_spacenavigator on a case ignoring filesystem.

src/OpenCOVER/cover/input/input.cpp
@@ -421,7 +421,20 @@ InputDevice *Input::getDevice(const std::string &name)
     {
         std::string conf = configPath("Device." + name);
         std::string type = coCoviseConfig::getEntry("driver", conf, "const");
-        if (type.empty())
+        DriverFactoryBase *plug = getDriverPlugin(coVRMSController::instance()->isMaster() ? type : "const");
+        if (!plug)
+        {
+            std::cerr << "Input: replaced driver " << name << " with \"const\"" << std::endl;
+            plug = getDriverPlugin("const");
+        }
+        if (plug)
+        {
+            dev = plug->newInstance("COVER.Input.Device." + name);
+            drivers[name] = dev;
+            if (dev->needsThread())
+                dev->start();
+        }
+        else if (type.empty())
         {
             if (coVRPlugin *coverPlugin = coVRPluginList::instance()->addPlugin(name.c_str(), coVRPluginList::Input))
             {
@@ -440,19 +453,6 @@ InputDevice *Input::getDevice(const std::string &name)
             }
         }
         //std::cerr << "Input: creating dev " << name << ", driver " << type << std::endl;
-        DriverFactoryBase *plug = getDriverPlugin(coVRMSController::instance()->isMaster() ? type : "const");
-        if (!plug)
-        {
-            std::cerr << "Input: replaced driver " << name << " with \"const\"" << std::endl;
-            plug = getDriverPlugin("const");
-        }
-        if (plug)
-        {
-            dev = plug->newInstance("COVER.Input.Device." + name);
-            drivers[name] = dev;
-            if (dev->needsThread())
-                dev->start();
-        }
     }
     return dev;
 }

commit b6b5e618503eb4af2e57deea9fdcfdc1ec748b4f

try to fix input plugin initialisation
broken in e01df9effc5d545207ee9c1a0e8fc5b12249b8fb

src/OpenCOVER/cover/input/input.cpp
@@ -424,34 +424,31 @@ InputDevice *Input::getDevice(const std::string &name)
         DriverFactoryBase *plug = getDriverPlugin(coVRMSController::instance()->isMaster() ? type : "const");
         if (!plug)
         {
+            if (type.empty())
+            {
+                if (coVRPlugin *coverPlugin = coVRPluginList::instance()->addPlugin(name.c_str(), coVRPluginList::Input))
+                {
+                    dev = findInMap(drivers, name);
+                    if (dev)
+                        return dev;
+                }
+            }
+            else if (type != "const")
+            {
+                if (coVRPlugin *coverPlugin = coVRPluginList::instance()->addPlugin(type.c_str(), coVRPluginList::Input))
+                {
+                    dev = findInMap(drivers, name);
+                    if (dev)
+                        return dev;
+                }
+            }
             std::cerr << "Input: replaced driver " << name << " with \"const\"" << std::endl;
             plug = getDriverPlugin("const");
         }
-        if (plug)
-        {
-            dev = plug->newInstance("COVER.Input.Device." + name);
-            drivers[name] = dev;
-            if (dev->needsThread())
-                dev->start();
-        }
-        else if (type.empty())
-        {
-            if (coVRPlugin *coverPlugin = coVRPluginList::instance()->addPlugin(name.c_str(), coVRPluginList::Input))
-            {
-                dev = findInMap(drivers, name);
-                if (dev)
-                    return dev;
-            }
-        }
-        else if (type != "const")
-        {
-            if (coVRPlugin *coverPlugin = coVRPluginList::instance()->addPlugin(type.c_str(), coVRPluginList::Input))
-            {
-                dev = findInMap(drivers, name);
-                if (dev)
-                    return dev;
-            }
-        }
+        dev = plug->newInstance("COVER.Input.Device." + name);
+        drivers[name] = dev;
+        if (dev->needsThread())
+            dev->start();
         //std::cerr << "Input: creating dev " << name << ", driver " << type << std::endl;
     }
     return dev;