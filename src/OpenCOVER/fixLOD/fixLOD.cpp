f653ccfe93c050581f6b4f4aa4757f2fa7db7e8b
Uwe Woessner <woessner@hlrs.de>
2025-01-15 00:26:00 +0100

    first working version

--- src/OpenCOVER/fixLOD/fixLOD.cpp
+++ src/OpenCOVER/fixLOD/fixLOD.cpp
@@ -4,7 +4,6 @@
 #include <vector>
 #include <sstream>
 #include <iomanip>
-#include <ogrsf_frmts.h>
 #include <osg/MatrixTransform>
 #include <osg/Geode>
 #include <osg/ProxyNode>
@@ -13,15 +12,68 @@
 #include <osgDB/WriteFile>
 #include <osgDB/FileUtils>
 #include <osgDB/FileNameUtils>
+#include <osg/PagedLOD>
 
 
 int fixLOD(const std::string& filename)
 {
 
+    osgDB::Options *options= new osgDB::Options;
+
+    options->setOptionString("WriteImageHint=IncludeFile");
     osg::Node *root=nullptr;
     root = osgDB::readNodeFile(filename.c_str());
-    osg::PagedLOD *plod = 
-    osgDB::writeNodeFile(*root, filename.c_str());
+    osg::PagedLOD* plod = dynamic_cast<osg::PagedLOD*>(root);
+    if (plod)
+    {
+        plod->setRangeMode(osg::LOD::RangeMode::DISTANCE_FROM_EYE_POINT);
+        auto ranges = plod->getRangeList();
+        osg::LOD::RangeList nrl;
+        for (int i = (int)ranges.size() - 1; i >= 0; i--)
+        {
+            nrl.push_back(ranges[i]);
+        }
+        plod->setRangeList(nrl);
+        osgDB::writeNodeFile(*root, filename.c_str(), options);
+    }
+    else
+    {
+        osg::Group* g = dynamic_cast<osg::Group*>(root);
+        if (g)
+        {
+            bool found = true;
+            for (size_t i = 0; i < g->getNumChildren(); i++)
+            {
+                osg::Node* node = g->getChild(i); 
+                osg::PagedLOD* plod = dynamic_cast<osg::PagedLOD*>(node);
+                if (plod)
+                {
+                    plod->setRangeMode(osg::LOD::RangeMode::DISTANCE_FROM_EYE_POINT);
+                    auto ranges = plod->getRangeList();
+                    osg::LOD::RangeList nrl;
+                    for (int i = (int)ranges.size() - 1; i >= 0; i--)
+                    {
+                        nrl.push_back(ranges[i]);
+                    }
+                    plod->setRangeList(nrl);
+                    found = true;
+                }
+
+            }
+            if (found)
+            {
+                osgDB::writeNodeFile(*root, filename.c_str(), options);
+            }
+            else
+            {
+                fprintf(stderr, "no pagedLOD %s\n", filename.c_str());
+            }
+        }
+        else
+        {
+            fprintf(stderr, "no pagedLOD %s\n", filename.c_str());
+        }
+    }
     return 0;
 }
 
