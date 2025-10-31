0360fa90be8f412b3da7768115fc73a6f94490cb
hpcdgrie <grieger@hlrs.de>
2025-10-31 11:55:04 +0100

    Added FlexCell plugin that receives robot positions from rabbitMQ

---
+++ src/OpenCOVER/plugins/hlrs/FlexCell/VrmlNodes.h
@@ -0,0 +1,25 @@
+#ifndef COVISE_PLUGIN_FLEX_CELL_VRMLNODES_H
+#define COVISE_PLUGIN_FLEX_CELL_VRMLNODES_H
+
+
+#include <vrml97/vrml/VrmlNodeChild.h>
+#include <vrml97/vrml/VrmlNodeType.h>
+
+#include <array>
+#include <set>
+
+class FlexCellNode : public vrml::VrmlNodeChild {
+public:
+    static void initFields(FlexCellNode *node, vrml::VrmlNodeType *t);
+    static const char *typeName() { return "FlexCell"; }
+    FlexCellNode(vrml::VrmlScene *scene);
+    ~FlexCellNode();
+    void send(size_t axis, float value);
+    void bend();
+    void switchWorkpiece(int variant);
+    // std::array<vrml::VrmlSFFloat, 7> axisNames;
+};
+
+extern std::set<FlexCellNode *> flexCellNodes;
+
+#endif // COVISE_PLUGIN_FLEX_CELL_VRMLNODES_H
