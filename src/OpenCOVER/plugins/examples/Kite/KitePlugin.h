cc1ef6fcfc6d017c3e3b278c907567e25eb4c16b
karthikeya-ms <writetokarthikeya99@gmail.com>
2025-12-15 17:06:16 +0100

    base kite animation template

--- src/OpenCOVER/plugins/examples/Kite/KitePlugin.h
+++ src/OpenCOVER/plugins/examples/Kite/KitePlugin.h
@@ -13,9 +13,6 @@
 #include <string>
 #include <vector>
 #include <osg/Vec3>
-#include <osg/Geometry>
-#include <osg/Geode>
-#include <osg/LineWidth>
 
 class KitePlugin : public opencover::coVRPlugin
 {
@@ -38,28 +35,12 @@ private:
         double roll = 0.0;
         double pitch = 0.0;
         double yaw = 0.0;
-        double tetherLength = 0.0;
-        double tetherForce = 0.0;
-        double tetherReeloutSpeed = 0.0;
-        double windSpeed = 0.0;
-        double windDir = 0.0;
-        double kiteElevation = 0.0;
-        double kiteAzimuth = 0.0;
-        double kiteHeading = 0.0;
-        double kiteCourse = 0.0;
     };
 
     osg::ref_ptr<osg::MatrixTransform> m_transform;
     osg::ref_ptr<osg::Node> m_model;
-    osg::ref_ptr<osg::Geode> m_tetherGeode;
-    osg::ref_ptr<osg::Geometry> m_tetherGeom;
-    osg::ref_ptr<osg::LineWidth> m_tetherWidth;
-    osg::ref_ptr<osg::Geode> m_groundGeode;
-    osg::ref_ptr<osg::Geode> m_windGeode;
-    osg::ref_ptr<osg::Geometry> m_windGeom;
     std::vector<Frame> m_frames;
     std::string m_csvPath;
-    double m_maxForce = 0.0;
 };
 
 #endif // KITE_PLUGIN_H
