563e8bcc9d95abb0cb138a09d378efafa249b61f
MDjur <marko.djuric@gmx.de>
2026-02-25 11:10:52 +0100

    add convenience constructors for Pos

--- src/OpenCOVER/plugins/hlrs/Energy/lib/core/interfaces/IMovable.h
+++ src/OpenCOVER/plugins/hlrs/Energy/lib/core/interfaces/IMovable.h
@@ -1,10 +1,23 @@
 #pragma once
+#include <array>
 
 namespace core::interface {
 struct Pos {
     float x, y, z;
     Pos() : x(0.0f), y(0.0f), z(0.0f) {}
     Pos(float x, float y, float z) : x(x), y(y), z(z) {}
+    Pos(const float xyz[3])
+        : x(xyz[0])
+        , y(xyz[1])
+        , z(xyz[2])
+    {
+    }
+    Pos(const std::array<float, 4> &xyz)
+        : x(xyz[0])
+        , y(xyz[1])
+        , z(xyz[2])
+    {
+    }
 };
 
 class IMoveable {
