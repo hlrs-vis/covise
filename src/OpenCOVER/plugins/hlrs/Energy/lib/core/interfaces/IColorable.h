bafb64bb1840123419503f39157efccc83e6a1d1
MDjur <marko.djuric@gmx.de>
2026-02-25 10:48:19 +0100

    add convenience constructor

--- src/OpenCOVER/plugins/hlrs/Energy/lib/core/interfaces/IColorable.h
+++ src/OpenCOVER/plugins/hlrs/Energy/lib/core/interfaces/IColorable.h
@@ -1,23 +1,53 @@
 #pragma once
+#include <array>
 
-namespace core::interface {
-struct Color {
+namespace core::interface
+{
+struct Color
+{
     float r, g, b, a;
-    Color() : r(1.0f), g(1.0f), b(1.0f), a(1.0f) {}
-    Color(float r, float g, float b, float a) :
-        r(r), g(g), b(b), a(a) {}
+    Color()
+        : r(1.0f)
+        , g(1.0f)
+        , b(1.0f)
+        , a(1.0f)
+    {
+    }
+    Color(float r, float g, float b, float a)
+        : r(r)
+        , g(g)
+        , b(b)
+        , a(a)
+    {
+    }
+    Color(const float rgba[4])
+        : r(rgba[0])
+        , g(rgba[1])
+        , b(rgba[2])
+        , a(rgba[3])
+    {
+    }
+    Color(const std::array<float, 4> &rgba)
+        : r(rgba[0])
+        , g(rgba[1])
+        , b(rgba[2])
+        , a(rgba[3])
+    {
+    }
 
-    bool operator==(const Color& other) const {
+    bool operator==(const Color &other) const
+    {
         return r == other.r && g == other.g && b == other.b && a == other.a;
     }
 };
 
-class IColorable {
- public:
-  IColorable() = default;
-  virtual ~IColorable() = default;
-  IColorable(const IColorable&) = delete;
-  IColorable& operator=(const IColorable&) = delete;
-  virtual void updateColor(const Color &color) = 0;
+class IColorable
+{
+public:
+    IColorable() = default;
+    virtual ~IColorable() = default;
+    IColorable(const IColorable &) = delete;
+    IColorable &operator=(const IColorable &) = delete;
+    virtual void updateColor(const Color &color) = 0;
 };
-}  // namespace core::interface
+} // namespace core::interface
