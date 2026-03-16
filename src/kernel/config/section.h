3dc1c6a8c2c09748ccc602e8bf2825bdc70a216c
Martin Aumüller <aumuell@reserv.at>
2026-03-16 16:34:23 +0100

    update to covconfig with Section support

---
+++ src/kernel/config/section.h
@@ -0,0 +1,11 @@
+#ifndef COVISE_CONFIG_SECTION_H
+#define COVISE_CONFIG_SECTION_H
+#define CONFIG_NAMESPACE covise
+#ifndef COVCONFIG_COVISE
+#define COVCONFIG_COVISE
+#endif
+#include "covconfig/section.h"
+
+#undef COVCONFIG_COVISE
+#undef CONFIG_NAMESPACE
+#endif
