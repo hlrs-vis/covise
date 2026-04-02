753e24dc2b33fc667b21a302427e9e983cab0856
DeVasconcelos <malheiros.susanne@gmail.com>
2026-04-02 16:24:09 +0200

    get rid of 'ui::Owner: invalid name' errors

--- src/OpenCOVER/plugins/hlrs/GhostAvatar/ui/GhostAvatarControlsUI.cpp
+++ src/OpenCOVER/plugins/hlrs/GhostAvatar/ui/GhostAvatarControlsUI.cpp
@@ -73,8 +73,8 @@ void GhostAvatarControlsUI::cleanUpDebugLines()
 void GhostAvatarControlsUI::createSettingsMenu()
 {
     m_settingsMenu = new ui::Menu(m_mainMenu, "Settings");
-    m_tabletUINote = new ui::Action(m_settingsMenu, "Changes can only be made in the TabletUI!");
-
+    m_tabletUINote = new ui::Action(m_settingsMenu, "tabletUINote");
+    m_tabletUINote->setText("Changes can only be made in the TabletUI!");
 
     if (m_armAvailable)
         createArmSettingsMenu();
@@ -86,18 +86,20 @@ void GhostAvatarControlsUI::createArmSettingsMenu()
 {
     m_armSettingsMenu = new ui::Menu(m_settingsMenu, "Arm");
 
-    m_armBaseVectorField = new ui::VectorEditField(m_armSettingsMenu, "Base Vector");
+    m_armBaseVectorField = new ui::VectorEditField(m_armSettingsMenu, "armBaseVectorField");
+    m_armBaseVectorField->setText("Base Vector");
     m_armBaseVectorField->setValue(m_avatarControls.getArmBaseVector());
     m_armBaseVectorField->setCallback([this](const osg::Vec3 &dir)
         { m_avatarControls.setArmBaseVector(dir); });
 
-    m_armAdjustMatrixMenu = new ui::Menu(m_armSettingsMenu, "Adjust Matrix");
+    m_armAdjustMatrixMenu = new ui::Menu(m_armSettingsMenu, "armAdjustMatrixMenu");
+    m_armAdjustMatrixMenu->setText("Adjust Matrix");
     for (int row = 0; row < 3; ++row)
     {
         auto adjustMatrixArm = m_avatarControls.getArmAdjustMatrix();
         osg::Vec3 rowVec(adjustMatrixArm(row, 0), adjustMatrixArm(row, 1), adjustMatrixArm(row, 2));
-        std::string label = "Row " + std::to_string(row);
-        m_armAdjustMatrixFields[row] = new ui::VectorEditField(m_armAdjustMatrixMenu, label);
+        m_armAdjustMatrixFields[row] = new ui::VectorEditField(m_armAdjustMatrixMenu, "armAdjustMatrixFieldsRow" + std::to_string(row));
+        m_armAdjustMatrixFields[row]->setText("Row " + std::to_string(row));
         m_armAdjustMatrixFields[row]->setValue(rowVec);
         m_armAdjustMatrixFields[row]->setCallback([this, row](const osg::Vec3 &v)
             { m_avatarControls.setArmAdjustMatrix(row, v); });
@@ -108,18 +110,20 @@ void GhostAvatarControlsUI::createHeadSettingsMenu()
 {
     m_headSettingsMenu = new ui::Menu(m_settingsMenu, "Head");
 
-    m_headBaseVectorField = new ui::VectorEditField(m_headSettingsMenu, "Base Vector");
+    m_headBaseVectorField = new ui::VectorEditField(m_headSettingsMenu, "headBaseVectorField");
+    m_headBaseVectorField->setText("Base Vector");
     m_headBaseVectorField->setValue(m_avatarControls.getHeadBaseVector());
     m_headBaseVectorField->setCallback([this](const osg::Vec3 &dir)
         { m_avatarControls.setHeadBaseVector(dir); });
 
-    m_headAdjustMatrixMenu = new ui::Menu(m_headSettingsMenu, "Adjust Matrix");
+    m_headAdjustMatrixMenu = new ui::Menu(m_headSettingsMenu, "headAdjustMatrixMenu");
+    m_headAdjustMatrixMenu->setText("Adjust Matrix");
     for (int row = 0; row < 3; ++row)
     {
         auto adjustMatrixHead = m_avatarControls.getHeadAdjustMatrix();
         osg::Vec3 rowVec(adjustMatrixHead(row, 0), adjustMatrixHead(row, 1), adjustMatrixHead(row, 2));
-        std::string label = "Row " + std::to_string(row);
-        m_headAdjustMatrixFields[row] = new ui::VectorEditField(m_headAdjustMatrixMenu, label);
+        m_headAdjustMatrixFields[row] = new ui::VectorEditField(m_headAdjustMatrixMenu, "headAdjustMatrixFieldsRow" + std::to_string(row));
+        m_headAdjustMatrixFields[row]->setText("Row " + std::to_string(row));
         m_headAdjustMatrixFields[row]->setValue(rowVec);
         m_headAdjustMatrixFields[row]->setCallback([this, row](const osg::Vec3 &v)
             { m_avatarControls.setHeadAdjustMatrix(row, v); });
@@ -130,18 +134,21 @@ void GhostAvatarControlsUI::createDebugMenu()
 {
     m_debugMenu = new ui::Menu(m_mainMenu, "Debugging");
 
-    m_showTargetLines = new ui::Button(m_debugMenu, "Show Target Lines");
+    m_showTargetLines = new ui::Button(m_debugMenu, "showTargetLines");
+    m_showTargetLines->setText("Show Target Lines");
     m_showTargetLines->setState(false);
     m_showTargetLines->setCallback([this](bool state)
         { m_showTargetLines->setState(state); 
                                        cleanUpDebugLines(); });
 
-    m_showFrames = new ui::Button(m_debugMenu, "Show Frames");
+    m_showFrames = new ui::Button(m_debugMenu, "showFrames");
+    m_showFrames->setText("Show Frames");
     m_showFrames->setState(false);
     m_showFrames->setCallback([this](bool state)
         { m_showFrames->setState(state);
                                     cleanUpDebugLines(); });
 
-    m_axisNote = new ui::Action(m_debugMenu, "x - red, y - green, z - blue");
+    m_axisNote = new ui::Action(m_debugMenu, "axisNote");
+    m_axisNote->setText("x - red, y - green, z - blue");
     m_axisNote->setEnabled(false);
 }
