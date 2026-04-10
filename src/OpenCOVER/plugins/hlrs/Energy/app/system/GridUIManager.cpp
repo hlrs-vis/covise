d61748cd32dfee21f3777ee3e73ecbe6a2e61785
MDjur <marko.djuric@gmx.de>
2026-04-10 12:39:35 +0200

    add some cpp template for GridUIManager

---
+++ src/OpenCOVER/plugins/hlrs/Energy/app/system/GridUIManager.cpp
@@ -0,0 +1,9 @@
+#include "GridUIManager.h"
+
+GridUIManager::GridUIManager(opencover::ui::Group *parentMenu)
+{
+}
+
+void GridUIManager::updateUIState(EnergyType type, std::shared_ptr<core::simulation::SimulationResult> result)
+{
+}
