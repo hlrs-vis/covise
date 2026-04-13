491a2fe57076763103e4ca3242f2d73d47e08080
MDjur <marko.djuric@gmx.de>
2026-04-13 07:19:37 +0200

    check for valid data in DataManager

--- src/OpenCOVER/plugins/hlrs/Energy/app/system/DataManager.cpp
+++ src/OpenCOVER/plugins/hlrs/Energy/app/system/DataManager.cpp
@@ -5,8 +5,10 @@ void DataManager::loadScenario(Storage storageType, const Scenario &scenario, Da
 {
     for (auto type : ENERGYTYPE_RANGE)
     {
-        if (m_cache.count(scenario.id) && m_cache[scenario.id].count(type)) 
-            continue;
+        if (m_cache.count(scenario.id) && m_cache[scenario.id].count(type))
+            // check if data is valid/stale
+            if (m_cache[scenario.id][type].use_count() > 0 )
+                continue;
 
         auto package = loader.fetch(storageType, scenario, type);
         auto result = DataFactory::create(package, scenario, type);
