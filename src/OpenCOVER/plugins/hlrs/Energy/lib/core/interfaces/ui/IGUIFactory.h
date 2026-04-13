0acf7f216c6efa01f1278bc525b9579697b75465
MDjur <marko.djuric@gmx.de>
2026-04-13 09:10:19 +0200

    add virtual destructor for IGUIFactory

--- src/OpenCOVER/plugins/hlrs/Energy/lib/core/interfaces/ui/IGUIFactory.h
+++ src/OpenCOVER/plugins/hlrs/Energy/lib/core/interfaces/ui/IGUIFactory.h
@@ -11,6 +11,7 @@ namespace core::interface::ui
 class IGUIFactory
 {
 public:
+    virtual ~IGUIFactory() = default;
     virtual std::unique_ptr<IButton> createButton(IComponent *parent, const std::string &name) const = 0;
     virtual std::unique_ptr<IButtonGroup> createButtonGroup(IComponent *parent, const std::string &name, std::vector<std::unique_ptr<IButton>> &&buttons) const = 0;
     virtual std::unique_ptr<IMenu> createMenu(const std::string &name, IComponent *parent = nullptr) const = 0;
