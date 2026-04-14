b7c424d2994c6c5b8db994196702091aa1cdbf7b
DeVasconcelos <malheiros.susanne@gmail.com>
2026-04-14 11:11:57 +0200

    for now, adjusting target interactors based on avatar

--- src/OpenCOVER/plugins/hlrs/GhostAvatar/GhostAvatar.cpp
+++ src/OpenCOVER/plugins/hlrs/GhostAvatar/GhostAvatar.cpp
@@ -11,8 +11,8 @@ using namespace ui;
 
 GhostAvatar::GhostAvatar()
     : coVRPlugin(COVER_PLUGIN_NAME)
-    , m_avatarControls(std::make_unique<TestAvatarControls>("/data/STARTS-ECHO/Avatars/shaderTests/ghost_cave_uniform.fbx", "LeftArm", ""))
-    //, m_avatarControls(std::make_unique<PlanarAvatarControls>("/data/STARTS-ECHO/Avatars/planarAvatar/PLANEE6.fbx", "Arm", "Head"))
+    //, m_avatarControls(std::make_unique<TestAvatarControls>("/data/STARTS-ECHO/Avatars/shaderTests/ghost_cave_uniform.fbx", "LeftArm", ""))
+    , m_avatarControls(std::make_unique<PlanarAvatarControls>("/data/STARTS-ECHO/Avatars/planarAvatar/PLANEE6.fbx", "Arm", "Head"))
     , m_avatarTexture(std::make_unique<SplotchTerroirTexture>(100))
     //, m_avatarTexture(std::make_unique<StripesTerroirTexture>(100))
     , m_avatarControlsUI(GhostAvatarControlsUI(COVER_PLUGIN_NAME, *m_avatarControls))
@@ -54,18 +54,21 @@ void GhostAvatar::createInteractors()
     // TODO: try to set them to the default position first
     osg::Matrix m;
     auto interSize = 10;
-    m.setTrans(0, 0.0, 0.0);
-    m.setRotate(osg::Quat(0, 0, 0.707107, 0.707107));
+    m.setTrans(0, 0, 0);
+    //m.setRotate(osg::Quat(0, 0, 0.707107, 0.707107)); // ghost
+    m.setRotate(osg::Quat(1, 0, 0, 0)); // planar
     m_interactorFloor.reset(new coVR3DTransRotInteractor(m, interSize, vrui::coInteraction::InteractionType::ButtonA, "floor", "targetInteractor", vrui::coInteraction::InteractionPriority::Medium));
     m_interactorFloor->enableIntersection();
     m_interactorFloor->show();
 
-    m.setTrans(0.0, 900.0, -1100.0);
+    //m.setTrans(-120, 0, 80); // ghost
+    m.setTrans(900, 0, 1100); // planar
     m_interactorHand.reset(new coVR3DTransRotInteractor(m, interSize, vrui::coInteraction::InteractionType::ButtonA, "hand", "targetInteractor", vrui::coInteraction::InteractionPriority::Medium));
     m_interactorHand->enableIntersection();
     m_interactorHand->show();
 
-    m.setTrans(0.0, 0.0, -1900.0);
+    //m.setTrans(0, 0, 160); //ghost
+    m.setTrans(0, 0, 1900); // planar
     m_interactorHead.reset(new coVR3DTransRotInteractor(m, interSize, vrui::coInteraction::InteractionType::ButtonA, "head", "targetInteractor", vrui::coInteraction::InteractionPriority::Medium));
     m_interactorHead->enableIntersection();
     m_interactorHead->show();
