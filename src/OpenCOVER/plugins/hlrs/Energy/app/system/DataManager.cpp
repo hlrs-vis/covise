ba8ab3e86914186765e8853b74fc3c08173be10c
MDjur <marko.djuric@gmx.de>
2026-04-14 15:14:37 +0200

    refactor DataFactory:
    DataFactory is now more generic and allows to parse arguments if the correct is available in Parser.h.

--- src/OpenCOVER/plugins/hlrs/Energy/app/system/DataManager.cpp
+++ src/OpenCOVER/plugins/hlrs/Energy/app/system/DataManager.cpp
@@ -1,20 +1,24 @@
 #include "DataManager.h"
 #include "DataFactory.h"
 
-void DataManager::loadScenario(Storage storageType, const Scenario &scenario, DataLoadManager &loader)
+void DataManager::loadScenario(Storage storageType, const Scenario &scenario,
+    DataLoadManager &loader)
 {
     for (auto type : ENERGYTYPE_RANGE)
     {
         if (m_cache.count(scenario.id) && m_cache[scenario.id].count(type))
             // check if data is valid/stale
-            if (m_cache[scenario.id][type].use_count() > 0 )
+            if (m_cache[scenario.id][type].use_count() > 0)
                 continue;
 
         auto package = loader.fetch(storageType, scenario, type);
-        if (package) {
-            auto result = DataFactory::create(*package, scenario, type);
+        if (package)
+        {
+            auto result = DataFactory::create(*package, type, scenario);
             m_cache[scenario.id][type] = result;
-        } else {
+        }
+        else
+        {
             error("Failed to load data for scenario" + scenario.name);
         }
     }
--- src/OpenCOVER/plugins/hlrs/Energy/app/system/Parser.h
+++ src/OpenCOVER/plugins/hlrs/Energy/app/system/Parser.h
@@ -4,47 +4,121 @@
 #include "Scenario.h"
 #include <lib/core/simulation/powerresult.h>
 #include <lib/core/simulation/heatingresult.h>
+#include <lib/core/interfaces/IEnergyGrid.h>
 #include <utility>
 
 namespace cs = core::simulation;
+using Result = std::shared_ptr<cs::SimulationResult>;
+using EnergyGrid = std::unique_ptr<core::interface::IEnergyGrid>;
 
-struct Parser
+template <typename T>
+struct DataPackageParser
 {
-    virtual std::shared_ptr<cs::SimulationResult> operator()(const CSVDataMap &map) = 0;
-    virtual std::shared_ptr<cs::SimulationResult> operator()(const ArrowDataMap &map) = 0;
-    virtual std::shared_ptr<cs::SimulationResult> operator()(const ArrowData &data) = 0;
-    virtual std::shared_ptr<cs::SimulationResult> operator()(const CSVData &data) = 0;
+    typedef T Type;
+    // template<typename...Args>
+    // DataPackageParser<T>(Args&&... args){}
+    virtual T operator()(const CSVDataMap &map) = 0;
+    virtual T operator()(const ArrowDataMap &map) = 0;
+    virtual T operator()(const ArrowData &data) = 0;
+    virtual T operator()(const CSVData &data) = 0;
 };
 
-struct PowerParser : public Parser
+struct GridParser : DataPackageParser<EnergyGrid>
 {
-    std::shared_ptr<cs::SimulationResult> operator()(const CSVDataMap &map) override;
-    std::shared_ptr<cs::SimulationResult> operator()(const ArrowDataMap &map) override;
-    std::shared_ptr<cs::SimulationResult> operator()(const ArrowData &data) override;
-    std::shared_ptr<cs::SimulationResult> operator()(const CSVData &data) override;
 };
 
-struct HeatingParser : public Parser
+struct PowerGridParser final : GridParser
 {
-    std::shared_ptr<cs::SimulationResult> operator()(const CSVDataMap &map) override;
-    std::shared_ptr<cs::SimulationResult> operator()(const ArrowDataMap &map) override;
-    std::shared_ptr<cs::SimulationResult> operator()(const ArrowData &data) override;
-    std::shared_ptr<cs::SimulationResult> operator()(const CSVData &data) override;
+    EnergyGrid operator()(const CSVDataMap &map) override;
+    EnergyGrid operator()(const ArrowDataMap &map) override;
+    EnergyGrid operator()(const ArrowData &data) override;
+    EnergyGrid operator()(const CSVData &data) override;
+};
+
+struct HeatingGridParser final : GridParser
+{
+    EnergyGrid operator()(const CSVDataMap &map) override;
+    EnergyGrid operator()(const ArrowDataMap &map) override;
+    EnergyGrid operator()(const ArrowData &data) override;
+    EnergyGrid operator()(const CSVData &data) override;
+};
+
+struct SimulationParser : DataPackageParser<Result>
+{
+    Scenario scenario;
+    SimulationParser(const Scenario &s)
+        : scenario(s)
+    {
+    }
+};
+
+struct PowerParser final : public SimulationParser
+{
+    using SimulationParser::SimulationParser;
+    Result operator()(const CSVDataMap &map) override;
+    Result operator()(const ArrowDataMap &map) override;
+    Result operator()(const ArrowData &data) override;
+    Result operator()(const CSVData &data) override;
+};
+
+struct HeatingParser final : public SimulationParser
+{
+    using SimulationParser::SimulationParser;
+    Result operator()(const CSVDataMap &map) override;
+    Result operator()(const ArrowDataMap &map) override;
+    Result operator()(const ArrowData &data) override;
+    Result operator()(const CSVData &data) override;
+};
+
+template <EnergyType E>
+struct ParserMapping;
+
+template <>
+struct ParserMapping<EnergyType::POWER>
+{
+    using Sim = PowerParser;
+    using Grid = PowerGridParser;
+};
+
+template <>
+struct ParserMapping<EnergyType::HEATING>
+{
+    using Sim = HeatingParser;
+    using Grid = HeatingGridParser;
 };
 
 struct ParseManager
 {
-    template <typename T>
-    std::shared_ptr<cs::SimulationResult> operator()(const Scenario &scenario, EnergyType type, T &&data)
+    template <typename T, typename... Args>
+    auto operator()(EnergyType type, T &&data, Args &&...args) const -> decltype(auto)
     {
         switch (type)
         {
         case EnergyType::POWER:
-            return PowerParser()(std::forward<T>(data));
+            return parse<EnergyType::POWER>(std::forward<T>(data), std::forward<Args>(args)...);
         case EnergyType::HEATING:
-            return HeatingParser()(std::forward<T>(data));
+            return parse<EnergyType::HEATING>(std::forward<T>(data), std::forward<Args>(args)...);
         default:
-            return nullptr;
+            // Determine a safe null return type based on what we expected to get
+            using ExpectedRet = decltype(parse<EnergyType::POWER>(std::forward<T>(data), std::forward<Args>(args)...));
+            return ExpectedRet {};
+        }
+    }
+
+private:
+    template <EnergyType E, typename T, typename... Args>
+    auto parse(T &&data, Args &&...args) const -> decltype(auto)
+    {
+        using Map = ParserMapping<E>;
+
+        // Use SFINAE/Compile-time checks to pick the right parser
+        if constexpr (std::is_constructible_v<typename Map::Sim, Args...>)
+        {
+            return typename Map::Sim(std::forward<Args>(args)...)(std::forward<T>(data));
+        }
+        else
+        {
+            return typename Map::Grid(std::forward<Args>(args)...)(std::forward<T>(data));
         }
     }
 };
