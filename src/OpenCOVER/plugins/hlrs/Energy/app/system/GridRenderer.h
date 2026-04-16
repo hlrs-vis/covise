fa8baed1563cdd6a01d370cc26a40b21ecb02938
MDjur <marko.djuric@gmx.de>
2026-04-16 16:45:15 +0200

    GridRenderer: use matrix transform to hold EnergyGrids

--- src/OpenCOVER/plugins/hlrs/Energy/app/system/GridRenderer.h
+++ src/OpenCOVER/plugins/hlrs/Energy/app/system/GridRenderer.h
@@ -1,22 +1,32 @@
 #pragma once
 #include "EnergyType.h"
+#include "DataLoadManager.h"
+#include <lib/core/interfaces/IMovable.h>
 #include <lib/core/simulation/simulationresult.h>
+#include <lib/core/ClassLogger.h>
 #include <osg/Group>
+#include <osg/Switch>
 #include <osg/ref_ptr>
 
-class GridRenderer
+struct GridRenderConfig {
+    std::vector<double> offset;
+    std::string font;
+};
+
+class GridRenderer : core::ClassLogger
 {
 public:
-    GridRenderer(osg::ref_ptr<osg::Group> rootNode);
+    GridRenderer(osg::ref_ptr<osg::Switch> rootNode, const GridRenderConfig& offset, core::interface::ILogger& logger);
 
-    void buildGrid(EnergyType type);
+    void buildGrid(EnergyType type, DataLoadManager& loader);
     void setData(EnergyType type, std::shared_ptr<core::simulation::SimulationResult> data);
     void update();
     void updateStep(int timestep);
     void setVisible(EnergyType type, bool visible);
 
 private:
-    osg::ref_ptr<osg::Group> m_root;
-    std::map<EnergyType, osg::ref_ptr<osg::Switch>> m_gridNodes;
+    GridRenderConfig m_config;
+    osg::ref_ptr<osg::Switch> m_root;
+    std::map<EnergyType, osg::ref_ptr<osg::MatrixTransform>> m_gridNodes;
     std::map<EnergyType, std::shared_ptr<core::simulation::SimulationResult>> m_activeData;
 };
