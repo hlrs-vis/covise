6574ba6e2461da5177752826c41a3576b4b9a0fb
MDjur <marko.djuric@gmx.de>
2026-04-16 11:10:26 +0200

    Parser: rename typedef to prevent typename collision

--- src/OpenCOVER/plugins/hlrs/Energy/app/system/Parser.h
+++ src/OpenCOVER/plugins/hlrs/Energy/app/system/Parser.h
@@ -2,14 +2,16 @@
 #include "EnergyType.h"
 #include "DataPackage.h"
 #include "Scenario.h"
+#include "app/osg/presentation/grid.h"
 #include <lib/core/simulation/powerresult.h>
 #include <lib/core/simulation/heatingresult.h>
 #include <lib/core/interfaces/IEnergyGrid.h>
+#include <utils/read/csv/csv.h>
 #include <utility>
 
 namespace cs = core::simulation;
-using Result = std::shared_ptr<cs::SimulationResult>;
-using EnergyGrid = std::unique_ptr<core::interface::IEnergyGrid>;
+using result_ptr = std::shared_ptr<cs::SimulationResult>;
+using grid_ptr = std::unique_ptr<core::interface::IEnergyGrid>;
 
 template <typename T>
 struct DataPackageParser
@@ -21,27 +23,43 @@ struct DataPackageParser
     virtual T operator()(const CSVData &data) = 0;
 };
 
-struct GridParser : DataPackageParser<EnergyGrid>
+struct GridParser : DataPackageParser<grid_ptr>
 {
 };
 
 struct PowerGridParser final : GridParser
 {
-    EnergyGrid operator()(const CSVDataMap &map) override;
-    EnergyGrid operator()(const ArrowDataMap &map) override;
-    EnergyGrid operator()(const ArrowData &data) override;
-    EnergyGrid operator()(const CSVData &data) override;
+    grid_ptr operator()(const CSVDataMap &map) override;
+    grid_ptr operator()(const ArrowDataMap &map) override;
+    grid_ptr operator()(const ArrowData &data) override;
+    grid_ptr operator()(const CSVData &data) override;
+
+private:
+    typedef std::unordered_map<int, std::string> IDLookupTable;
+
+    std::vector<IDLookupTable> retrieveBusNameIdMapping(opencover::utils::read::CSVStream &stream);
+    std::vector<grid::PointsMap> createPowerGridPoints(
+        opencover::utils::read::CSVStream &stream, size_t &numPoints,
+        const float &sphereRadius, const std::vector<IDLookupTable> &busNames, const CSVDataMap& map);
+    std::pair<std::vector<grid::Lines>, std::vector<grid::ConnectionDataList>>
+    getPowerGridLines(opencover::utils::read::CSVStream &stream, const std::vector<grid::PointsMap> &points);
+    void helper_getAdditionalPowerGridPointData_addData(
+        int busId, grid::PointDataList &additionalData, const grid::Data &data);
+    void helper_getAdditionalPowerGridPointData_handleDuplicate(
+        std::string &name, std::map<std::string, uint> &duplicateMap);
+    std::unique_ptr<grid::PointDataList> getAdditionalPowerGridPointData(
+        const std::size_t &numOfBus, const CSVDataMap& map);
 };
 
 struct HeatingGridParser final : GridParser
 {
-    EnergyGrid operator()(const CSVDataMap &map) override;
-    EnergyGrid operator()(const ArrowDataMap &map) override;
-    EnergyGrid operator()(const ArrowData &data) override;
-    EnergyGrid operator()(const CSVData &data) override;
+    grid_ptr operator()(const CSVDataMap &map) override;
+    grid_ptr operator()(const ArrowDataMap &map) override;
+    grid_ptr operator()(const ArrowData &data) override;
+    grid_ptr operator()(const CSVData &data) override;
 };
 
-struct SimulationParser : DataPackageParser<Result>
+struct SimulationParser : DataPackageParser<result_ptr>
 {
     Scenario scenario;
     SimulationParser(const Scenario &s)
@@ -53,19 +71,19 @@ struct SimulationParser : DataPackageParser<Result>
 struct PowerParser final : public SimulationParser
 {
     using SimulationParser::SimulationParser;
-    Result operator()(const CSVDataMap &map) override;
-    Result operator()(const ArrowDataMap &map) override;
-    Result operator()(const ArrowData &data) override;
-    Result operator()(const CSVData &data) override;
+    result_ptr operator()(const CSVDataMap &map) override;
+    result_ptr operator()(const ArrowDataMap &map) override;
+    result_ptr operator()(const ArrowData &data) override;
+    result_ptr operator()(const CSVData &data) override;
 };
 
 struct HeatingParser final : public SimulationParser
 {
     using SimulationParser::SimulationParser;
-    Result operator()(const CSVDataMap &map) override;
-    Result operator()(const ArrowDataMap &map) override;
-    Result operator()(const ArrowData &data) override;
-    Result operator()(const CSVData &data) override;
+    result_ptr operator()(const CSVDataMap &map) override;
+    result_ptr operator()(const ArrowDataMap &map) override;
+    result_ptr operator()(const ArrowData &data) override;
+    result_ptr operator()(const CSVData &data) override;
 };
 
 template <EnergyType E>
