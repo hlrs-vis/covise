cf3964d67f644a1a344db1edb35569d20a692c3a
MDjur <marko.djuric@gmx.de>
2026-04-20 15:59:51 +0200

    Parser: movig typedefs result_ptr and grid_ptr to typedefs.h

--- src/OpenCOVER/plugins/hlrs/Energy/app/typedefs.h
+++ src/OpenCOVER/plugins/hlrs/Energy/app/typedefs.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <lib/core/interfaces/IInfoboard.h>
+#include <lib/core/interfaces/IEnergyGrid.h>
 
+#include "lib/core/simulation/simulationresult.h"
 #include "lib/core/simulation/powerresult.h"
 #include "osg/OsgImpl.h"
 
@@ -10,3 +12,5 @@ typedef core::interface::Pos Pos;
 typedef core::simulation::power::PVData PVData;
 typedef OsgBuildingImpl BuildingImpl;
 typedef OsgBuildingTimedependImpl BuildingTimedependImpl;
+typedef std::shared_ptr<core::interface::IEnergyGrid> grid_ptr;
+typedef std::shared_ptr<core::simulation::SimulationResult> result_ptr;
