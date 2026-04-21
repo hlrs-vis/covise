75dd3682f840e3ed46d3cbfd9a50a851a4dde6d5
MDjur <marko.djuric@gmx.de>
2026-04-21 16:00:47 +0200

    PowerParser: migrate PowerSimulation parse logic to PowerParser

--- src/OpenCOVER/plugins/hlrs/Energy/app/system/PowerParser.cpp
+++ src/OpenCOVER/plugins/hlrs/Energy/app/system/PowerParser.cpp
@@ -1,6 +1,23 @@
 #include "PowerParser.h"
 #include <lib/core/simulation/powerresult.h>
 
+namespace
+{
+const std::array<std::string, 13> skipInfluxTables {
+    "timestamp", "district", "hkw", "new-buildings",
+    "pv-penetration", "loc_emob", "n_emob", "awz_scaling",
+    "loc_ev", "n_ev", "new_buildings", "operation_mode",
+    "pv_scaling"
+};
+
+auto isSkippedInfluxTable(const std::string &name)
+{
+    return std::any_of(skipInfluxTables.begin(), skipInfluxTables.end(),
+        [&](const auto &s)
+        { return s == name; });
+}
+}
+
 result_ptr PowerParser::operator()(CSVDataMap &map)
 {
     auto result = std::make_shared<cs::power::PowerSimulationResult>();
@@ -14,11 +31,69 @@ result_ptr PowerParser::operator()(CSVDataMap &map)
 result_ptr PowerParser::operator()(const ArrowDataMap &map)
 {
     auto result = std::make_shared<cs::power::PowerSimulationResult>();
+    auto &cables = result->Cables();
+    auto &buses = result->Buses();
+    auto &buildings = result->Buildings();
+    auto *objectMap = &cables;
+    std::string key { "loading_percent" };
+
     for (auto &[name, table] : map)
     {
-        // TODO: put data into result
+        if (name == "electrical_grid.res_bus.vm_pu")
+        {
+            key = "vm_pu";
+            objectMap = &buses;
+        }
+        else if (name == "electrical_prosumer.res_mw")
+        {
+            key = "res_mw";
+            objectMap = &buildings;
+        }
+        else if (name == "electrical_grid.res_line.loading_percent")
+        {
+            key = "loading_percent";
+            objectMap = &cables;
+        }
+        processArrowTableColumns(table, *objectMap, key);
     }
-    return result;
+    result->init();
+    return std::move(result);
+}
+
+void PowerParser::processArrowTableColumns(const std::shared_ptr<arrow::Table> &tbl, ObjectMap &map,
+    const std::string &dataKey)
+{
+    auto columnNames = tbl->schema()->fields();
+    for (int j = 0; j < tbl->num_columns(); ++j)
+    {
+        auto columnName = columnNames[j]->name();
+        std::replace(columnName.begin(), columnName.end(), ' ', '_');
+        std::replace(columnName.begin(), columnName.end(), '/', '-');
+        if (isSkippedInfluxTable(columnName))
+            continue;
+        auto column = tbl->column(j);
+        int64_t chunk_offset = 0;
+        for (int i = 0; i < column->num_chunks(); ++i)
+        {
+            auto chunk = column->chunk(i);
+            if (chunk->type_id() == arrow::Type::DOUBLE)
+            {
+                auto darr = std::static_pointer_cast<arrow::DoubleArray>(chunk);
+                auto rawValues = darr->raw_values();
+                // create entry if it doesn't exist
+                if (map.find(columnName) == map.end())
+                {
+                    Data data = { { dataKey, std::vector<double>(column->length()) } };
+                    auto object = std::make_unique<Object>(columnName, std::move(data));
+                    map.emplace(columnName, std::move(object));
+                }
+                auto &vec = map[columnName]->getData()[dataKey];
+                std::copy(rawValues, rawValues + darr->length(),
+                    vec.begin() + chunk_offset);
+                chunk_offset += darr->length();
+            }
+        }
+    }
 }
 
 result_ptr PowerParser::operator()(const ArrowData &data)
