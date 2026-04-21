71a149fa40d58951c82aa177ba00620b9fc69af5
MDjur <marko.djuric@gmx.de>
2026-04-21 14:16:01 +0200

    ParseManager: prototype for compile time evaluation of data support of parser (will be worked later on)

--- src/OpenCOVER/plugins/hlrs/Energy/app/system/ParseManager.h
+++ src/OpenCOVER/plugins/hlrs/Energy/app/system/ParseManager.h
@@ -1,10 +1,12 @@
 #pragma once
 #include "EnergyType.h"
+#include "DataPackage.h"
 #include "PowerGridParser.h"
 #include "HeatingGridParser.h"
 #include "PowerParser.h"
 #include "HeatingParser.h"
 #include <utility>
+#include <type_traits>
 
 template <EnergyType E>
 struct ParserMapping;
@@ -22,6 +24,61 @@ struct ParserMapping<EnergyType::HEATING>
     using Sim = HeatingParser;
     using Grid = HeatingGridParser;
 };
+
+// template <EnergyType E>
+// struct ParserCapabilities
+// {
+//     static constexpr bool supports_csv_data_map = false;
+//     static constexpr bool supports_csv_data = false;
+//     static constexpr bool supports_arrow_data_map = false;
+//     static constexpr bool supports_arrow_data = false;
+// };
+
+// template <EnergyType E>
+// struct GridParserCapabilities : ParserCapabilities<E>
+// {
+// };
+
+// template <EnergyType E>
+// struct SimulationParserCapabilities : ParserCapabilities<E>
+// {
+// };
+
+// template <>
+// struct GridParserCapabilities<EnergyType::HEATING>
+// {
+//     static constexpr bool supports_csv_data_map = false;
+//     static constexpr bool supports_csv_data = false;
+//     static constexpr bool supports_arrow_data_map = false;
+//     static constexpr bool supports_arrow_data = false;
+// };
+
+// template <>
+// struct GridParserCapabilities<EnergyType::POWER>
+// {
+//     static constexpr bool supports_csv_data_map = false;
+//     static constexpr bool supports_csv_data = false;
+//     static constexpr bool supports_arrow_data_map = false;
+//     static constexpr bool supports_arrow_data = false;
+// };
+
+// template <>
+// struct SimulationParserCapabilities<EnergyType::HEATING>
+// {
+//     static constexpr bool supports_csv_data_map =  true;
+//     static constexpr bool supports_csv_data = false;
+//     static constexpr bool supports_arrow_data_map = false;
+//     static constexpr bool supports_arrow_data = false;
+// };
+
+// template <>
+// struct SimulationParserCapabilities<EnergyType::POWER>
+// {
+//     static constexpr bool supports_csv_data_map = false;
+//     static constexpr bool supports_csv_data = false;
+//     static constexpr bool supports_arrow_data_map = false;
+//     static constexpr bool supports_arrow_data = false;
+// };
 
 struct ParseManager
 {
@@ -42,6 +99,7 @@ struct ParseManager
     }
 
 private:
+
     template <EnergyType E, typename T, typename... Args>
     auto parse(T &&data, Args &&...args) const -> decltype(auto)
     {
@@ -57,4 +115,44 @@ private:
             return typename Map::Grid(std::forward<Args>(args)...)(std::forward<T>(data));
         }
     }
+
+    // NOTE: Trying to check for implemented operator Parser functions.
+    // template <typename Capabilities, typename T>
+    // struct is_supported {
+    //     // remove references and cv-qualifiers
+    //     using RawT = std::decay_t<T>;
+    
+    //     static constexpr bool value = 
+    //         (std::is_same_v<RawT, CSVData> && Capabilities::supports_csv_data) ||
+    //         (std::is_same_v<RawT, CSVDataMap> && Capabilities::supports_csv_data_map) ||
+    //         (std::is_same_v<RawT, ArrowData> && Capabilities::supports_arrow_data) ||
+    //         (std::is_same_v<RawT, ArrowDataMap> && Capabilities::supports_arrow_data_map);
+    // };
+    //
+    // template <EnergyType E, typename T, typename... Args>
+    // auto parse(T &&data, Args &&...args) const -> decltype(auto)
+    // {
+    //     using Map = ParserMapping<E>;
+
+    //     // Use SFINAE/Compile-time checks to pick the right parser
+    //     if constexpr (std::is_constructible_v<typename Map::Sim, Args...>)
+    //     {
+    //         using Caps = SimulationParserCapabilities<E>;
+    //         // Check if parser is capabable of parsing this type of data
+    //         if constexpr (is_supported<Caps, T>::value){
+    //             return typename Map::Sim(std::forward<Args>(args)...)(std::forward<T>(data));
+    //         } else {
+    //             return result_ptr(nullptr);
+    //         }
+    //     }
+    //     if constexpr (std::is_constructible_v<typename Map::Grid, Args...>)
+    //     {
+    //         using Caps = GridParserCapabilities<E>;
+    //         if constexpr (is_supported<Caps, T>::value){
+    //             return typename Map::Grid(std::forward<Args>(args)...)(std::forward<T>(data));
+    //         } else {
+    //             return grid_ptr(nullptr);
+    //         }
+    //     }
+    // }
 };
