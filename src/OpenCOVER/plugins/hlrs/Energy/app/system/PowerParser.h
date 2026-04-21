e68627b7bee43f209663212389316c5f1d28e44e
MDjur <marko.djuric@gmx.de>
2026-04-21 16:00:47 +0200

    PowerParser: migrate PowerSimulation parse logic to PowerParser

--- src/OpenCOVER/plugins/hlrs/Energy/app/system/PowerParser.h
+++ src/OpenCOVER/plugins/hlrs/Energy/app/system/PowerParser.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "SimulationParser.h"
+#include <lib/core/simulation/object.h>
+
+using namespace core::simulation;
 
 struct PowerParser final : public SimulationParser
 {
@@ -8,4 +11,8 @@ struct PowerParser final : public SimulationParser
     result_ptr operator()(const ArrowDataMap &map) override;
     result_ptr operator()(const ArrowData &data) override;
     result_ptr operator()(CSVData &data) override;
+
+private:
+    void processArrowTableColumns(const std::shared_ptr<arrow::Table> &tbl, ObjectMap &map,
+        const std::string &dataKey);
 };
