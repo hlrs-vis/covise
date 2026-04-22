28e64b58657a8eb03d1faba160fc70ca2afebe8a
MDjur <marko.djuric@gmx.de>
2026-04-22 09:06:09 +0200

    HeatingParser: migrate init of HeatingSimulationResult to HeatingParser

--- src/OpenCOVER/plugins/hlrs/Energy/app/system/HeatingParser.h
+++ src/OpenCOVER/plugins/hlrs/Energy/app/system/HeatingParser.h
@@ -1,5 +1,13 @@
 #pragma once
 #include "SimulationParser.h"
+#include "lib/core/simulation/heatingresult.h"
+#include <lib/core/simulation/object_type.h>
+#include <lib/core/simulation/object_factory.h>
+#include <lib/core/simulation/object.h>
+#include <memory>
+
+using namespace core::simulation;
+typedef std::shared_ptr<heating::HeatingSimulationResult> heating_ptr;
 
 struct HeatingParser final : public SimulationParser
 {
@@ -8,4 +16,10 @@ struct HeatingParser final : public SimulationParser
     result_ptr operator()(const ArrowDataMap &map) override;
     result_ptr operator()(const ArrowData &data) override;
     result_ptr operator()(CSVData &data) override;
+
+private:
+    auto getObjMapByType(heating_ptr result, ObjectType type) -> ObjectMap *;
+    void createObjAndAddToMap(heating_ptr result, ObjectType type, const std::string &name);
+    auto getObjPtr(heating_ptr result, ObjectType type, const std::string &name) -> Object*;
+    void addDataToMap(heating_ptr result, ObjectType type, const std::string &name, const std::string &valName, double value);
 };
