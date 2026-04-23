28ef0f97adbf4d31038ed418e3c8cbcf205173a5
DeVasconcelos <malheiros.susanne@gmail.com>
2026-04-23 11:04:19 +0200

    flip avatar based on head rotation

--- src/OpenCOVER/plugins/hlrs/GhostAvatar/controls/PlanarAvatarControls.cpp
+++ src/OpenCOVER/plugins/hlrs/GhostAvatar/controls/PlanarAvatarControls.cpp
@@ -31,14 +31,48 @@ void PlanarAvatarControls::updateBones(const osg::Matrix &floorMatrix, const osg
     auto targetHeight = headMatrix.getTrans().z() - floorMatrix.getTrans().z();
     float scale = targetHeight / (getInitialBounds())[1];
 
+    auto headForward = getForwardDirection() * osg::Matrix::rotate(headMatrix.getRotate());
+    auto up = getUpDirection();
+    up.normalize();
+
+    // Evaluate turn-around by yaw only: ignore head pitch/roll.
+    headForward -= up * (headForward * up);
+    bool flipBody = false;
+    if (headForward.length2() > 1e-8)
+    {
+        headForward.normalize();
+        const auto refForward = getForwardDirection();
+        flipBody = (headForward * refForward) < 0.0f;
+    }
+
+    osg::Quat bodyFlip;
+    if (flipBody)
+    {
+        constexpr double pi = 3.14159265358979323846;
+        bodyFlip.makeRotate(pi, up);
+    }
+    else
+    {
+        bodyFlip.makeRotate(0.0, up);
+    }
+
     m_avatarTrans->setMatrix(osg::Matrix::scale(scale, scale, scale) *
                              osg::Matrix::rotate(getBaseRotation()) *
+                             osg::Matrix::rotate(bodyFlip) *
                              osg::Matrix::translate(floorMatrix.getTrans()));
+
+/*     m_avatarTrans->setMatrix(osg::Matrix::scale(scale, scale, scale) *
+                             osg::Matrix::rotate(getBaseRotation()) *
+                             osg::Matrix::translate(floorMatrix.getTrans())); */
 
     // TODO: the adjust matrix should be part of the bone and not have to be passed to these methods
     if (m_armBone)
         moveBoneToTarget(*m_armBone, handMatrix.getTrans(), m_armAdjustMatrix);
 
     if (m_headBone)
-        rotateBone(*m_headBone, headMatrix.getRotate(), m_headAdjustMatrix);
+    {
+        // Compensate the body flip so the head doesn't over-rotate
+        osg::Quat headRot = flipBody ? bodyFlip.inverse() * headMatrix.getRotate() : headMatrix.getRotate();
+        rotateBone(*m_headBone, headRot, m_headAdjustMatrix);
+    }
 }
