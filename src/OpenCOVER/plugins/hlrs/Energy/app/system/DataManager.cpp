e5c7f7bf3d4c6502f3ed0562ab9c916cdceefc84
MDjur <marko.djuric@gmx.de>
2026-04-27 11:59:59 +0200

    DataManager: cleanup

--- src/OpenCOVER/plugins/hlrs/Energy/app/system/DataManager.cpp
+++ src/OpenCOVER/plugins/hlrs/Energy/app/system/DataManager.cpp
@@ -6,7 +6,6 @@ void DataManager::loadScenario(Storage storageType, const Scenario &scenario,
 {
     for (auto type : ENERGYTYPE_RANGE)
     {
-        // if (m_cache.count(scenario.id) && m_cache[scenario.id].count(type))
         if (m_cache.count(storageType) && m_cache[storageType].count(scenario.id) && m_cache[storageType][scenario.id].count(type))
             // check if data is valid/stale
             if (m_cache[storageType][scenario.id][type].use_count() > 0)
