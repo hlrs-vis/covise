1749ff85486b6cde26098ad411892609d53de1f4
MDjur <marko.djuric@gmx.de>
2026-04-28 14:30:08 +0200

    Storage: add new function to convert Storage to string

--- src/OpenCOVER/plugins/hlrs/Energy/app/system/Storage.h
+++ src/OpenCOVER/plugins/hlrs/Energy/app/system/Storage.h
@@ -11,3 +11,16 @@ enum class Storage
 
 inline constexpr std::array<Storage, 2> FILE_STORAGE_RANGE = { Storage::ARROW, Storage::CSV };
 inline constexpr std::array<Storage, 3> FULL_STORAGE_RANGE = { Storage::ARROW, Storage::CSV, Storage::PSQL };
+
+inline auto StorageToString(Storage type) {
+    switch(type) {
+        case Storage::ARROW:
+            return "arrow";
+        case Storage::CSV:
+            return "csv";
+        case Storage::PSQL:
+            return "psql";
+        default:
+            return "unkown";
+    }
+}
