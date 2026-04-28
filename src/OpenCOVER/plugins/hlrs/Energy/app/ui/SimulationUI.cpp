bcf2e1626fda43d7b52f0dbf0fef597be7d69a7e
MDjur <marko.djuric@gmx.de>
2026-04-28 14:30:47 +0200

    SimulationUI: add storage selectionlists for each EnergyType to ui

--- src/OpenCOVER/plugins/hlrs/Energy/app/ui/SimulationUI.cpp
+++ src/OpenCOVER/plugins/hlrs/Energy/app/ui/SimulationUI.cpp
@@ -1,6 +1,7 @@
 #include "SimulationUI.h"
 #include "BaseUI.h"
 #include "app/system/EnergyType.h"
+#include "app/system/Storage.h"
 #include <stdexcept>
 #include <algorithm>
 
@@ -26,4 +27,41 @@ void SimulationUI::init(const core::interface::ui::IGUIFactory &factory, const s
     std::transform(ENERGYTYPE_RANGE.begin(), ENERGYTYPE_RANGE.end(), typeNames.data(), [&](EnergyType type)
         { return EnergyTypeToString(type); });
     m_energyGrids->setList(typeNames);
+    
+    std::vector<std::string> storageNames(FULL_STORAGE_RANGE.size());
+    std::transform(FULL_STORAGE_RANGE.begin(), FULL_STORAGE_RANGE.end(), storageNames.data(), [&](Storage storage)
+        { return StorageToString(storage); });
+    
+    std::string selListName("");
+    for (auto type: ENERGYTYPE_RANGE){
+        selListName = EnergyTypeToString(type);
+        auto selList = factory.createSelectionList(m_menu.get(), selListName + " Storage");
+        selList->setList(storageNames);
+        m_storage[type] = std::move(selList);
+    }
+}
+
+void SimulationUI::setStorageSelectionCallback(EnergyType type, const std::function<void(int)> &func)
+{
+    if (auto it = m_storage.find(type); it != m_storage.end())
+        it->second->setCallback(func);
+}
+
+void SimulationUI::setStorageSelectionDefault(EnergyType type, Storage storage) 
+{
+    if (auto it = m_storage.find(type); it != m_storage.end())
+    {
+       auto &selList = it->second; 
+       auto storage_ptr = std::find(FULL_STORAGE_RANGE.begin(), FULL_STORAGE_RANGE.end(), storage);
+       int index_of_ptr = storage_ptr - FULL_STORAGE_RANGE.begin();
+       selList->select(index_of_ptr);
+    }
+}
+
+Storage SimulationUI::getSelectedStorage(EnergyType type)
+{
+    if (m_storage.find(type) == m_storage.end())
+        return Storage::UNKNOWN;
+    // will work because of the initialization order in init()
+    return FULL_STORAGE_RANGE[m_storage[type]->selectedIndex()];
 }
