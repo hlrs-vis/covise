d0fba82cf26a3e8c1a90c9d052469701a3e5d0b8
Uwe Woessner <woessner@hlrs.de>
2026-06-05 00:00:07 +0200

    fix elevator buttons on landings renumber correctly

--- src/OpenCOVER/plugins/hlrs/Revit/Elevators.h
+++ src/OpenCOVER/plugins/hlrs/Revit/Elevators.h
@@ -56,7 +56,7 @@ public:
     PartType type = PartUdefined;
     std::vector<ElevatorPart *> doors;
     int levelNumber;
-    ElevatorPart *buttonLanding; // landing this button is for.
+    ElevatorPart *buttonLanding=nullptr; // landing this button is for.
     int doorNumber;
     int direction = 1;
     virtual int hit(vrui::vruiHit *hit);
@@ -70,6 +70,7 @@ public:
     CabinState oldTravelDirection = MoveUp;
     int currentLanding=0;
     float carPos = 0;
+    float heightOffset = 0;
     float destinationY = 0;
     float startingY = 0;
     float v = 0;
