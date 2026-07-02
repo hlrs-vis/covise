6a5c11232f0e614bfb890f087cf643887e5e5a27
MDjur <marko.djuric@gmx.de>
2026-07-02 12:09:57 +0200

    EnergyCampus: add missing files

---
+++ src/OpenCOVER/plugins/hlrs/EnergyCampus/lib/ennovatis/building.cpp
@@ -0,0 +1,25 @@
+#include "building.h"
+
+#include <sstream>
+
+namespace ennovatis {
+bool Channel::empty() const {
+  return name.empty() && id.empty() && description.empty() && type.empty() &&
+         unit.empty() && group == ChannelGroup::None;
+}
+
+void Channel::clear() {
+  name.clear();
+  id.clear();
+  description.clear();
+  type.clear();
+  unit.clear();
+}
+
+const std::string Channel::to_string() const {
+  std::stringstream ss;
+  ss << "name: " << name << "\ndescription: " << description << "\ntype: " << type
+     << "\nunit: " << unit << "\nChannelgroup: " << ChannelGroupToString(group);
+  return ss.str();
+}
+}  // namespace ennovatis
