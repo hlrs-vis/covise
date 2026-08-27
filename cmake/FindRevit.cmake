6018b43b8a499b6d762e349fc3bd963598538a6a
Marko Djuric <marko.djuric@gmx.de>
2026-08-27 12:25:27 +0200

    FindRevit: add Definition REVIT_DEFINES
    REVIT_DEFINES is a list which contains a Defintion for REVIT_<VERSION> (e.g. REVIT_2027) along with a range of REVIT_<2019-VERSION>_OR_GREATER variables, starting from version 2019.

--- cmake/FindRevit.cmake
+++ cmake/FindRevit.cmake
@@ -1,9 +1,10 @@
 # - Try to find revit-library
 # Once done this will define
 #
-#  REVIT_DIRECTORY    - where to find Revit
+#  REVIT_DIRECTORY      - where to find Revit
 #  REVIT_LIBRARIES      - list of libraries when using revit
 #  REVIT_FOUND          - True if revit was found.
+#  REVIT_DEFINES        - Includes target compiledefinitions for range 2019 to current version: REVIT_<VERSION> or REVIT_<2019::VERSION>_OR_GREATER
 
 SET(tmpPATH $ENV{ProgramFiles})
 string(REPLACE "\\" "/" tmpPATH "${tmpPATH}")
@@ -31,3 +32,14 @@ INCLUDE(FindPackageHandleStandardArgs)
 
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(Revit DEFAULT_MSG REVIT_DIRECTORY)
 MARK_AS_ADVANCED(REVIT_DIRECTORY)
+
+# Define Revit Version without the need of Nuget packages
+string(REGEX MATCH "Revit ([0-9]+)" _match ${REVIT_DIRECTORY})
+set(REVIT_VERSION ${CMAKE_MATCH_1})
+message(STATUS "Detected Revit version: ${REVIT_VERSION}")
+
+# Build cumulative defines
+set(REVIT_DEFINES "REVIT_${REVIT_VERSION}")
+foreach(v RANGE 2019 ${REVIT_VERSION})
+  list(APPEND REVIT_DEFINES "REVIT_${v}_OR_GREATER")
+endforeach()
