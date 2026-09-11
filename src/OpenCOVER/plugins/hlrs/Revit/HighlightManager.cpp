8b920b21667a225411c1a6cbabab851fb46b9557
Marko Djuric <marko.djuric@gmx.de>
2026-09-11 15:54:56 +0200

    RevitPlugin: format HighlightManager

--- src/OpenCOVER/plugins/hlrs/Revit/HighlightManager.cpp
+++ src/OpenCOVER/plugins/hlrs/Revit/HighlightManager.cpp
@@ -3,47 +3,55 @@
 #include <osg/MatrixTransform>
 #include <cover/coVRSelectionManager.h>
 
-namespace Revit {
+namespace Revit
+{
 
-size_t HighlightManager::findColorIdx(const Color& color) {
+size_t HighlightManager::findColorIdx(const Color &color)
+{
     auto it = std::ranges::find(_palette, color);
-    if (it == std::ranges::end(_palette)) {
+    if (it == std::ranges::end(_palette))
+    {
         _palette.push_back(color);
         return _palette.size() - 1;
     }
     return it - _palette.begin();
 }
 
-void HighlightManager::add(std::span<Node> nodes, Color color) {
+void HighlightManager::add(std::span<Node> nodes, Color color)
+{
     auto idx = findColorIdx(color);
-	for (auto node : nodes)
-		_highlightedNodes.insert_or_assign(node, idx);
+    for (auto node : nodes)
+        _highlightedNodes.insert_or_assign(node, idx);
 }
 
-void HighlightManager::remove(Node node) {
+void HighlightManager::remove(Node node)
+{
     if (!isHighlightedNode(node))
         return;
     highlight(node, false);
     _highlightedNodes.erase(node);
 }
 
-void HighlightManager::highlight(Node node, bool enable) {
+void HighlightManager::highlight(Node node, bool enable)
+{
     if (!isHighlightedNode(node))
         return;
-    
+
     auto colorIdx = _highlightedNodes[node];
     auto color = _palette[colorIdx];
-    
+
     _highlighter(node, color, enable);
 }
 
-bool HighlightManager::isHighlightedNode(Node node) {
+bool HighlightManager::isHighlightedNode(Node node)
+{
     if (!node)
         return false;
     return _highlightedNodes.contains(node);
 }
 
-void HighlightManager::setHighlighter(HighlightStrategy higlighter) {
+void HighlightManager::setHighlighter(HighlightStrategy higlighter)
+{
     _highlighter = higlighter;
 }
 
--- src/OpenCOVER/plugins/hlrs/Revit/HighlightManager.h
+++ src/OpenCOVER/plugins/hlrs/Revit/HighlightManager.h
@@ -5,23 +5,28 @@
 #include <span>
 #include <functional>
 
-namespace opencover {
-    class coVRSelectionManager;
+namespace opencover
+{
+class coVRSelectionManager;
 }
-namespace Revit {
-struct Color {
+namespace Revit
+{
+struct Color
+{
     float red = 0.0f, green = 0.0f, blue = 0.0f;
-    bool operator==(const Color&) const = default;
+    bool operator==(const Color &) const = default;
 };
 
 using ColorIndex = size_t;
 using Node = osg::ref_ptr<osg::Node>;
 
-class HighlightManager final {
+class HighlightManager final
+{
 public:
-    using HighlightStrategy = std::function<void(Node, Color const&, bool)>;
+    using HighlightStrategy = std::function<void(Node, Color const &, bool)>;
 
-    HighlightManager(HighlightStrategy highlighter) : _highlighter{highlighter} {};
+    HighlightManager(HighlightStrategy highlighter)
+        : _highlighter { highlighter } { };
     void add(std::span<Node> nodes, Color color);
     void remove(Node node);
     void highlight(Node node, bool enable);
@@ -29,7 +34,7 @@ public:
     void setHighlighter(HighlightStrategy higlighter);
 
 private:
-    size_t findColorIdx(const Color& color);
+    size_t findColorIdx(const Color &color);
 
     std::map<Node, ColorIndex> _highlightedNodes;
     std::vector<Color> _palette;
